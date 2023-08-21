84f803b90476f9d68dc196faa49a0196455e22f6
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/forms/[formId]/route.ts

---
+++ app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/forms/[formId]/route.ts
@@ -0,0 +1,11 @@
+import {
+  GET_GetAppFormSettingsById,
+  PUT_UpdateAppFormSettings,
+  PATCH_DeleteAppFormSettings,
+} from "@/engines/app-form/form-settings.api";
+
+export {
+  GET_GetAppFormSettingsById as GET,
+  PUT_UpdateAppFormSettings as PUT,
+  PATCH_DeleteAppFormSettings as PATCH,
+};
