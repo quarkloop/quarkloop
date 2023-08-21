6792f7c9d1c9960ee68d3497528fa13717c13f01
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file app/api/v1/os/route.ts

---
+++ app/api/v1/os/route.ts
@@ -0,0 +1,9 @@
+import {
+    GET_GetOperatingSystemsByUserId,
+    POST_CreateOperatingSystem,
+} from "@/engines/os/os.api";
+
+export {
+    GET_GetOperatingSystemsByUserId as GET,
+    POST_CreateOperatingSystem as POST,
+};
