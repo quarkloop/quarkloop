568784679a9548b61a2b375a4873890bd7415acb
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/pages/route.ts

---
+++ app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/pages/route.ts
@@ -0,0 +1,9 @@
+import {
+  GET_GetAppPagesSettingsByAppId,
+  POST_CreateAppPageSettings,
+} from "@/engines/app-page/page-settings.api";
+
+export {
+  GET_GetAppPagesSettingsByAppId as GET,
+  POST_CreateAppPageSettings as POST,
+};
