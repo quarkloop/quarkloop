67da43eb79b58bfcab876e310805e9bb59e2f1da
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file app/api/v1/user/sessions/route.ts

---
+++ app/api/v1/user/sessions/route.ts
@@ -0,0 +1,11 @@
+import {
+    GET_GetUserSessions,
+    PATCH_TerminateUserSession,
+} from "@/engines/user/user.api";
+
+export const dynamic = "force-dynamic";
+
+export {
+    GET_GetUserSessions as GET,
+    PATCH_TerminateUserSession as PATCH,
+};
