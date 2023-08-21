ebd2efb4c9acc029b71609ce10a2fff80f3c4c43
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
+  swcMinify: true,
+  experimental: {
+    esmExternals: true,
+  },
+  transpilePackages: ["@quarkloop/lib", "@quarkloop/prisma"],
+});
+
+module.exports = nextConfig;
