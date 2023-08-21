4a8137b3311854cd5d3066ecbe9d37ae0ff2c0b1
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/files/route.ts

---
+++ app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/files/route.ts
@@ -0,0 +1,9 @@
+import {
+  GET_GetAppFileSettingsByAppId,
+  POST_CreateAppFileSettings,
+} from "@/engines/app-file/file-settings.api";
+
+export {
+  GET_GetAppFileSettingsByAppId as GET,
+  POST_CreateAppFileSettings as POST,
+};
