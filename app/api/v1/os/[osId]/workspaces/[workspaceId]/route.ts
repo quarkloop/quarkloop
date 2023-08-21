be67ef974bd99854fd11430b9d0c787ff70c4c74
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-21 13:51:35 +0200

    [Init] add file app/api/v1/os/[osId]/workspaces/[workspaceId]/route.ts

---
+++ app/api/v1/os/[osId]/workspaces/[workspaceId]/route.ts
@@ -0,0 +1,11 @@
+import {
+    GET_GetWorkspaceById,
+    PUT_UpdateWorkspace,
+    PATCH_DeleteWorkspace,
+} from "@/engines/workspace/workspace.api";
+
+export {
+    GET_GetWorkspaceById as GET,
+    PUT_UpdateWorkspace as PUT,
+    PATCH_DeleteWorkspace as PATCH,
+};
