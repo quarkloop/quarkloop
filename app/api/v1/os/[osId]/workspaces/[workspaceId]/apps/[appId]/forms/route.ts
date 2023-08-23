ba1ae38e75d6f5fc212cdedb225758b13f6e27cd
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:09:17 +0200

    [Workspace] remove file app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/forms/route.ts

--- app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/forms/route.ts
+++
@@ -1,9 +0,0 @@
-import {
-  GET_GetAppFormsSettingsByAppId,
-  POST_CreateAppFormSettings,
-} from "@/engines/app-form/form-settings.api";
-
-export {
-  GET_GetAppFormsSettingsByAppId as GET,
-  POST_CreateAppFormSettings as POST,
-};
