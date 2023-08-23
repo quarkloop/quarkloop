684ab6a69d03ce6af3a9c8379dcad1915c1deab6
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:09:17 +0200

    [Workspace] remove file app/api/v1/app/submissions/[submissionId]/route.ts

--- app/api/v1/app/submissions/[submissionId]/route.ts
+++
@@ -1,8 +0,0 @@
-import {
-  GET_GetAppSubmissionById,
-  PUT_UpdateAppSubmission,
-} from "@/engines/app-submission";
-
-export const dynamic = "force-dynamic";
-
-export { GET_GetAppSubmissionById as GET, PUT_UpdateAppSubmission as PUT };
