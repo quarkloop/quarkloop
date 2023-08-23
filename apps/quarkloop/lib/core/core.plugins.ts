badfa8e6677569c6098245053a11d6e6a419b4c2
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:15:51 +0200

    [quarkloop] add workspace file apps/quarkloop/lib/core/core.plugins.ts

---
+++ apps/quarkloop/lib/core/core.plugins.ts
@@ -0,0 +1,151 @@
+import { createPlugin } from "@/lib/pipeline";
+
+import {
+  ApiResponse,
+  PipelineState,
+  PluginStatusEntry,
+} from "@/lib/core/pipeline";
+
+/// GetApiResponse
+export interface GetApiResponse extends ApiResponse {}
+export interface GetApiResponsePluginArgs {}
+
+/// GetApiResponse Plugin
+export const GetApiResponsePlugin = createPlugin<
+  PipelineState,
+  GetApiResponsePluginArgs[]
+>({
+  name: "GetApiResponsePlugin",
+  config: {},
+  handler: async (state, config, ...args): Promise<PipelineState> => {
+    // Error Response
+    if (state.status) {
+      return {
+        ...state,
+        apiResponse: {
+          status: state.status,
+        },
+      };
+    }
+
+    // Success Response
+    return {
+      ...state,
+      apiResponse: {
+        status: PluginStatusEntry.OK(),
+        isOwner: false,
+        user: state.user?.user,
+        planSubscription: state.database?.planSubscription?.records as any,
+        data: {
+          ...(state.database && { database: state.database }),
+        },
+      },
+    };
+  },
+});
+
+/// CreateApiResponse
+export interface CreateApiResponse extends ApiResponse {}
+export interface CreateApiResponsePluginArgs {}
+
+/// CreateApiResponse Plugin
+export const CreateApiResponsePlugin = createPlugin<
+  PipelineState,
+  CreateApiResponsePluginArgs[]
+>({
+  name: "CreateApiResponsePlugin",
+  config: {},
+  handler: async (state, config, ...args): Promise<PipelineState> => {
+    // Error Response
+    if (state.status) {
+      return {
+        ...state,
+        apiResponse: {
+          status: state.status,
+        },
+      };
+    }
+
+    // Success Response
+    return {
+      ...state,
+      apiResponse: {
+        status: PluginStatusEntry.CREATED(),
+        data: {
+          ...(state.database && { database: state.database }),
+          //appDatabase: state.appDatabase,
+          //appIntegration: state.appIntegration,
+        },
+      },
+    };
+  },
+});
+
+/// UpdateApiResponse
+export interface UpdateApiResponse extends ApiResponse {}
+export interface UpdateApiResponsePluginArgs {}
+
+/// UpdateApiResponse Plugin
+export const UpdateApiResponsePlugin = createPlugin<
+  PipelineState,
+  UpdateApiResponsePluginArgs[]
+>({
+  name: "UpdateApiResponsePlugin",
+  config: {},
+  handler: async (state, config, ...args): Promise<PipelineState> => {
+    // Error Response
+    if (state.status) {
+      return {
+        ...state,
+        apiResponse: {
+          status: state.status,
+        },
+      };
+    }
+
+    // Success Response
+    return {
+      ...state,
+      apiResponse: {
+        status: PluginStatusEntry.OK(),
+        data: {
+          ...(state.database && { database: state.database }),
+          //appDatabase: state.appDatabase,
+          //appIntegration: state.appIntegration,
+        },
+      },
+    };
+  },
+});
+
+/// DeleteApiResponse
+export interface DeleteApiResponse extends ApiResponse {}
+export interface DeleteApiResponsePluginArgs {}
+
+/// DeleteApiResponse Plugin
+export const DeleteApiResponsePlugin = createPlugin<
+  PipelineState,
+  DeleteApiResponsePluginArgs[]
+>({
+  name: "DeleteApiResponsePlugin",
+  config: {},
+  handler: async (state, config, ...args): Promise<PipelineState> => {
+    // Error Response
+    if (state.status) {
+      return {
+        ...state,
+        apiResponse: {
+          status: state.status,
+        },
+      };
+    }
+
+    // Success Response
+    return {
+      ...state,
+      apiResponse: {
+        status: PluginStatusEntry.NO_CONTENT(),
+      },
+    };
+  },
+});
