0b09fb8b30bce16b942b339871a2a3c7933ad2bf
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:15:52 +0200

    [quarkloop] add workspace file apps/quarkloop/app/api/v1/user/planSubscriptions/[subscriptionId]/route.ts

---
+++ apps/quarkloop/app/api/v1/user/planSubscriptions/[subscriptionId]/route.ts
@@ -0,0 +1,11 @@
+import {
+  GET_GetPlanSubscriptionById,
+  PUT_UpdatePlanSubscription,
+  PATCH_DeletePlanSubscription,
+} from "@/engines/plan/planSubscription.api";
+
+export {
+  GET_GetPlanSubscriptionById as GET,
+  PUT_UpdatePlanSubscription as PUT,
+  PATCH_DeletePlanSubscription as PATCH,
+};
