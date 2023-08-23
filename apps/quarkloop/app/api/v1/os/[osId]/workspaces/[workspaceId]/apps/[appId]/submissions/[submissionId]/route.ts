0e45bd56b0d47ff8c5f06fe5ebcfb3c5516d2a02
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:15:52 +0200

    [quarkloop] add workspace file apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/submissions/[submissionId]/route.ts

---
+++ apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/submissions/[submissionId]/route.ts
@@ -0,0 +1,5 @@
+import { GET_GetAdminAppSubmissionById } from "@/engines/app-submission/app-submission.api";
+
+export const dynamic = "force-dynamic";
+
+export { GET_GetAdminAppSubmissionById as GET };
