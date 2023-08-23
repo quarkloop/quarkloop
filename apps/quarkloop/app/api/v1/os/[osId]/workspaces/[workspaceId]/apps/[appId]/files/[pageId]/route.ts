8344af333978bd5772a4975aaba6897eaa228c18
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:15:52 +0200

    [quarkloop] add workspace file apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/files/[pageId]/route.ts

---
+++ apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/files/[pageId]/route.ts
@@ -0,0 +1,11 @@
+import {
+  GET_GetAppFileSettingsById,
+  PUT_UpdateAppFileSettings,
+  PATCH_DeleteAppFileSettings,
+} from "@/engines/app-file/file-settings.api";
+
+export {
+  GET_GetAppFileSettingsById as GET,
+  PUT_UpdateAppFileSettings as PUT,
+  PATCH_DeleteAppFileSettings as PATCH,
+};
