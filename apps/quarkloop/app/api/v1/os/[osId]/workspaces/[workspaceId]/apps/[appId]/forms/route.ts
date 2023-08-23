92c05312187498033260d3daf793a64e8dd0861d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:15:52 +0200

    [quarkloop] add workspace file apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/forms/route.ts

---
+++ apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/forms/route.ts
@@ -0,0 +1,9 @@
+import {
+  GET_GetAppFormsSettingsByAppId,
+  POST_CreateAppFormSettings,
+} from "@/engines/app-form/form-settings.api";
+
+export {
+  GET_GetAppFormsSettingsByAppId as GET,
+  POST_CreateAppFormSettings as POST,
+};
