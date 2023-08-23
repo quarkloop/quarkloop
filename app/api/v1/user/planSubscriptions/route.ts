80a0f1fa5219399414ad35df6408221914e6b7e7
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:09:17 +0200

    [Workspace] remove file app/api/v1/user/planSubscriptions/route.ts

--- app/api/v1/user/planSubscriptions/route.ts
+++
@@ -1,9 +0,0 @@
-import {
-  GET_GetPlanSubscriptionByUserSession,
-  POST_CreatePlanSubscription,
-} from "@/engines/plan/planSubscription.api";
-
-export {
-  GET_GetPlanSubscriptionByUserSession as GET,
-  POST_CreatePlanSubscription as POST,
-};
