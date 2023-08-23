2f76f93cdccff9097a6b77bae5cebb989049b2c6
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:09:17 +0200

    [Workspace] remove file engines/app-conversation/index.ts

--- engines/app-conversation/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./conversation-settings.type";
-export * from "./conversation-settings.plugin";
-
-export * from "./conversation-data.type";
-export * from "./conversation-data.plugin";
