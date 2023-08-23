7be3d73f5b91dfce3b318ac967a44c412516ec86
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-08-23 02:15:52 +0200

    [quarkloop] add workspace file apps/quarkloop/engines/app-conversation/index.ts

---
+++ apps/quarkloop/engines/app-conversation/index.ts
@@ -0,0 +1,5 @@
+export * from "./conversation-settings.type";
+export * from "./conversation-settings.plugin";
+
+export * from "./conversation-data.type";
+export * from "./conversation-data.plugin";
