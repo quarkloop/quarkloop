20ae400e568a214821d8c69bac0a61d5e0fd2b23
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-01 12:35:07 +0200

    [Maintenance] add file: packages/types/index.ts

---
+++ packages/types/index.ts
@@ -0,0 +1,10 @@
+export * from "./src/api-response";
+
+export * from "./src/app";
+export * from "./src/app-instance";
+
+export * from "./src/thread-data";
+export * from "./src/thread-settings";
+export * from "./src/file-settings";
+export * from "./src/form-settings";
+export * from "./src/page-settings";
