6720780f03957e0531cd860e8e48e9c20f134ccc
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-01 12:16:47 +0200

    [Maintenance] delete file: apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/pages/route.ts

--- apps/quarkloop/app/api/v1/os/[osId]/workspaces/[workspaceId]/apps/[appId]/pages/route.ts
+++
@@ -1,9 +0,0 @@
-import {
-  GET_GetAppPagesSettingsByAppId,
-  POST_CreateAppPageSettings,
-} from "@/engines/app-page/page-settings.api";
-
-export {
-  GET_GetAppPagesSettingsByAppId as GET,
-  POST_CreateAppPageSettings as POST,
-};
