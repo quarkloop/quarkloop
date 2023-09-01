e18d41a1957386b277b8683f1173661cbac07729
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-01 12:16:47 +0200

    [Maintenance] delete file: apps/quarkloop/app/api/v1/os/route.ts

--- apps/quarkloop/app/api/v1/os/route.ts
+++
@@ -1,9 +0,0 @@
-import {
-    GET_GetOperatingSystemsByUserId,
-    POST_CreateOperatingSystem,
-} from "@/engines/os/os.api";
-
-export {
-    GET_GetOperatingSystemsByUserId as GET,
-    POST_CreateOperatingSystem as POST,
-};
