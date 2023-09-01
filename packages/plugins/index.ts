d56f3e3f056c6a58e389d0c8f4d980b0411377e3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-01 12:35:06 +0200

    [Maintenance] add file: packages/plugins/index.ts

---
+++ packages/plugins/index.ts
@@ -0,0 +1,11 @@
+export * from "./src/core";
+
+export * from "./src/app";
+export * from "./src/app-thread-data";
+export * from "./src/app-thread-settings";
+export * from "./src/app-file-settings";
+export * from "./src/app-form-settings";
+export * from "./src/app-page-settings";
+export * from "./src/app-instance";
+
+export * from "./src/pipeline";
