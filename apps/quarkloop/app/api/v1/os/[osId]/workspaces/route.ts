3f8762f6f29d2896cc9afdb46df50f8f0c1d971a
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-01 12:16:47 +0200

    [Maintenance] delete file: apps/quarkloop/app/api/v1/os/[osId]/workspaces/route.ts

--- apps/quarkloop/app/api/v1/os/[osId]/workspaces/route.ts
+++
@@ -1,9 +0,0 @@
-import {
-    GET_GetWorkspacesByOsId,
-    POST_CreateWorkspace,
-} from "@/engines/workspace/workspace.api";
-
-export {
-    GET_GetWorkspacesByOsId as GET,
-    POST_CreateWorkspace as POST,
-};
