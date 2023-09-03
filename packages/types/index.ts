85e4fa19c325dbaaf781e89e6ea4d875c7cdb505
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-03 18:50:13 +0200

    [quarkloop/types] update type: packages/types/index.ts

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -3,8 +3,11 @@ export * from "./src/api-response";
 export * from "./src/app";
 export * from "./src/app-instance";
 
-export * from "./src/thread-data";
+export * from "./src/thread";
 export * from "./src/thread-settings";
+export * from "./src/file";
 export * from "./src/file-settings";
+export * from "./src/form";
 export * from "./src/form-settings";
+export * from "./src/page";
 export * from "./src/page-settings";
