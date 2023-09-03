0bf98f8b048cb7b79e15cca4da2d16a8fe1c51e7
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-04 01:28:55 +0200

    [quarkloop/plugins] update plugin: packages/plugins/index.ts

--- packages/plugins/index.ts
+++ packages/plugins/index.ts
@@ -1,11 +1,14 @@
 export * from "./src/core";
+export * from "./src/pipeline";
 
 export * from "./src/app";
-export * from "./src/app-thread-data";
-export * from "./src/app-thread-settings";
-export * from "./src/app-file-settings";
-export * from "./src/app-form-settings";
-export * from "./src/app-page-settings";
 export * from "./src/app-instance";
 
-export * from "./src/pipeline";
+export * from "./src/app-thread";
+export * from "./src/app-thread-settings";
+export * from "./src/app-file";
+export * from "./src/app-file-settings";
+export * from "./src/app-form";
+export * from "./src/app-form-settings";
+export * from "./src/app-page";
+export * from "./src/app-page-settings";
