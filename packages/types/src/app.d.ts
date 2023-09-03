dcfab52f7928e0a27538ae7f039b3de7b490d4ac
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-03 18:50:13 +0200

    [quarkloop/types] update type: packages/types/src/app.d.ts

--- packages/types/src/app.d.ts
+++ packages/types/src/app.d.ts
@@ -1,7 +1,4 @@
-import {
-  App as PrismaApp,
-  UserRoleAssignmentMap as PrismaUserRoleAssignmentMap,
-} from "@quarkloop/prisma/types";
+import { App as PrismaApp } from "@quarkloop/prisma/types";
 import { ApiResponse } from "./api-response";
 
 /// Types
@@ -9,9 +6,6 @@ export interface App extends Partial<PrismaApp> {}
 
 export type AppPluginArgs =
   | GetAppByIdPluginArgs
-  | GetAppByPathPluginArgs
-  | GetAppsByOsIdPluginArgs
-  //| GetAppsByWorkspaceIdPluginArgs
   | CreateAppPluginArgs
   | UpdateAppPluginArgs
   | DeleteAppPluginArgs;
@@ -21,48 +15,13 @@ export interface GetAppById {}
 export interface GetAppByIdApiResponse extends ApiResponse {}
 export interface GetAppByIdApiArgs {
   id: string;
-  osId: string;
-  workspaceId?: string;
 }
 export interface GetAppByIdPluginArgs extends GetAppByIdApiArgs {}
-
-/// GetAppByPath
-export interface GetAppByPath {}
-export interface GetAppByPathApiResponse extends ApiResponse {}
-export interface GetAppByPathApiArgs {
-  osId: string;
-  workspaceId: string;
-  workspacePath?: string;
-  profilePath?: string;
-}
-export interface GetAppByPathPluginArgs extends GetAppByPathApiArgs {}
-
-/// GetAppsByOsId
-export interface GetAppsByOsId {}
-export interface GetAppsByOsIdApiResponse extends ApiResponse {}
-export interface GetAppsByOsIdApiArgs {
-  osId: string;
-  workspaceId?: string;
-}
-export interface GetAppsByOsIdPluginArgs extends GetAppsByOsIdApiArgs {}
-
-// /// GetAppsByWorkspaceId
-// export interface GetAppsByWorkspaceId {}
-// export interface GetAppsByWorkspaceIdApiResponse extends ApiResponse {}
-// export interface GetAppsByWorkspaceIdApiArgs {
-//   osId: string;
-//   workspaceId: string;
-// }
-// export interface GetAppsByWorkspaceIdPluginArgs
-//   extends GetAppsByWorkspaceIdApiArgs {}
 
 /// CreateApp
 export interface CreateApp {}
 export interface CreateAppApiResponse extends ApiResponse {}
-export interface CreateAppApiArgs extends App {
-  osId: string;
-  workspaceId: string;
-}
+export interface CreateAppApiArgs extends App {}
 export interface CreateAppPluginArgs extends CreateAppApiArgs {}
 
 /// UpdateApp
@@ -70,8 +29,6 @@ export interface UpdateApp {}
 export interface UpdateAppApiResponse extends ApiResponse {}
 export interface UpdateAppApiArgs extends App {
   id: string;
-  osId: string;
-  workspaceId: string;
 }
 export interface UpdateAppPluginArgs extends UpdateAppApiArgs {}
 
@@ -80,7 +37,5 @@ export interface DeleteApp {}
 export interface DeleteAppApiResponse extends ApiResponse {}
 export interface DeleteAppApiArgs {
   id: string;
-  osId: string;
-  workspaceId: string;
 }
 export interface DeleteAppPluginArgs extends DeleteAppApiArgs {}
