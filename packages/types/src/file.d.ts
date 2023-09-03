ef2ec476c025ef0b9b5f05fb2006c72f3909b4c9
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-03 18:31:01 +0200

    [quarkloop/types] add type: packages/types/src/file.d.ts

---
+++ packages/types/src/file.d.ts
@@ -0,0 +1,56 @@
+import { AppFile as PrismaAppFile } from "@quarkloop/prisma/types";
+import { ApiResponse } from "./api-response";
+
+/// Types
+export interface AppFile extends Partial<PrismaAppFile> {}
+
+export type AppFilePluginArgs =
+  | GetAppFileByIdPluginArgs
+  | GetAppFileByAppInstanceIdPluginArgs
+  | CreateAppFilePluginArgs
+  | UpdateAppFilePluginArgs
+  | DeleteAppFilePluginArgs;
+
+/// GetAppFileById
+export interface GetAppFileById {}
+export interface GetAppFileByIdApiResponse extends ApiResponse {}
+export interface GetAppFileByIdApiArgs {
+  id: number;
+  appInstanceId: string;
+}
+export interface GetAppFileByIdPluginArgs extends GetAppFileByIdApiArgs {}
+
+/// GetAppFileByAppInstanceId
+export interface GetAppFileByAppInstanceId {}
+export interface GetAppFileByAppInstanceIdApiResponse extends ApiResponse {}
+export interface GetAppFileByAppInstanceIdApiArgs {
+  appInstanceId: string;
+}
+export interface GetAppFileByAppInstanceIdPluginArgs
+  extends GetAppFileByAppInstanceIdApiArgs {}
+
+/// CreateAppFile
+export interface CreateAppFile {}
+export interface CreateAppFileApiResponse extends ApiResponse {}
+export interface CreateAppFileApiArgs extends Partial<AppFile> {
+  appInstanceId: string;
+}
+export interface CreateAppFilePluginArgs extends CreateAppFileApiArgs {}
+
+/// UpdateAppFile
+export interface UpdateAppFile {}
+export interface UpdateAppFileApiResponse extends ApiResponse {}
+export interface UpdateAppFileApiArgs extends Partial<AppFile> {
+  id: number;
+  appInstanceId: string;
+}
+export interface UpdateAppFilePluginArgs extends UpdateAppFileApiArgs {}
+
+/// DeleteAppFile
+export interface DeleteAppFile {}
+export interface DeleteAppFileApiResponse extends ApiResponse {}
+export interface DeleteAppFileApiArgs {
+  id: number;
+  appInstanceId: string;
+}
+export interface DeleteAppFilePluginArgs extends DeleteAppFileApiArgs {}
