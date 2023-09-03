7bdc3e55785286c70362fd09b98195e907ffc402
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-03 14:31:11 +0200

    fix route handler: apps/db-server/app/api/v1/tables/appInstance/route.ts

--- apps/db-server/app/api/v1/tables/appInstance/route.ts
+++ apps/db-server/app/api/v1/tables/appInstance/route.ts
@@ -23,10 +23,8 @@ import {
   UpdateAppInstancePlugin,
 } from "@quarkloop/plugins";
 
-export async function GET_GetAdminAppInstanceById(
-  request: Request,
-  { params }: { params: any }
-) {
+// GetAdminAppInstanceById
+export async function GET(request: Request, { params }: { params: any }) {
   const { osId, workspaceId, appId, id } = params;
 
   const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
@@ -49,60 +47,60 @@ export async function GET_GetAdminAppInstanceById(
   return NextResponse.json(finalState.apiResponse);
 }
 
-export async function GET_GetAppInstanceById(
-  request: Request,
-  { params }: { params: any }
-) {
-  const { userId, appId, id } = params;
+// // GetAppInstanceById
+// export async function GET(
+//   request: Request,
+//   { params }: { params: any }
+// ) {
+//   const { userId, appId, id } = params;
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
+//   const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+//     initialState: {},
+//   });
 
-  const finalState = await pipeline
-    .use(GetAppInstanceByIdPlugin)
-    .use(GetApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appInstance: {
-        userId: userId,
-        appId: appId,
-        id: id,
-      } as GetAppInstanceByIdPluginArgs,
-    });
+//   const finalState = await pipeline
+//     .use(GetAppInstanceByIdPlugin)
+//     .use(GetApiResponsePlugin)
+//     .onError(DefaultErrorHandler)
+//     .execute({
+//       appInstance: {
+//         userId: userId,
+//         appId: appId,
+//         id: id,
+//       } as GetAppInstanceByIdPluginArgs,
+//     });
 
-  return NextResponse.json(finalState.apiResponse);
-}
+//   return NextResponse.json(finalState.apiResponse);
+// }
 
-export async function GET_GetAppInstancesByAppId(
-  request: Request,
-  { params }: { params: any }
-) {
-  const { searchParams } = new URL(request.url);
+// // GetAppInstancesByAppId
+// export async function GET(
+//   request: Request,
+//   { params }: { params: any }
+// ) {
+//   const { searchParams } = new URL(request.url);
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
+//   const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+//     initialState: {},
+//   });
 
-  const finalState = await pipeline
-    .use(GetAppInstancesByAppIdPlugin)
-    .use(GetApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appInstance: {
-        osId: searchParams.get("osId"),
-        workspaceId: searchParams.get("workspaceId"),
-        appId: searchParams.get("appId"),
-      } as GetAppInstancesByAppIdPluginArgs,
-    });
+//   const finalState = await pipeline
+//     .use(GetAppInstancesByAppIdPlugin)
+//     .use(GetApiResponsePlugin)
+//     .onError(DefaultErrorHandler)
+//     .execute({
+//       appInstance: {
+//         osId: searchParams.get("osId"),
+//         workspaceId: searchParams.get("workspaceId"),
+//         appId: searchParams.get("appId"),
+//       } as GetAppInstancesByAppIdPluginArgs,
+//     });
 
-  return NextResponse.json(finalState.apiResponse);
-}
+//   return NextResponse.json(finalState.apiResponse);
+// }
 
-export async function POST_CreateAppInstance(
-  request: Request,
-  { params }: { params: any }
-) {
+// CreateAppInstance
+export async function POST(request: Request, { params }: { params: any }) {
   const body = await request.json();
 
   const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
@@ -120,10 +118,8 @@ export async function POST_CreateAppInstance(
   return NextResponse.json(finalState.apiResponse);
 }
 
-export async function PUT_UpdateAppInstance(
-  request: Request,
-  { params }: { params: any }
-) {
+// UpdateAppInstance
+export async function PUT(request: Request, { params }: { params: any }) {
   const { submissionId } = params;
   const body = await request.json();
 
