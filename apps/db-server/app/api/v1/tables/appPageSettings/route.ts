f82479537836a0469d10320ffa104f0bea67b03b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-03 14:51:16 +0200

    Change PATCH http method to DELETE : apps/db-server/app/api/v1/tables/appPageSettings/route.ts

--- apps/db-server/app/api/v1/tables/appPageSettings/route.ts
+++ apps/db-server/app/api/v1/tables/appPageSettings/route.ts
@@ -121,7 +121,7 @@ export async function PUT(request: Request, { params }: { params: any }) {
 }
 
 // DeleteAppPageSettings
-export async function PATCH(request: Request, { params }: { params: any }) {
+export async function DELETE(request: Request, { params }: { params: any }) {
   const { osId, workspaceId, appId, pageId } = params;
 
   const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
