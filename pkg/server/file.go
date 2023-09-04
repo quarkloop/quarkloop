8df7d3e406be75d2269019d47e49c7ca871cf278
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-04 16:48:52 +0200

    [quarkloop/api-server] Add go project file: pkg/server/file.go

---
+++ pkg/server/file.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) HandleGetFile(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (s *Server) HandleCreateFile(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{"status": "Created"})
+}
+
+func (s *Server) HandleUpdateFile(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (s *Server) HandleDeleteFile(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{"status": "NoContent"})
+}
