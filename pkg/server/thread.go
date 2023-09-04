db7d4cb1f836d4be20f9581c45c9dc73d7c6c32e
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-04 16:48:52 +0200

    [quarkloop/api-server] Add go project file: pkg/server/thread.go

---
+++ pkg/server/thread.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) HandleGetThread(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (s *Server) HandleCreateThread(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{"status": "Created"})
+}
+
+func (s *Server) HandleUpdateThread(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (s *Server) HandleDeleteThread(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{"status": "NoContent"})
+}
