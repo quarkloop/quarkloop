71ff2b1702414b6d4fff04a5b22fbfc820468a8a
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-04 19:04:52 +0200

    [quarkloop/api-server] Add GET app handler: pkg/server/form.go

--- pkg/server/form.go
+++ pkg/server/form.go
@@ -10,6 +10,10 @@ func (s *Server) HandleGetForm(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "OK"})
 }
 
+func (s *Server) HandleGetForms(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
 func (s *Server) HandleCreateForm(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"status": "Created"})
 }
