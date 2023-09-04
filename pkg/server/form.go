b17f71363b3d32a86157fc6ce92e91750ed2f847
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-04 16:48:52 +0200

    [quarkloop/api-server] Add go project file: pkg/server/form.go

---
+++ pkg/server/form.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) HandleGetForm(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (s *Server) HandleCreateForm(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{"status": "Created"})
+}
+
+func (s *Server) HandleUpdateForm(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (s *Server) HandleDeleteForm(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{"status": "NoContent"})
+}
