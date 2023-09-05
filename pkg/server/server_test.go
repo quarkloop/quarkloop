45e56f9efb14d4b3b7ff32b7d93fd64ae66653bb
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-05 23:51:23 +0200

    [quarkloop/api-server] add server test

---
+++ pkg/server/server_test.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewTestRouter() *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.RedirectFixedPath = false
+	router.RedirectTrailingSlash = false
+	return router
+}
+
+func PerformRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	return w
+}
+
+func PerformRequestWithBody(r http.Handler, method, path string, body any) *httptest.ResponseRecorder {
+	payloadBuf := new(bytes.Buffer)
+	json.NewEncoder(payloadBuf).Encode(body)
+
+	req, _ := http.NewRequest(method, path, payloadBuf)
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	return w
+}
+
+func SetUpServer() *Server {
+	router := NewTestRouter()
+
+	s := Server{
+		Status: 0,
+		Router: router,
+	}
+	s.BindHandlers()
+
+	return &s
+}
