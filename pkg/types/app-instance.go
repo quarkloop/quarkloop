cb5d24d3e75b624734b716175e1eadce222221d3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-07 00:51:34 +0200

    Add app instance type

---
+++ pkg/types/app-instance.go
@@ -0,0 +1,10 @@
+package types
+
+import "time"
+
+type AppInstance struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
