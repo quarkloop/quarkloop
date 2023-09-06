f4f132b2b4e89015455d5f57510743ef43f92ca9
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-06 18:55:04 +0200

    [quarkloop/api-server] Update api handlers: pkg/server/app.go

--- pkg/server/app.go
+++ pkg/server/app.go
@@ -3,68 +3,241 @@ package server
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/db"
 	"github.com/quarkloop/quarkloop/pkg/types"
 )
 
+type AppRequestPayload struct {
+	App types.App `json:"app"`
+}
+
+type AppResponsePayload struct {
+	Status       int         `json:"status,omitempty"`
+	StatusString string      `json:"statusText,omitempty"`
+	Error        error       `json:"error,omitempty"`
+	ErrorString  string      `json:"errorString,omitempty"`
+	App          interface{} `json:"app,omitempty"`
+}
+
 func (s *Server) HandleGetApp(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+	payload := AppResponsePayload{
+		Status:       http.StatusOK,
+		StatusString: "OK",
+		App:          types.App{Name: "UploadFileToS3"},
+	}
+
+	c.JSON(http.StatusOK, payload)
 }
 
 func (s *Server) HandleGetApps(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+	payload := AppResponsePayload{
+		Status:       http.StatusOK,
+		StatusString: "OK",
+		App:          types.App{Name: "UploadFileToS3"},
+	}
+
+	c.JSON(http.StatusOK, payload)
 }
 
 func (s *Server) HandleCreateApp(c *gin.Context) {
-	app := &types.App{}
+	payload := &AppRequestPayload{}
 
-	err := c.BindJSON(app)
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+	if err := c.BindJSON(payload); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[BindJSON] %s", err.Error()),
+		})
 		return
 	}
 
-	marshalled, err := json.Marshal(app)
+	marshalled, err := json.Marshal(payload.App)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"status": "Marshalling failed"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Marshal] %s", err.Error()),
+		})
 		return
 	}
 
-	req, err := http.NewRequest("POST", "http://localhost:3001/db", bytes.NewBuffer(marshalled))
+	req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/tables/app", bytes.NewBuffer(marshalled))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusInternalServerError,
+			StatusString: "InternalServerError",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[NewRequest] %s", err.Error()),
+		})
+		return
+	}
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"status": "InternalServerError"})
-		return
-	}
 
 	client := http.Client{Timeout: 10 * time.Second}
 	res, err := client.Do(req)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"status": "Failed to send request"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Client] %s", err.Error()),
+		})
 		return
 	}
 	defer res.Body.Close()
 
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"status": "Failed to read body"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[BufferReader] %s", err.Error()),
+		})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{
-		"status":  "Created",
-		"message": string(resBody),
-	})
+	databasePayload := db.DatabaseResponsePayload{}
+	if err := json.Unmarshal(resBody, &databasePayload); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Unmarshal] %s", err.Error()),
+		})
+		return
+	}
+
+	resPayload := AppResponsePayload{
+		Status:       http.StatusCreated,
+		StatusString: "Created",
+		App:          databasePayload.Database.App.Records,
+	}
+
+	c.JSON(http.StatusCreated, resPayload)
 }
 
 func (s *Server) HandleUpdateApp(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+	appId := c.Param("appId")
+
+	payload := &AppRequestPayload{}
+	payload.App.Id = appId
+
+	if err := c.BindJSON(payload); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[BindJSON] %s", err.Error()),
+		})
+		return
+	}
+
+	marshalled, err := json.Marshal(payload.App)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Marshal] %s", err.Error()),
+		})
+		return
+	}
+
+	req, err := http.NewRequest("PUT", "http://localhost:3000/api/v1/tables/app", bytes.NewBuffer(marshalled))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusInternalServerError,
+			StatusString: "InternalServerError",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[NewRequest] %s", err.Error()),
+		})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := http.Client{Timeout: 10 * time.Second}
+	res, err := client.Do(req)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Client] %s", err.Error()),
+		})
+		return
+	}
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[BufferReader] %s", err.Error()),
+		})
+		return
+	}
+
+	databasePayload := db.DatabaseResponsePayload{}
+	if err := json.Unmarshal(resBody, &databasePayload); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Unmarshal] %s", err.Error()),
+		})
+		return
+	}
+
+	resPayload := AppResponsePayload{
+		Status:       http.StatusOK,
+		StatusString: "OK",
+		App:          databasePayload.Database.App.Records,
+	}
+
+	c.JSON(http.StatusOK, resPayload)
 }
 
 func (s *Server) HandleDeleteApp(c *gin.Context) {
-	c.JSON(http.StatusNoContent, gin.H{"status": "NoContent"})
+	appId := c.Param("appId")
+
+	req, err := http.NewRequest("DELETE", "http://localhost:3000/api/v1/tables/app", nil)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusInternalServerError,
+			StatusString: "InternalServerError",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[NewRequest] %s", err.Error()),
+		})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	q := req.URL.Query()
+	q.Add("id", appId)
+	req.URL.RawQuery = q.Encode()
+
+	client := http.Client{Timeout: 10 * time.Second}
+	res, err := client.Do(req)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Client] %s", err.Error()),
+		})
+		return
+	}
+	defer res.Body.Close()
+
+	c.JSON(res.StatusCode, nil)
 }
