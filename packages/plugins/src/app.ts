464bb8402bef23d344008ff371dfd1402d3dff29
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-06 18:58:17 +0200

    [quarkloop/db-server] update api handlers and plugins: packages/plugins/src/app.ts

--- packages/plugins/src/app.ts
+++ packages/plugins/src/app.ts
@@ -1,177 +1,202 @@
-import { prisma } from "@quarkloop/prisma/client";
+import { Prisma, prisma } from "@quarkloop/prisma/client";
 
 import { generateId } from "@quarkloop/lib";
 import { createPlugin } from "@quarkloop/plugin";
 import { PipelineArgs, PipelineState, PluginStatusEntry } from "./pipeline";
 
 import {
-  GetAppByIdPluginArgs,
-  CreateAppPluginArgs,
-  UpdateAppPluginArgs,
-  DeleteAppPluginArgs,
+    GetAppByIdPluginArgs,
+    CreateAppPluginArgs,
+    UpdateAppPluginArgs,
+    DeleteAppPluginArgs,
 } from "@quarkloop/types";
 
 /// GetAppById Plugin
 export const GetAppByIdPlugin = createPlugin<PipelineState, PipelineArgs[]>({
-  name: "GetAppByIdPlugin",
-  config: {},
-  handler: async (state, config, ...args): Promise<PipelineState> => {
-    if (state.status) {
-      return state;
-    }
+    name: "GetAppByIdPlugin",
+    config: {},
+    handler: async (state, config, ...args): Promise<PipelineState> => {
+        if (state.status) {
+            return state;
+        }
 
-    if (args.length === 0 || args[0].app == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.INTERNAL_SERVER_ERROR("[GetAppByIdPlugin]"),
-      };
-    }
+        if (args.length === 0 || args[0].app == null) {
+            return {
+                ...state,
+                status: PluginStatusEntry.INTERNAL_SERVER_ERROR(
+                    "[GetAppByIdPlugin]"
+                ),
+            };
+        }
 
-    const getArgs = args[0].app as GetAppByIdPluginArgs;
+        const getArgs = args[0].app as GetAppByIdPluginArgs;
 
-    const record = await prisma.app.findUnique({
-      where: {
-        id: getArgs.id,
-      },
-    });
+        const record = await prisma.app.findUnique({
+            where: {
+                id: getArgs.id,
+            },
+        });
 
-    if (record == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.NOT_FOUND("[GetAppByIdPlugin]"),
-      };
-    }
+        if (record == null) {
+            return {
+                ...state,
+                status: PluginStatusEntry.NOT_FOUND("[GetAppByIdPlugin]"),
+            };
+        }
 
-    return {
-      ...state,
-      database: {
-        ...state.database,
-        app: {
-          records: record,
-          totalRecords: 1,
-        },
-      },
-    };
-  },
+        return {
+            ...state,
+            database: {
+                ...state.database,
+                app: {
+                    records: record,
+                    totalRecords: 1,
+                },
+            },
+        };
+    },
 });
 
 /// CreateApp Plugin
 export const CreateAppPlugin = createPlugin<PipelineState, PipelineArgs[]>({
-  name: "CreateAppPlugin",
-  config: {},
-  handler: async (state, config, ...args): Promise<PipelineState> => {
-    if (state.status) {
-      return state;
-    }
+    name: "CreateAppPlugin",
+    config: {},
+    handler: async (state, config, ...args): Promise<PipelineState> => {
+        if (state.status) {
+            return state;
+        }
 
-    if (args.length === 0 || args[0].app == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.INTERNAL_SERVER_ERROR("[CreateAppPlugin]"),
-      };
-    }
+        if (args.length === 0 || args[0].app == null) {
+            return {
+                ...state,
+                status: PluginStatusEntry.INTERNAL_SERVER_ERROR(
+                    "[CreateAppPlugin]"
+                ),
+            };
+        }
 
-    const createArgs = args[0].app as CreateAppPluginArgs;
+        const createArgs = args[0].app as CreateAppPluginArgs;
 
-    const appId = generateId();
+        const appId = generateId();
 
-    const record = await prisma.app.create({
-      data: {
-        id: appId,
-        name: createArgs.name!,
-        type: createArgs.type!,
-        icon: createArgs.icon,
-        metadata: createArgs.metadata!,
-        updatedAt: new Date(),
-      },
-    });
+        const record = await prisma.app.create({
+            data: {
+                id: appId,
+                name: createArgs.name!,
+                type: createArgs.type!,
+                icon: createArgs.icon,
+                metadata: createArgs.metadata!,
+                updatedAt: new Date(),
+            },
+        });
 
-    return {
-      ...state,
-      database: {
-        ...state.database,
-        app: {
-          records: record,
-          totalRecords: 1,
-        },
-      },
-    };
-  },
+        return {
+            ...state,
+            database: {
+                ...state.database,
+                app: {
+                    records: record,
+                    totalRecords: 1,
+                },
+            },
+        };
+    },
 });
 
 /// UpdateApp Plugin
 export const UpdateAppPlugin = createPlugin<PipelineState, PipelineArgs[]>({
-  name: "UpdateAppPlugin",
-  config: {},
-  handler: async (state, config, ...args): Promise<PipelineState> => {
-    if (state.status) {
-      return state;
-    }
+    name: "UpdateAppPlugin",
+    config: {},
+    handler: async (state, config, ...args): Promise<PipelineState> => {
+        if (state.status) {
+            return state;
+        }
 
-    if (args.length === 0 || args[0].app == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.INTERNAL_SERVER_ERROR("[UpdateAppPlugin]"),
-      };
-    }
+        if (args.length === 0 || args[0].app == null) {
+            return {
+                ...state,
+                status: PluginStatusEntry.INTERNAL_SERVER_ERROR(
+                    "[UpdateAppPlugin]"
+                ),
+            };
+        }
 
-    const updateArgs = args[0].app as UpdateAppPluginArgs;
+        const updateArgs = args[0].app as UpdateAppPluginArgs;
 
-    const record = await prisma.app.update({
-      where: {
-        id: updateArgs.id,
-      },
-      data: {
-        // once an app created, only name, metadata and icon can be updated.
-        ...(updateArgs.name && { name: updateArgs.name }),
-        ...(updateArgs.icon && { icon: updateArgs.icon }),
-        ...(updateArgs.metadata && { metadata: updateArgs.metadata }),
-        updatedAt: new Date(),
-      },
-    });
+        const record = await prisma.app.update({
+            where: {
+                id: updateArgs.id,
+            },
+            data: {
+                // once an app created, only name, metadata and icon can be updated.
+                ...(updateArgs.name && { name: updateArgs.name }),
+                ...(updateArgs.icon && { icon: updateArgs.icon }),
+                ...(updateArgs.metadata && { metadata: updateArgs.metadata }),
+                updatedAt: new Date(),
+            },
+        });
 
-    if (record == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.NOT_FOUND("[UpdateAppPlugin]"),
-      };
-    }
+        if (record == null) {
+            return {
+                ...state,
+                status: PluginStatusEntry.NOT_FOUND("[UpdateAppPlugin]"),
+            };
+        }
 
-    return state;
-  },
+        return {
+            ...state,
+            database: {
+                ...state.database,
+                app: {
+                    records: record,
+                    totalRecords: 1,
+                },
+            },
+        };
+    },
 });
 
 /// DeleteApp Plugin
 export const DeleteAppPlugin = createPlugin<PipelineState, PipelineArgs[]>({
-  name: "DeleteAppPlugin",
-  config: {},
-  handler: async (state, config, ...args): Promise<PipelineState> => {
-    if (state.status) {
-      return state;
-    }
+    name: "DeleteAppPlugin",
+    config: {},
+    handler: async (state, config, ...args): Promise<PipelineState> => {
+        if (state.status) {
+            return state;
+        }
 
-    if (args.length === 0 || args[0].app == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.INTERNAL_SERVER_ERROR("[DeleteAppPlugin]"),
-      };
-    }
+        if (args.length === 0 || args[0].app == null) {
+            return {
+                ...state,
+                status: PluginStatusEntry.INTERNAL_SERVER_ERROR(
+                    "[DeleteAppPlugin]"
+                ),
+            };
+        }
 
-    const deleteArgs = args[0].app as DeleteAppPluginArgs;
+        const deleteArgs = args[0].app as DeleteAppPluginArgs;
 
-    const record = await prisma.app.delete({
-      where: {
-        id: deleteArgs.id,
-      },
-    });
+        try {
+            const record = await prisma.app.delete({
+                where: {
+                    id: deleteArgs.id,
+                },
+            });
+        } catch (e) {
+            // https://github.com/prisma/prisma/issues/12128
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                if (e.code === "P2025") {
+                }
+            }
 
-    if (record == null) {
-      return {
-        ...state,
-        status: PluginStatusEntry.NOT_FOUND("[DeleteAppPlugin]"),
-      };
-    }
+            return {
+                ...state,
+                status: PluginStatusEntry.NOT_FOUND(
+                    "[DeleteAppPlugin] Record to delete does not exist."
+                ),
+            };
+        }
 
-    return state;
-  },
+        return state;
+    },
 });
