f331e6b753c124014708cebd5e560c1ed9e1677f
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-07 13:41:21 +0200

    Add AppId to AppInstance

--- pkg/types/app-instance.go
+++ pkg/types/app-instance.go
@@ -4,6 +4,7 @@ import "time"
 
 type AppInstance struct {
 	Id        string    `json:"id"`
+	AppId     string    `json:"appId"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
