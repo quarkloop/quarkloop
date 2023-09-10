3f0fa64ee0d9ac1088d318baf66026a5d60f7103
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-11 01:05:24 +0200

    Add op api handler

---
+++ pkg/server/op.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/ops"
+)
+
+type OpCallRequestPayload struct {
+	AppID      string `json:"appId,omitempty"`
+	InstanceID string `json:"instanceId,omitempty"`
+	Call       struct {
+		Name string          `json:"name"`
+		Args json.RawMessage `json:"args"`
+	} `json:"call"`
+}
+
+type OpCallResponsePayload struct {
+	Status       int         `json:"status,omitempty"`
+	StatusString string      `json:"statusText,omitempty"`
+	Error        error       `json:"error,omitempty"`
+	ErrorString  string      `json:"errorString,omitempty"`
+	AppInstance  interface{} `json:"appInstance,omitempty"`
+}
+
+func (s *Server) HandleCallOp(c *gin.Context) {
+	payload := &OpCallRequestPayload{}
+	payload.AppID = c.Param("appId")
+	payload.InstanceID = c.Param("instanceId")
+
+	if err := c.BindJSON(payload); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, AppResponsePayload{
+			Status:       http.StatusBadRequest,
+			StatusString: "BadRequest",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[BindJSON] %s", err.Error()),
+		})
+		return
+	}
+
+	catalog, err := ops.FindOp(payload.AppID, payload.InstanceID, payload.Call.Name, payload.Call.Args)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, AppResponsePayload{
+			Status:       http.StatusNotFound,
+			StatusString: "NotFound",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[FindOp] %s", err.Error()),
+		})
+		return
+	}
+
+	res, err := catalog.Exec()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusExpectationFailed, AppResponsePayload{
+			Status:       http.StatusExpectationFailed,
+			StatusString: "ExpectationFailed",
+			Error:        err,
+			ErrorString:  fmt.Sprintf("[Call] %s", err.Error()),
+		})
+		return
+	}
+
+	fmt.Printf("******** %+v\n", res)
+
+	c.JSON(http.StatusOK, gin.H{"status": res})
+}
