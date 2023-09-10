6645b880d8b6730a83f3f0603d17a627df212b9e
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-10 18:03:29 +0200

    [api-server] Add db-server file api: pkg/db/api/file.go

---
+++ pkg/db/api/file.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"encoding/json"
+
+	"github.com/quarkloop/quarkloop/pkg/db"
+	"github.com/quarkloop/quarkloop/pkg/db/model"
+)
+
+func GetFileById() (interface{}, error) {
+	payload := model.File{
+		InstanceId: "",
+		Enable:     false,
+	}
+
+	marshalled, err := json.Marshal(&payload)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := db.HttpClientInstance.Get("http://localhost:3000/api/v1/tables/appFile", marshalled, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
