413c9c7355f021741e3376afddb929a9f7475453
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-11 22:46:32 +0200

    [api-server] Update types, models, and client in db package: pkg/db/model/file.go

--- pkg/db/model/file.go
+++ pkg/db/model/file.go
@@ -1,8 +1,13 @@
 package model
 
+import "github.com/quarkloop/quarkloop/pkg/db"
+
 type File struct {
-	AppId      string `json:"appId"`
-	InstanceId string `json:"instanceId"`
-	Id         string `json:"id,omitempty"`
-	Enable     bool   `json:"enable"`
+	Status   db.DatabaseResponseStatus `json:"status"`
+	Database struct {
+		AppId      string `json:"appId"`
+		InstanceId string `json:"instanceId"`
+		Id         string `json:"id,omitempty"`
+		Enable     bool   `json:"enable"`
+	} `json:"database,omitempty"`
 }
