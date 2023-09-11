48f71cc5793d782f7424949c893955933909c181
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-11 22:46:32 +0200

    [api-server] Update types, models, and client in db package: pkg/db/api/file.go

--- pkg/db/api/file.go
+++ pkg/db/api/file.go
@@ -1,19 +1,41 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
+	"net/http"
 	"net/url"
 
 	"github.com/quarkloop/quarkloop/pkg/db"
+	"github.com/quarkloop/quarkloop/pkg/db/client"
+	"github.com/quarkloop/quarkloop/pkg/db/model"
 )
 
-func GetFileById(appId, instanceId, fileId string) (interface{}, error) {
+func GetFileById(appId, instanceId, fileId string) (*model.File, error) {
 	params := url.Values{}
 	params.Add("id", fileId)
 
-	res, err := db.HttpClientInstance.Get("http://localhost:3000/api/v1/tables/appFile", &params)
+	res, err := client.DatabaseClient.Get("http://localhost:3000/api/v1/tables/appFile", &params)
 	if err != nil {
 		return nil, err
 	}
+	defer res.Body.Close()
 
-	return res, nil
+	if res.StatusCode == http.StatusOK {
+		var file model.File
+
+		var payload db.DatabaseResponsePayload
+		decodeErr := json.NewDecoder(res.Body).Decode(&payload)
+		if decodeErr != nil {
+			return nil, decodeErr
+		}
+
+		val, ok := payload.Database.File.Records.(model.File)
+		if ok {
+			file = val
+			return &file, nil
+		}
+	}
+
+	return nil, errors.New("file not found")
 }
