589e3a10df60d1fa8e235f1a2ebe128f1020e088
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-12 15:38:54 +0200

    [api-server] Update db package: pkg/db/model/file.go

--- pkg/db/model/file.go
+++ pkg/db/model/file.go
@@ -1,13 +1,12 @@
 package model
 
-import "github.com/quarkloop/quarkloop/pkg/db"
+import "time"
 
 type File struct {
-	Status   db.DatabaseResponseStatus `json:"status"`
-	Database struct {
-		AppId      string `json:"appId"`
-		InstanceId string `json:"instanceId"`
-		Id         string `json:"id,omitempty"`
-		Enable     bool   `json:"enable"`
-	} `json:"database,omitempty"`
+	AppId      string    `json:"appId,omitempty"`
+	InstanceId string    `json:"appInstanceId,omitempty"`
+	Id         string    `json:"id,omitempty"`
+	Enable     *bool     `json:"enable,omitempty"`
+	CreatedAt  time.Time `json:"createdAt,omitempty"`
+	UpdatedAt  time.Time `json:"updatedAt,omitempty"`
 }
