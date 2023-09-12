7d4eec4e427f71670c64f03e58141ed2f2bde1d9
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-12 15:36:46 +0200

    [db-server] Update api route status code: apps/db-server/app/api/v1/tables/appThread/route.ts

--- apps/db-server/app/api/v1/tables/appThread/route.ts
+++ apps/db-server/app/api/v1/tables/appThread/route.ts
@@ -1,136 +1,148 @@
 import { NextResponse } from "next/server";
 
 import {
-  CreateAppThreadPluginArgs,
-  DeleteAppThreadPluginArgs,
-  GetAppThreadByIdPluginArgs,
-  GetAppThreadByAppInstanceIdPluginArgs,
-  UpdateAppThreadPluginArgs,
+    CreateAppThreadPluginArgs,
+    DeleteAppThreadPluginArgs,
+    GetAppThreadByIdPluginArgs,
+    GetAppThreadByAppInstanceIdPluginArgs,
+    UpdateAppThreadPluginArgs,
 } from "@quarkloop/types";
 import { createPipeline } from "@quarkloop/plugin";
 import {
-  PipelineState,
-  PipelineArgs,
-  DefaultErrorHandler,
-  GetApiResponsePlugin,
-  CreateApiResponsePlugin,
-  UpdateApiResponsePlugin,
-  DeleteApiResponsePlugin,
-  GetAppThreadByIdPlugin,
-  GetAppThreadByAppInstanceIdPlugin,
-  CreateAppThreadPlugin,
-  UpdateAppThreadPlugin,
-  DeleteAppThreadPlugin,
+    PipelineState,
+    PipelineArgs,
+    DefaultErrorHandler,
+    GetApiResponsePlugin,
+    CreateApiResponsePlugin,
+    UpdateApiResponsePlugin,
+    DeleteApiResponsePlugin,
+    GetAppThreadByIdPlugin,
+    GetAppThreadByAppInstanceIdPlugin,
+    CreateAppThreadPlugin,
+    UpdateAppThreadPlugin,
+    DeleteAppThreadPlugin,
 } from "@quarkloop/plugins";
 
 // GetAppThreadById
 // GetAppThreadByAppInstanceId
 export async function GET(request: Request, { params }: { params: any }) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  const appInstanceId = searchParams.get("appInstanceId");
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const appInstanceId = searchParams.get("appInstanceId");
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
+    });
 
-  if (id) {
-    const finalState = await pipeline
-      .use(GetAppThreadByIdPlugin)
-      .use(GetApiResponsePlugin)
-      .onError(DefaultErrorHandler)
-      .execute({
-        appThread: {
-          id: id,
-        } as GetAppThreadByIdPluginArgs,
-      });
+    if (id) {
+        const finalState = await pipeline
+            .use(GetAppThreadByIdPlugin)
+            .use(GetApiResponsePlugin)
+            .onError(DefaultErrorHandler)
+            .execute({
+                appThread: {
+                    id: id,
+                } as GetAppThreadByIdPluginArgs,
+            });
 
-    return NextResponse.json(finalState.apiResponse);
-  }
+        return NextResponse.json(finalState.apiResponse, {
+            status:
+                (finalState.apiResponse?.status.statusCode as number) ?? 500,
+        });
+    }
 
-  if (appInstanceId) {
-    const finalState = await pipeline
-      .use(GetAppThreadByAppInstanceIdPlugin)
-      .use(GetApiResponsePlugin)
-      .onError(DefaultErrorHandler)
-      .execute({
-        appThread: {
-          appInstanceId: appInstanceId,
-        } as GetAppThreadByAppInstanceIdPluginArgs,
-      });
+    if (appInstanceId) {
+        const finalState = await pipeline
+            .use(GetAppThreadByAppInstanceIdPlugin)
+            .use(GetApiResponsePlugin)
+            .onError(DefaultErrorHandler)
+            .execute({
+                appThread: {
+                    appInstanceId: appInstanceId,
+                } as GetAppThreadByAppInstanceIdPluginArgs,
+            });
 
-    return NextResponse.json(finalState.apiResponse);
-  }
+        return NextResponse.json(finalState.apiResponse, {
+            status:
+                (finalState.apiResponse?.status.statusCode as number) ?? 500,
+        });
+    }
 
-  return NextResponse.json({ status: "Bad request" }, { status: 400 });
+    return NextResponse.json({ status: "Bad request" }, { status: 400 });
 }
 
 // CreateAppThread
 export async function POST(request: Request, { params }: { params: any }) {
-  const body = await request.json();
+    const body = await request.json();
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
-
-  const finalState = await pipeline
-    .use(CreateAppThreadPlugin)
-    .use(CreateApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appThread: {
-        ...body,
-      } as CreateAppThreadPluginArgs,
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
     });
 
-  return NextResponse.json(finalState.apiResponse);
+    const finalState = await pipeline
+        .use(CreateAppThreadPlugin)
+        .use(CreateApiResponsePlugin)
+        .onError(DefaultErrorHandler)
+        .execute({
+            appThread: {
+                ...body,
+            } as CreateAppThreadPluginArgs,
+        });
+
+    return NextResponse.json(finalState.apiResponse, {
+        status: (finalState.apiResponse?.status.statusCode as number) ?? 500,
+    });
 }
 
 // UpdateAppThread
 export async function PUT(request: Request, { params }: { params: any }) {
-  const body = await request.json();
+    const body = await request.json();
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
-
-  const finalState = await pipeline
-    .use(UpdateAppThreadPlugin)
-    .use(UpdateApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appThread: {
-        ...body,
-      } as UpdateAppThreadPluginArgs,
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
     });
 
-  return NextResponse.json(finalState.apiResponse);
+    const finalState = await pipeline
+        .use(UpdateAppThreadPlugin)
+        .use(UpdateApiResponsePlugin)
+        .onError(DefaultErrorHandler)
+        .execute({
+            appThread: {
+                ...body,
+            } as UpdateAppThreadPluginArgs,
+        });
+
+    return NextResponse.json(finalState.apiResponse, {
+        status: (finalState.apiResponse?.status.statusCode as number) ?? 500,
+    });
 }
 
 // DeleteAppThread
 export async function DELETE(request: Request, { params }: { params: any }) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  const appInstanceId = searchParams.get("appInstanceId");
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const appInstanceId = searchParams.get("appInstanceId");
 
-  if (id == null && appInstanceId == null) {
-    return NextResponse.json({ status: "Bad request" }, { status: 400 });
-  }
+    if (id == null && appInstanceId == null) {
+        return NextResponse.json({ status: "Bad request" }, { status: 400 });
+    }
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
-
-  const finalState = await pipeline
-    .use(DeleteAppThreadPlugin)
-    .use(DeleteApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appThread: {
-        id: id,
-        appInstanceId: appInstanceId,
-      } as DeleteAppThreadPluginArgs,
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
     });
 
-  return NextResponse.json(finalState.apiResponse);
+    const finalState = await pipeline
+        .use(DeleteAppThreadPlugin)
+        .use(DeleteApiResponsePlugin)
+        .onError(DefaultErrorHandler)
+        .execute({
+            appThread: {
+                id: id,
+                appInstanceId: appInstanceId,
+            } as DeleteAppThreadPluginArgs,
+        });
+
+    return NextResponse.json(finalState.apiResponse, {
+        status: (finalState.apiResponse?.status.statusCode as number) ?? 500,
+    });
 }
