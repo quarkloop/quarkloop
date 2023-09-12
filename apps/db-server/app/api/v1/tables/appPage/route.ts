76dae9a5c028961c1af591a0875ff7a57d546e0c
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-12 15:36:46 +0200

    [db-server] Update api route status code: apps/db-server/app/api/v1/tables/appPage/route.ts

--- apps/db-server/app/api/v1/tables/appPage/route.ts
+++ apps/db-server/app/api/v1/tables/appPage/route.ts
@@ -1,136 +1,148 @@
 import { NextResponse } from "next/server";
 
 import {
-  GetAppPageByIdPluginArgs,
-  CreateAppPagePluginArgs,
-  DeleteAppPagePluginArgs,
-  UpdateAppPagePluginArgs,
-  GetAppPageByAppInstanceIdPluginArgs,
+    GetAppPageByIdPluginArgs,
+    CreateAppPagePluginArgs,
+    DeleteAppPagePluginArgs,
+    UpdateAppPagePluginArgs,
+    GetAppPageByAppInstanceIdPluginArgs,
 } from "@quarkloop/types";
 import { createPipeline } from "@quarkloop/plugin";
 import {
-  PipelineState,
-  PipelineArgs,
-  DefaultErrorHandler,
-  GetApiResponsePlugin,
-  CreateApiResponsePlugin,
-  UpdateApiResponsePlugin,
-  DeleteApiResponsePlugin,
-  GetAppPageByIdPlugin,
-  CreateAppPagePlugin,
-  UpdateAppPagePlugin,
-  DeleteAppPagePlugin,
-  GetAppPageByAppInstanceIdPlugin,
+    PipelineState,
+    PipelineArgs,
+    DefaultErrorHandler,
+    GetApiResponsePlugin,
+    CreateApiResponsePlugin,
+    UpdateApiResponsePlugin,
+    DeleteApiResponsePlugin,
+    GetAppPageByIdPlugin,
+    CreateAppPagePlugin,
+    UpdateAppPagePlugin,
+    DeleteAppPagePlugin,
+    GetAppPageByAppInstanceIdPlugin,
 } from "@quarkloop/plugins";
 
 // GetAppPageById
 // GetAppPageByAppInstanceId
 export async function GET(request: Request, { params }: { params: any }) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  const appInstanceId = searchParams.get("appInstanceId");
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const appInstanceId = searchParams.get("appInstanceId");
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
+    });
 
-  if (id) {
-    const finalState = await pipeline
-      .use(GetAppPageByIdPlugin)
-      .use(GetApiResponsePlugin)
-      .onError(DefaultErrorHandler)
-      .execute({
-        appPage: {
-          id: id,
-        } as GetAppPageByIdPluginArgs,
-      });
+    if (id) {
+        const finalState = await pipeline
+            .use(GetAppPageByIdPlugin)
+            .use(GetApiResponsePlugin)
+            .onError(DefaultErrorHandler)
+            .execute({
+                appPage: {
+                    id: id,
+                } as GetAppPageByIdPluginArgs,
+            });
 
-    return NextResponse.json(finalState.apiResponse);
-  }
+        return NextResponse.json(finalState.apiResponse, {
+            status:
+                (finalState.apiResponse?.status.statusCode as number) ?? 500,
+        });
+    }
 
-  if (appInstanceId) {
-    const finalState = await pipeline
-      .use(GetAppPageByAppInstanceIdPlugin)
-      .use(GetApiResponsePlugin)
-      .onError(DefaultErrorHandler)
-      .execute({
-        appPage: {
-          appInstanceId: appInstanceId,
-        } as GetAppPageByAppInstanceIdPluginArgs,
-      });
+    if (appInstanceId) {
+        const finalState = await pipeline
+            .use(GetAppPageByAppInstanceIdPlugin)
+            .use(GetApiResponsePlugin)
+            .onError(DefaultErrorHandler)
+            .execute({
+                appPage: {
+                    appInstanceId: appInstanceId,
+                } as GetAppPageByAppInstanceIdPluginArgs,
+            });
 
-    return NextResponse.json(finalState.apiResponse);
-  }
+        return NextResponse.json(finalState.apiResponse, {
+            status:
+                (finalState.apiResponse?.status.statusCode as number) ?? 500,
+        });
+    }
 
-  return NextResponse.json({ status: "Bad request" }, { status: 400 });
+    return NextResponse.json({ status: "Bad request" }, { status: 400 });
 }
 
 // CreateAppPage
 export async function POST(request: Request, { params }: { params: any }) {
-  const body = await request.json();
+    const body = await request.json();
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
-
-  const finalState = await pipeline
-    .use(CreateAppPagePlugin)
-    .use(CreateApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appPage: {
-        ...body,
-      } as CreateAppPagePluginArgs,
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
     });
 
-  return NextResponse.json(finalState.apiResponse);
+    const finalState = await pipeline
+        .use(CreateAppPagePlugin)
+        .use(CreateApiResponsePlugin)
+        .onError(DefaultErrorHandler)
+        .execute({
+            appPage: {
+                ...body,
+            } as CreateAppPagePluginArgs,
+        });
+
+    return NextResponse.json(finalState.apiResponse, {
+        status: (finalState.apiResponse?.status.statusCode as number) ?? 500,
+    });
 }
 
 // UpdateAppPage
 export async function PUT(request: Request, { params }: { params: any }) {
-  const body = await request.json();
+    const body = await request.json();
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
-
-  const finalState = await pipeline
-    .use(UpdateAppPagePlugin)
-    .use(UpdateApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appPage: {
-        ...body,
-      } as UpdateAppPagePluginArgs,
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
     });
 
-  return NextResponse.json(finalState.apiResponse);
+    const finalState = await pipeline
+        .use(UpdateAppPagePlugin)
+        .use(UpdateApiResponsePlugin)
+        .onError(DefaultErrorHandler)
+        .execute({
+            appPage: {
+                ...body,
+            } as UpdateAppPagePluginArgs,
+        });
+
+    return NextResponse.json(finalState.apiResponse, {
+        status: (finalState.apiResponse?.status.statusCode as number) ?? 500,
+    });
 }
 
 // DeleteAppPage
 export async function DELETE(request: Request, { params }: { params: any }) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  const appInstanceId = searchParams.get("appInstanceId");
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const appInstanceId = searchParams.get("appInstanceId");
 
-  if (id == null && appInstanceId == null) {
-    return NextResponse.json({ status: "Bad request" }, { status: 400 });
-  }
+    if (id == null && appInstanceId == null) {
+        return NextResponse.json({ status: "Bad request" }, { status: 400 });
+    }
 
-  const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
-    initialState: {},
-  });
-
-  const finalState = await pipeline
-    .use(DeleteAppPagePlugin)
-    .use(DeleteApiResponsePlugin)
-    .onError(DefaultErrorHandler)
-    .execute({
-      appPage: {
-        id: id,
-        appInstanceId: appInstanceId,
-      } as DeleteAppPagePluginArgs,
+    const pipeline = createPipeline<PipelineState, PipelineArgs[]>({
+        initialState: {},
     });
 
-  return NextResponse.json(finalState.apiResponse);
+    const finalState = await pipeline
+        .use(DeleteAppPagePlugin)
+        .use(DeleteApiResponsePlugin)
+        .onError(DefaultErrorHandler)
+        .execute({
+            appPage: {
+                id: id,
+                appInstanceId: appInstanceId,
+            } as DeleteAppPagePluginArgs,
+        });
+
+    return NextResponse.json(finalState.apiResponse, {
+        status: (finalState.apiResponse?.status.statusCode as number) ?? 500,
+    });
 }
