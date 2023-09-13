a2b88df6c4ec4a14b3cc5ec35ba03346ff32a7b4
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-13 23:29:50 +0200

    [api-server] add UpdateFile op: pkg/db/api/file.go

--- pkg/db/api/file.go
+++ pkg/db/api/file.go
@@ -65,3 +65,32 @@ func CreateFile(appId, instanceId string, file *model.File) (*model.File, error)
 
 	return nil, errors.New("failed to create file")
 }
+
+func UpdateFile(appId, instanceId string, file *model.File) (*model.File, error) {
+	payload, err := json.Marshal(file)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.DatabaseClient.Update("http://localhost:3000/api/v1/tables/appFile", payload)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusCreated {
+		var payload DatabaseResponsePayload
+		if err := json.NewDecoder(res.Body).Decode(&payload); err != nil {
+			return nil, err
+		}
+
+		var file model.File
+		if err := json.Unmarshal(payload.Database.File.Records, &file); err != nil {
+			return nil, err
+		}
+
+		return &file, nil
+	}
+
+	return nil, errors.New("failed to update file")
+}
