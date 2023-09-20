5575da73f4f9dc5c51594da0283e4f53575c7f85
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-20 12:43:24 +0200

    Change api and db server req & res payloads and params for file op

--- pkg/db/model/file.go
+++ pkg/db/model/file.go
@@ -3,10 +3,8 @@ package model
 import "time"
 
 type File struct {
-	AppId      string    `json:"appId,omitempty"`
-	InstanceId string    `json:"appInstanceId,omitempty"`
-	Id         string    `json:"id,omitempty"`
-	Enable     *bool     `json:"enable,omitempty"`
-	CreatedAt  time.Time `json:"createdAt,omitempty"`
-	UpdatedAt  time.Time `json:"updatedAt,omitempty"`
+	Id        string    `json:"id,omitempty"`
+	Enable    *bool     `json:"enable,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty"`
 }
