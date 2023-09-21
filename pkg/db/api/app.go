059db3774c5470b71c5e89f7978476390fa5d4e0
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-21 23:23:30 +0200

    Add CreateApp db api: pkg/db/api/app.go

---
+++ pkg/db/api/app.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/quarkloop/quarkloop/pkg/db/client"
+	"github.com/quarkloop/quarkloop/pkg/db/model"
+)
+
+func CreateApp(app *model.App) (*model.App, error) {
+	payload, err := json.Marshal(app)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.DatabaseClient.Post("http://localhost:3000/api/v1/tables/app", nil, payload)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusCreated {
+		var payload DatabaseResponsePayload
+		if err := json.NewDecoder(res.Body).Decode(&payload); err != nil {
+			return nil, err
+		}
+
+		var app model.App
+		if err := json.Unmarshal(payload.Database.App.Records, &app); err != nil {
+			return nil, err
+		}
+
+		return &app, nil
+	}
+
+	return nil, errors.New("failed to create app")
+}
