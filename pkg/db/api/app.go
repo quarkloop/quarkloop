7261a54d6b87e5e687ba55e64bb71dc9e3e8a9a9
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-21 23:25:58 +0200

    Add UpdateApp db api: pkg/db/api/app.go

--- pkg/db/api/app.go
+++ pkg/db/api/app.go
@@ -37,3 +37,32 @@ func CreateApp(app *model.App) (*model.App, error) {
 
 	return nil, errors.New("failed to create app")
 }
+
+func UpdateApp(app *model.App) (*model.App, error) {
+	payload, err := json.Marshal(app)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.DatabaseClient.Update("http://localhost:3000/api/v1/tables/app", nil, payload)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusCreated {
+		var payload DatabaseResponsePayload
+		if err := json.NewDecoder(res.Body).Decode(&payload); err != nil {
+			return nil, err
+		}
+
+		var app model.App
+		if err := json.Unmarshal(payload.Database.App.Records, &app); err != nil {
+			return nil, err
+		}
+
+		return &app, nil
+	}
+
+	return nil, errors.New("failed to update app")
+}
