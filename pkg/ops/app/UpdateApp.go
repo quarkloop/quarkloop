9a062c728500c36af108d1450a7c0a0cf8c0bb9f
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-23 18:27:39 +0200

    Add Versioning to ops and their impls: pkg/ops/app/UpdateApp.go

--- pkg/ops/app/UpdateApp.go
+++ pkg/ops/app/UpdateApp.go
@@ -2,30 +2,24 @@ package ops
 
 import (
 	"encoding/json"
+	"errors"
 
-	"github.com/quarkloop/quarkloop/pkg/ops/app/db"
-	"github.com/quarkloop/quarkloop/pkg/ops/app/model"
+	"github.com/quarkloop/quarkloop/pkg/ops/app/impl"
 )
 
 type UpdateApp struct {
-	Name string `json:"name"`
-}
-
-type UpdateAppArgs struct {
-	AppID string    `json:"appId" binding:"required"`
-	App   model.App `json:"app"`
+	Name    string `json:"name"`
+	Version string `json:"version"`
 }
 
 func (op *UpdateApp) Call(args json.RawMessage) (interface{}, error) {
-	var appArgs UpdateAppArgs
-	if err := json.Unmarshal(args, &appArgs); err != nil {
-		return nil, err
+	if op.Version == "latest" {
+		val, err := impl.UpdateApp(args)
+		if err != nil {
+			return nil, err
+		}
+		return val, nil
 	}
 
-	app, err := db.UpdateApp(&appArgs.App)
-	if err != nil {
-		return nil, err
-	}
-
-	return app, nil
+	return nil, errors.New("failed to call op")
 }
