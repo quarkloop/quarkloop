d219c1917c9a64f282f7fe54f3fba455c2e39276
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-09-23 14:21:21 +0200

    Delete old app model and apis: pkg/db/model/app_instance.go

--- pkg/db/model/app_instance.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type AppInstance struct {
-	AppId      string `json:"appId"`
-	InstanceId string `json:"instanceId"`
-	ID         string `json:"id,omitempty"`
-	Name       string `json:"name"`
-}
