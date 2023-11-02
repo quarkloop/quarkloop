d0a6afc8b839411a4160c3d23bf21feb7d2f2b3d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-02 02:13:38 +0100

    [quarkloop-server] Add api package: pkg/api/api.go

---
+++ pkg/api/api.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/db/repository"
+)
+
+type ServerApi struct {
+	dataStore *repository.Repository
+}
+
+func NewServerApi(ds *repository.Repository) ServerApi {
+	return ServerApi{dataStore: ds}
+}
+
+type ApiResponse struct {
+	Status       int    `json:"status"`
+	StatusString string `json:"statusString"`
+	Error        error  `json:"error,omitempty"`
+	ErrorString  string `json:"errorString,omitempty"`
+}
+
+func AbortWithBadRequestJSON(c *gin.Context, err error) {
+	response := ApiResponse{
+		Status:       http.StatusBadRequest,
+		StatusString: "BadRequest",
+		Error:        err,
+		ErrorString:  fmt.Sprintf("[BindJSON] %s", err.Error()),
+	}
+
+	c.AbortWithStatusJSON(http.StatusBadRequest, response)
+}
+
+func AbortWithInternalServerErrorJSON(c *gin.Context, err error) {
+	response := ApiResponse{
+		Status:       http.StatusInternalServerError,
+		StatusString: "InternalServerError",
+		Error:        err,
+		ErrorString:  fmt.Sprintf("[BindJSON] %s", err.Error()),
+	}
+
+	c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+}
