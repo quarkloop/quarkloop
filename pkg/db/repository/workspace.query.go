f48f643e2be48c76d819d959730548e35c8e836a
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-04 01:34:37 +0100

    [pkg] update db repository: pkg/db/repository/workspace.query.go

--- pkg/db/repository/workspace.query.go
+++ pkg/db/repository/workspace.query.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 
@@ -14,16 +16,16 @@ import (
 
 type ListWorkspacesParams struct {
 	Context context.Context
-	OsId    string
+	OsId    []string
 }
 
 const listWorkspacesQuery = `
 SELECT 
-  "id", "name", "description", "path", "createdAt", "updatedAt"
+  "id", "name", "accessType", "description", "path", "createdAt", "updatedAt"
 FROM 
   "app"."Workspace"
 WHERE
-  "osId" = @osId;
+  "osId" = ANY (@osId);
 `
 
 func (r *Repository) ListWorkspaces(p *ListWorkspacesParams) ([]model.Workspace, error) {
@@ -41,6 +43,7 @@ func (r *Repository) ListWorkspaces(p *ListWorkspacesParams) ([]model.Workspace,
 		err := rows.Scan(
 			&workspace.Id,
 			&workspace.Name,
+			&workspace.AccessType,
 			&workspace.Description,
 			&workspace.Path,
 			&workspace.CreatedAt,
@@ -61,29 +64,99 @@ func (r *Repository) ListWorkspaces(p *ListWorkspacesParams) ([]model.Workspace,
 	return wsList, nil
 }
 
-/// GetWorkspaceById
+/// FindUniqueWorkspace
 
-type GetWorkspaceByIdParams struct {
+type FindUniqueWorkspaceParams struct {
 	Context context.Context
 	Id      string
 }
 
-const getWorkspaceByIdQuery = `
+const findUniqueWorkspaceQuery = `
 SELECT 
-  "id", "name", "description", "path", "createdAt", "updatedAt"
+  "id", "name", "accessType", "description", "path", "createdAt", "updatedAt"
 FROM 
   "app"."Workspace" 
 WHERE 
   "id" = @id;
 `
 
-func (r *Repository) GetWorkspaceById(p *GetWorkspaceByIdParams) (*model.Workspace, error) {
-	row := r.Conn.QueryRow(p.Context, getWorkspaceByIdQuery, pgx.NamedArgs{"id": p.Id})
+func (r *Repository) FindUniqueWorkspace(p *FindUniqueWorkspaceParams) (*model.Workspace, error) {
+	row := r.Conn.QueryRow(p.Context, findUniqueWorkspaceQuery, pgx.NamedArgs{"id": p.Id})
 
 	var workspace model.Workspace
 	err := row.Scan(
 		&workspace.Id,
 		&workspace.Name,
+		&workspace.AccessType,
+		&workspace.Description,
+		&workspace.Path,
+		&workspace.CreatedAt,
+		&workspace.UpdatedAt,
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[READ] failed: %v\n", err)
+		return nil, err
+	}
+
+	return &workspace, nil
+}
+
+/// FindFirstWorkspace
+
+type FindFirstWorkspaceParams struct {
+	Context   context.Context
+	OsId      string
+	Workspace model.Workspace
+}
+
+const findFirstWorkspaceQuery = `
+SELECT 
+  "id", "name", "accessType", "description", "path", "createdAt", "updatedAt"
+FROM 
+  "app"."Workspace" 
+WHERE
+`
+
+func (r *Repository) FindFirstWorkspace(p *FindFirstWorkspaceParams) (*model.Workspace, error) {
+	availableFields := []string{}
+	workspaceFields := map[string]interface{}{
+		"osId":       p.OsId,
+		"id":         p.Workspace.Id,
+		"name":       p.Workspace.Name,
+		"accessType": *p.Workspace.AccessType,
+		"path":       p.Workspace.Path,
+		"createdAt":  p.Workspace.CreatedAt,
+		"updatedAt":  p.Workspace.UpdatedAt,
+	}
+	for key, value := range workspaceFields {
+		switch v := value.(type) {
+		case int:
+			if v != 0 {
+				availableFields = append(availableFields, fmt.Sprintf("\"%s\" = '%d'", key, v))
+			}
+		case float64:
+			if v != 0.0 {
+				availableFields = append(availableFields, fmt.Sprintf("\"%s\" = '%f'", key, v))
+			}
+		case string:
+			if v != "" {
+				availableFields = append(availableFields, fmt.Sprintf("\"%s\" = '%s'", key, v))
+			}
+		case *time.Time:
+			if v != nil {
+				availableFields = append(availableFields, fmt.Sprintf("\"%s\" = '%s'", key, v))
+			}
+		}
+	}
+	finalQuery := findFirstWorkspaceQuery + strings.Join(availableFields, " AND ")
+
+	row := r.Conn.QueryRow(p.Context, finalQuery)
+
+	var workspace model.Workspace
+	err := row.Scan(
+		&workspace.Id,
+		&workspace.Name,
+		&workspace.AccessType,
 		&workspace.Description,
 		&workspace.Path,
 		&workspace.CreatedAt,
