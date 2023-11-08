a52e44cb920fa68d8d04abf423f0b3ddb446fc83
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-09 00:48:39 +0100

    [pkg] update api: pkg/api/app.mutation.go

--- pkg/api/app.mutation.go
+++ pkg/api/app.mutation.go
@@ -4,45 +4,109 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/db/model"
+	"github.com/quarkloop/quarkloop/pkg/db/repository"
 )
 
-type CreateAppRequest struct{}
-type CreateAppResponse struct{}
+type CreateAppRequest struct {
+	OsId        string    `json:"osId" binding:"required"`
+	WorkspaceId string    `json:"workspaceId" binding:"required"`
+	App         model.App `json:"app" binding:"required"`
+}
+
+type CreateAppResponse struct {
+	ApiResponse
+	Data model.App `json:"data,omitempty"`
+}
 
 func (s *ServerApi) CreateApp(c *gin.Context) {
 	req := &CreateAppRequest{}
-
 	if err := c.BindJSON(req); err != nil {
 		AbortWithBadRequestJSON(c, err)
 		return
 	}
 
-	res := &CreateAppResponse{}
+	// query database
+	ws, err := s.dataStore.CreateApp(
+		&repository.CreateAppParams{
+			Context:     c,
+			OsId:        req.OsId,
+			WorkspaceId: req.WorkspaceId,
+			App:         req.App,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
+		return
+	}
+
+	res := &CreateAppResponse{
+		ApiResponse: ApiResponse{
+			Status:       http.StatusCreated,
+			StatusString: "Created",
+		},
+		Data: *ws,
+	}
 	c.JSON(http.StatusCreated, res)
 }
 
-type UpdateAppByIdRequest struct{}
-type UpdateAppByIdResponse struct{}
+type UpdateAppByIdUriParams struct {
+	AppId string `uri:"appId" binding:"required"`
+}
+
+type UpdateAppByIdRequest struct {
+	model.App
+}
 
 func (s *ServerApi) UpdateAppById(c *gin.Context) {
-	req := &UpdateAppByIdRequest{}
+	uriParams := &UpdateAppByIdUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
 
+	req := &UpdateAppByIdRequest{}
 	if err := c.BindJSON(req); err != nil {
 		AbortWithBadRequestJSON(c, err)
 		return
 	}
 
-	res := &UpdateAppByIdResponse{}
-	c.JSON(http.StatusOK, res)
+	// query database
+	err := s.dataStore.UpdateAppById(
+		&repository.UpdateAppByIdParams{
+			Context: c,
+			AppId:   uriParams.AppId,
+			App:     req.App,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
 }
 
-type DeleteAppByIdRequest struct{}
+type DeleteAppByIdUriParams struct {
+	AppId string `uri:"appId" binding:"required"`
+}
 
 func (s *ServerApi) DeleteAppById(c *gin.Context) {
-	req := &DeleteAppByIdRequest{}
-
-	if err := c.BindJSON(req); err != nil {
+	uriParams := &DeleteAppByIdUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
 		AbortWithBadRequestJSON(c, err)
+		return
+	}
+
+	// query database
+	err := s.dataStore.DeleteAppById(
+		&repository.DeleteAppByIdParams{
+			Context: c,
+			AppId:   uriParams.AppId,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
 		return
 	}
 
