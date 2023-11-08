90d8d7e125e9b2d508409d577831d50966aeecfa
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-08 23:24:44 +0100

    [pkg] update api: pkg/api/workspace.query.go

--- pkg/api/workspace.query.go
+++ pkg/api/workspace.query.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,11 @@ type GetWorkspaceListResponse struct {
 }
 
 func (s *ServerApi) GetWorkspaceList(c *gin.Context) {
-	osId := c.Param("osId")
+	osId, exists := c.GetQueryArray("osId")
+	if !exists {
+		AbortWithBadRequestJSON(c, errors.New("missing osId parameter"))
+		return
+	}
 
 	// query database
 	wsList, err := s.dataStore.ListWorkspaces(
@@ -48,8 +53,8 @@ func (s *ServerApi) GetWorkspaceById(c *gin.Context) {
 	wsId := c.Param("workspaceId")
 
 	// query database
-	ws, err := s.dataStore.GetWorkspaceById(
-		&repository.GetWorkspaceByIdParams{
+	ws, err := s.dataStore.FindUniqueWorkspace(
+		&repository.FindUniqueWorkspaceParams{
 			Context: c,
 			Id:      wsId,
 		},
@@ -60,6 +65,47 @@ func (s *ServerApi) GetWorkspaceById(c *gin.Context) {
 	}
 
 	res := &GetWorkspaceByIdResponse{
+		ApiResponse: ApiResponse{
+			Status:       http.StatusOK,
+			StatusString: "OK",
+		},
+		Data: *ws,
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+type GetWorkspaceQueryParams struct {
+	OsId string `form:"osId" binding:"required"`
+	model.Workspace
+}
+
+type GetWorkspaceResponse struct {
+	ApiResponse
+	OsId string          `json:"osId" binding:"required"`
+	Data model.Workspace `json:"data,omitempty"`
+}
+
+func (s *ServerApi) GetWorkspace(c *gin.Context) {
+	queryParams := &GetWorkspaceQueryParams{}
+	if err := c.ShouldBindQuery(queryParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
+
+	// query database
+	ws, err := s.dataStore.FindFirstWorkspace(
+		&repository.FindFirstWorkspaceParams{
+			Context:   c,
+			OsId:      queryParams.OsId,
+			Workspace: queryParams.Workspace,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
+		return
+	}
+
+	res := &GetWorkspaceResponse{
 		ApiResponse: ApiResponse{
 			Status:       http.StatusOK,
 			StatusString: "OK",
