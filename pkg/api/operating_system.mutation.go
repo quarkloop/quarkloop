989ad05b12a41054091b9d2da9514f8a76f225f4
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-09 00:48:39 +0100

    [pkg] update api: pkg/api/operating_system.mutation.go

--- pkg/api/operating_system.mutation.go
+++ pkg/api/operating_system.mutation.go
@@ -47,24 +47,32 @@ func (s *ServerApi) CreateOperatingSystem(c *gin.Context) {
 	c.JSON(http.StatusCreated, res)
 }
 
+type UpdateOperatingSystemByIdUriParams struct {
+	OsId string `uri:"osId" binding:"required"`
+}
+
 type UpdateOperatingSystemByIdRequest struct {
 	model.OperatingSystem
 }
 
 func (s *ServerApi) UpdateOperatingSystemById(c *gin.Context) {
+	uriParams := &UpdateOperatingSystemByIdUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
+
 	req := &UpdateOperatingSystemByIdRequest{}
 	if err := c.BindJSON(req); err != nil {
 		AbortWithBadRequestJSON(c, err)
 		return
 	}
 
-	osId := c.Param("osId")
-
 	// query database
 	err := s.dataStore.UpdateOperatingSystemById(
 		&repository.UpdateOperatingSystemByIdParams{
 			Context: c,
-			OsId:    osId,
+			OsId:    uriParams.OsId,
 			Os:      req.OperatingSystem,
 		},
 	)
@@ -76,14 +84,22 @@ func (s *ServerApi) UpdateOperatingSystemById(c *gin.Context) {
 	c.JSON(http.StatusOK, nil)
 }
 
+type DeleteOperatingSystemByIdUriParams struct {
+	OsId string `uri:"osId" binding:"required"`
+}
+
 func (s *ServerApi) DeleteOperatingSystemById(c *gin.Context) {
-	osId := c.Param("osId")
+	uriParams := &DeleteOperatingSystemByIdUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
 
 	// query database
 	err := s.dataStore.DeleteOperatingSystemById(
 		&repository.DeleteOperatingSystemByIdParams{
 			Context: c,
-			Id:      osId,
+			OsId:    uriParams.OsId,
 		},
 	)
 	if err != nil {
