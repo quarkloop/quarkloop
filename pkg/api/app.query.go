84acd758e2a439e48ad4c734ea4960166775cefc
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-08 23:24:44 +0100

    [pkg] update api: pkg/api/app.query.go

--- pkg/api/app.query.go
+++ pkg/api/app.query.go
@@ -6,23 +6,42 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type GetAppListUriParams struct {
+	OsId        string `uri:"osId" binding:"required"`
+	WorkspaceId string `uri:"workspaceId" binding:"required"`
+}
+
 type GetAppListResponse struct{}
 
 func (s *ServerApi) GetAppList(c *gin.Context) {
+	uriParams := &GetAppListUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
+
+	// query database
+
 	res := &GetAppListResponse{}
 	c.JSON(http.StatusOK, res)
 }
 
-type GetAppByIdRequest struct{}
+type GetAppByIdUriParams struct {
+	OsId        string `uri:"osId" binding:"required"`
+	WorkspaceId string `uri:"workspaceId" binding:"required"`
+	AppId       string `uri:"appId" binding:"required"`
+}
+
 type GetAppByIdResponse struct{}
 
 func (s *ServerApi) GetAppById(c *gin.Context) {
-	req := &GetAppByIdRequest{}
-
-	if err := c.BindJSON(req); err != nil {
+	uriParams := &GetAppByIdUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
 		AbortWithBadRequestJSON(c, err)
 		return
 	}
+
+	// query database
 
 	res := &GetAppByIdResponse{}
 	c.JSON(http.StatusOK, res)
