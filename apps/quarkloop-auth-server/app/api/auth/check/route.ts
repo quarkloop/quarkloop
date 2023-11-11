c41751117fd72fe5e7eddb382761f2ca87629521
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-11 23:03:15 +0100

    update auth server: apps/quarkloop-auth-server/app/api/auth/check/route.ts

--- apps/quarkloop-auth-server/app/api/auth/check/route.ts
+++ apps/quarkloop-auth-server/app/api/auth/check/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@quarkloop/lib";
+import { authOptions } from "@/auth";
 
 export async function GET(request: Request, params: { params: any }) {
     const session = await getServerSession(authOptions);
