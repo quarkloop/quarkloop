88b353ba082433c6b54e73d836d386e5823c5cb0
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-12 17:15:50 +0100

    [Go] update app_component package: pkg/api/app_component.mutation.go

--- pkg/api/app_component.mutation.go
+++ pkg/api/app_component.mutation.go
@@ -4,26 +4,56 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/db/model"
+	"github.com/quarkloop/quarkloop/pkg/db/repository"
 )
 
-type CreateAppComponentRequest struct{}
-type CreateAppComponentResponse struct{}
+type CreateAppComponentUriParams struct {
+	AppId string `uri:"appId" binding:"required"`
+}
+
+type CreateAppComponentRequest struct {
+	model.AppComponent
+}
+
+type CreateAppComponentResponse struct {
+	ApiResponse
+	Data model.SheetInstance `json:"data,omitempty"`
+}
 
 func (s *ServerApi) CreateAppComponent(c *gin.Context) {
+	uriParams := &CreateAppComponentUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
+
 	req := &CreateAppComponentRequest{}
 	if err := c.BindJSON(req); err != nil {
 		AbortWithBadRequestJSON(c, err)
 		return
 	}
 
-	osId := c.Param("osId")
-	workspaceId := c.Param("workspaceId")
-	appId := c.Param("appId")
-	_ = osId + workspaceId + appId
-
 	// query database
+	ws, err := s.dataStore.CreateSheetInstance(
+		&repository.CreateSheetInstanceParams{
+			Context:       c,
+			AppId:         uriParams.AppId,
+			SheetInstance: req.SheetInstance,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
+		return
+	}
 
-	res := &CreateAppComponentResponse{}
+	res := &CreateAppComponentResponse{
+		ApiResponse: ApiResponse{
+			Status:       http.StatusCreated,
+			StatusString: "Created",
+		},
+		Data: *ws,
+	}
 	c.JSON(http.StatusCreated, res)
 }
 
