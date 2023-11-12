1c028d15aac5a6b2638902ab11277982658bbe6e
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-12 17:19:15 +0100

    [apps] add package: packages/apps/tailwind.config.js

---
+++ packages/apps/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
