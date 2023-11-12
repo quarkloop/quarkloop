c4e0f04dfeed6843d580d801fdee375526446f4f
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-12 17:15:50 +0100

    [Go] update app_component package: pkg/api/app_component.query.go

--- pkg/api/app_component.query.go
+++ pkg/api/app_component.query.go
@@ -4,33 +4,84 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/db/model"
+	"github.com/quarkloop/quarkloop/pkg/db/repository"
 )
 
-type GetAppComponentListResponse struct{}
+type GetAppComponentListUriParams struct {
+	AppId string `uri:"appId" binding:"required"`
+}
+
+type GetAppComponentListResponse struct {
+	ApiResponse
+	Data []model.AppComponent `json:"data,omitempty"`
+}
 
 func (s *ServerApi) GetAppComponentList(c *gin.Context) {
-	osId := c.Param("osId")
-	workspaceId := c.Param("workspaceId")
-	appId := c.Param("appId")
-	_ = osId + workspaceId + appId
+	uriParams := &GetAppComponentListUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
 
 	// query database
+	compList, err := s.dataStore.ListAppComponents(
+		&repository.ListAppComponentsParams{
+			Context: c,
+			AppId:   uriParams.AppId,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
+		return
+	}
 
-	res := &GetAppComponentListResponse{}
+	res := &GetAppComponentListResponse{
+		ApiResponse: ApiResponse{
+			Status:       http.StatusOK,
+			StatusString: "OK",
+		},
+		Data: compList,
+	}
 	c.JSON(http.StatusOK, res)
 }
 
-type GetAppComponentByIdResponse struct{}
+type GetAppComponentByIdUriParams struct {
+	AppId       string `uri:"appId" binding:"required"`
+	ComponentId string `uri:"componentId" binding:"required"`
+}
+
+type GetAppComponentByIdResponse struct {
+	ApiResponse
+	Data model.AppComponent `json:"data,omitempty"`
+}
 
 func (s *ServerApi) GetAppComponentById(c *gin.Context) {
-	osId := c.Param("osId")
-	workspaceId := c.Param("workspaceId")
-	appId := c.Param("appId")
-	componentId := c.Param("componentId")
-	_ = osId + workspaceId + appId + componentId
+	uriParams := &GetAppComponentByIdUriParams{}
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		AbortWithBadRequestJSON(c, err)
+		return
+	}
 
 	// query database
+	comp, err := s.dataStore.FindUniqueAppComponent(
+		&repository.FindUniqueAppComponentParams{
+			Context:     c,
+			AppId:       uriParams.AppId,
+			ComponentId: uriParams.ComponentId,
+		},
+	)
+	if err != nil {
+		AbortWithInternalServerErrorJSON(c, err)
+		return
+	}
 
-	res := &GetAppComponentByIdResponse{}
+	res := &GetAppComponentByIdResponse{
+		ApiResponse: ApiResponse{
+			Status:       http.StatusOK,
+			StatusString: "OK",
+		},
+		Data: *comp,
+	}
 	c.JSON(http.StatusOK, res)
 }
