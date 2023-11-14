be8d58f49280ba97a8167875c4210fccdad899b3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-14 22:58:29 +0100

    Add lib package service: packages/lib/src/service/hooks/index.ts

---
+++ packages/lib/src/service/hooks/index.ts
@@ -0,0 +1,14 @@
+export * from "./user.service";
+export * from "./os.service";
+export * from "./workspace.service";
+export * from "./app.service";
+export * from "./planSubscription.service";
+
+// export * from "./app.service";
+// export * from "./app-submission.service";
+
+// export * from "./conversation-settings.service";
+// export * from "./conversation-data.service";
+// export * from "./page-settings.service";
+// export * from "./file-settings.service";
+// export * from "./form-settings.service";
