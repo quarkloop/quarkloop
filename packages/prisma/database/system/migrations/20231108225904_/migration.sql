72914a3003929e19e9cd71f0a1c29913e6a30fc7
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-15 23:40:07 +0100

    Delete migration: packages/prisma/database/system/migrations/20231108225904_/migration.sql

--- packages/prisma/database/system/migrations/20231108225904_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `accessType` to the `App` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "system"."App" ADD COLUMN     "accessType" INTEGER NOT NULL;
