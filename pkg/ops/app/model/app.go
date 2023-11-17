6067539fc35a08793667a75e1135f709dc191b5d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-17 23:54:11 +0100

    Update op: pkg/ops/app/model/app.go

--- pkg/ops/app/model/app.go
+++ pkg/ops/app/model/app.go
@@ -5,14 +5,21 @@ import (
 	"time"
 )
 
-type App struct {
-	AppId      string          `json:"appId"`
-	InstanceId string          `json:"instanceId"`
-	ID         string          `json:"id,omitempty"`
-	Name       string          `json:"name,omitempty"`
-	Type       int             `json:"type,omitempty"`
-	Icon       string          `json:"icon,omitempty"`
-	Metadata   json.RawMessage `json:"metadata,omitempty"`
-	CreatedAt  time.Time       `json:"createdAt,omitempty"`
-	UpdatedAt  time.Time       `json:"updatedAt,omitempty"`
+type Project struct {
+	ID        string          `json:"id,omitempty"`
+	Name      string          `json:"name,omitempty"`
+	Type      int             `json:"type,omitempty"`
+	Status    AppStatus       `json:"status,omitempty"`
+	Icon      string          `json:"icon,omitempty"`
+	Metadata  json.RawMessage `json:"metadata,omitempty"`
+	CreatedAt time.Time       `json:"createdAt,omitempty"`
+	UpdatedAt time.Time       `json:"updatedAt,omitempty"`
 }
+
+type AppStatus int
+
+const (
+	On       AppStatus = 0
+	Off      AppStatus = 1
+	Archived AppStatus = 2
+)
