c7e563cbbd7ba925e7d2fcf483907fbca32a8344
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-18 23:28:16 +0100

    Update service api: packages/lib/src/service/hook.ts

--- packages/lib/src/service/hook.ts
+++ packages/lib/src/service/hook.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import type { AppState, AppDispatch } from './store';
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import type { AppState, AppDispatch } from "./store";
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
+// Use throughout your project instead of plain `useDispatch` and `useSelector`
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
 export const useAppDispatch: () => AppDispatch = useDispatch;
