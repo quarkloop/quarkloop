0d304f0cee31295d9a6b4785729c5bd6f0bd6e4d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-19 23:47:18 +0100

    Update api package: pkg/api/app_component.mutation.go

--- pkg/api/app_component.mutation.go
+++ pkg/api/app_component.mutation.go
@@ -1,92 +1,92 @@
 package api
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/quarkloop/quarkloop/pkg/db/model"
-	"github.com/quarkloop/quarkloop/pkg/db/repository"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/quarkloop/quarkloop/pkg/model"
+// 	"github.com/quarkloop/quarkloop/pkg/store/repository"
+// )
 
-type CreateAppComponentUriParams struct {
-	AppId string `uri:"appId" binding:"required"`
-}
+// type CreateAppComponentUriParams struct {
+// 	AppId string `uri:"projectId" binding:"required"`
+// }
 
-type CreateAppComponentRequest struct {
-	model.AppComponent
-}
+// type CreateAppComponentRequest struct {
+// 	model.AppComponent
+// }
 
-type CreateAppComponentResponse struct {
-	ApiResponse
-	Data model.SheetInstance `json:"data,omitempty"`
-}
+// type CreateAppComponentResponse struct {
+// 	ApiResponse
+// 	Data model.SheetInstance `json:"data,omitempty"`
+// }
 
-func (s *ServerApi) CreateAppComponent(c *gin.Context) {
-	uriParams := &CreateAppComponentUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		AbortWithBadRequestJSON(c, err)
-		return
-	}
+// func (s *ServerApi) CreateAppComponent(c *gin.Context) {
+// 	uriParams := &CreateAppComponentUriParams{}
+// 	if err := c.ShouldBindUri(uriParams); err != nil {
+// 		AbortWithBadRequestJSON(c, err)
+// 		return
+// 	}
 
-	req := &CreateAppComponentRequest{}
-	if err := c.BindJSON(req); err != nil {
-		AbortWithBadRequestJSON(c, err)
-		return
-	}
+// 	req := &CreateAppComponentRequest{}
+// 	if err := c.BindJSON(req); err != nil {
+// 		AbortWithBadRequestJSON(c, err)
+// 		return
+// 	}
 
-	// query database
-	ws, err := s.dataStore.CreateSheetInstance(
-		&repository.CreateSheetInstanceParams{
-			Context:       c,
-			AppId:         uriParams.AppId,
-			SheetInstance: req.SheetInstance,
-		},
-	)
-	if err != nil {
-		AbortWithInternalServerErrorJSON(c, err)
-		return
-	}
+// 	// query database
+// 	ws, err := s.dataStore.CreateAppComponent(
+// 		&repository.CreateAppComponentParams{
+// 			Context:       c,
+// 			AppId:         uriParams.AppId,
+// 			SheetInstance: req.SheetInstance,
+// 		},
+// 	)
+// 	if err != nil {
+// 		AbortWithInternalServerErrorJSON(c, err)
+// 		return
+// 	}
 
-	res := &CreateAppComponentResponse{
-		ApiResponse: ApiResponse{
-			Status:       http.StatusCreated,
-			StatusString: "Created",
-		},
-		Data: *ws,
-	}
-	c.JSON(http.StatusCreated, res)
-}
+// 	res := &CreateAppComponentResponse{
+// 		ApiResponse: ApiResponse{
+// 			Status:       http.StatusCreated,
+// 			StatusString: "Created",
+// 		},
+// 		Data: *ws,
+// 	}
+// 	c.JSON(http.StatusCreated, res)
+// }
 
-type UpdateAppComponentByIdRequest struct{}
-type UpdateAppComponentByIdResponse struct{}
+// type UpdateAppComponentByIdRequest struct{}
+// type UpdateAppComponentByIdResponse struct{}
 
-func (s *ServerApi) UpdateAppComponentById(c *gin.Context) {
-	req := &UpdateAppComponentByIdRequest{}
-	if err := c.BindJSON(req); err != nil {
-		AbortWithBadRequestJSON(c, err)
-		return
-	}
+// func (s *ServerApi) UpdateAppComponentById(c *gin.Context) {
+// 	req := &UpdateAppComponentByIdRequest{}
+// 	if err := c.BindJSON(req); err != nil {
+// 		AbortWithBadRequestJSON(c, err)
+// 		return
+// 	}
 
-	osId := c.Param("osId")
-	workspaceId := c.Param("workspaceId")
-	appId := c.Param("appId")
-	componentId := c.Param("componentId")
-	_ = osId + workspaceId + appId + componentId
+// 	orgId := c.Param("orgId")
+// 	workspaceId := c.Param("workspaceId")
+// 	projectId := c.Param("projectId")
+// 	componentId := c.Param("componentId")
+// 	_ = orgId + workspaceId + projectId + componentId
 
-	// query database
+// 	// query database
 
-	res := &UpdateAppComponentByIdResponse{}
-	c.JSON(http.StatusOK, res)
-}
+// 	res := &UpdateAppComponentByIdResponse{}
+// 	c.JSON(http.StatusOK, res)
+// }
 
-func (s *ServerApi) DeleteAppComponentById(c *gin.Context) {
-	osId := c.Param("osId")
-	workspaceId := c.Param("workspaceId")
-	appId := c.Param("appId")
-	componentId := c.Param("componentId")
-	_ = osId + workspaceId + appId + componentId
+// func (s *ServerApi) DeleteAppComponentById(c *gin.Context) {
+// 	orgId := c.Param("orgId")
+// 	workspaceId := c.Param("workspaceId")
+// 	projectId := c.Param("projectId")
+// 	componentId := c.Param("componentId")
+// 	_ = orgId + workspaceId + projectId + componentId
 
-	// query database
+// 	// query database
 
-	c.JSON(http.StatusNoContent, nil)
-}
+// 	c.JSON(http.StatusNoContent, nil)
+// }
