7c7f5c859e668e6251b1ab3e7a3074775cde54ed
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-20 17:24:02 +0100

    Add new api package: pkg/api/workspace/workspace.go

---
+++ pkg/api/workspace/workspace.go
@@ -0,0 +1,25 @@
+package workspace
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/service/workspace"
+)
+
+type Api interface {
+	GetWorkspaceList(c *gin.Context)
+	GetWorkspaceById(c *gin.Context)
+	GetWorkspace(c *gin.Context)
+	CreateWorkspace(c *gin.Context)
+	UpdateWorkspaceById(c *gin.Context)
+	DeleteWorkspaceById(c *gin.Context)
+}
+
+type WorkspaceApi struct {
+	workspaceService workspace.Service
+}
+
+func NewWorkspaceApi(service workspace.Service) *WorkspaceApi {
+	return &WorkspaceApi{
+		workspaceService: service,
+	}
+}
