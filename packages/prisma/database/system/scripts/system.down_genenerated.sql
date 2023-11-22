64419f8930f5aca99743c43c6988be0a7d09a1d8
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-22 01:41:35 +0100

    Update system database: packages/prisma/database/system/scripts/system.down_genenerated.sql

--- packages/prisma/database/system/scripts/system.down_genenerated.sql
+++ packages/prisma/database/system/scripts/system.down_genenerated.sql
@@ -1,128 +1,140 @@
 -- DropForeignKey
-ALTER TABLE "system"."Workspace" DROP CONSTRAINT "Workspace_osId_fkey";
+ALTER TABLE "system"."Workspace" DROP CONSTRAINT "Workspace_orgId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "system"."App" DROP CONSTRAINT "App_osId_fkey";
+ALTER TABLE "system"."Project" DROP CONSTRAINT "Project_orgId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "system"."App" DROP CONSTRAINT "App_workspaceId_fkey";
+ALTER TABLE "system"."Project" DROP CONSTRAINT "Project_workspaceId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "system"."AppComponent" DROP CONSTRAINT "AppComponent_appId_fkey";
+ALTER TABLE "system"."ProjectService" DROP CONSTRAINT "ProjectService_projectId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "system"."AppIssue" DROP CONSTRAINT "AppIssue_appId_fkey";
+ALTER TABLE "system"."ProjectSubmission" DROP CONSTRAINT "ProjectSubmission_projectId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "system"."AppSubmission" DROP CONSTRAINT "AppSubmission_appId_fkey";
+ALTER TABLE "system"."ProjectDiscussion" DROP CONSTRAINT "ProjectDiscussion_projectId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "system"."AppDeployment" DROP CONSTRAINT "AppDeployment_appId_fkey";
+ALTER TABLE "system"."ProjectForm" DROP CONSTRAINT "ProjectForm_projectId_fkey";
 
 -- DropTable
-DROP TABLE "system"."OperatingSystem";
+DROP TABLE "system"."Organization";
 
 -- DropTable
 DROP TABLE "system"."Workspace";
 
 -- DropTable
-DROP TABLE "system"."App";
+DROP TABLE "system"."Project";
 
 -- DropTable
-DROP TABLE "system"."AppComponent";
+DROP TABLE "system"."ProjectService";
 
 -- DropTable
-DROP TABLE "system"."AppIssue";
+DROP TABLE "system"."ProjectSubmission";
 
 -- DropTable
-DROP TABLE "system"."AppSubmission";
+DROP TABLE "system"."ProjectDiscussion";
 
 -- DropTable
-DROP TABLE "system"."AppDeployment";
+DROP TABLE "system"."ProjectForm";
 
 -- CreateTable
-CREATE TABLE "App" (
-    "id" TEXT NOT NULL,
-    "osId" TEXT NOT NULL,
-    "workspaceId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
-    "description" TEXT,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "App_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "AppComponent" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "config" JSONB NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "appId" TEXT,
-
-    CONSTRAINT "AppComponent_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "AppDeployment" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "appId" TEXT,
-
-    CONSTRAINT "AppDeployment_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "AppIssue" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "appId" TEXT,
-
-    CONSTRAINT "AppIssue_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "AppSubmission" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "appId" TEXT,
-
-    CONSTRAINT "AppSubmission_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "OperatingSystem" (
+CREATE TABLE "Organization" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "overview" TEXT,
-    "imageUrl" TEXT,
-    "websiteUrl" TEXT,
-    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "accessType" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
 
-    CONSTRAINT "OperatingSystem_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "orgId" TEXT NOT NULL,
+    "workspaceId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "accessType" INTEGER NOT NULL,
+    "metadata" JSONB,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectDiscussion" (
+    "id" SERIAL NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "metadata" JSONB,
+    "data" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "ProjectDiscussion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectForm" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "metadata" JSONB,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "projectId" TEXT NOT NULL,
+
+    CONSTRAINT "ProjectForm_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectService" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "metadata" JSONB,
+    "data" JSONB NOT NULL,
+    "isBuiltin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "projectId" TEXT NOT NULL,
+
+    CONSTRAINT "ProjectService_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectSubmission" (
+    "id" SERIAL NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "metadata" JSONB,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "ProjectSubmission_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Workspace" (
     "id" TEXT NOT NULL,
-    "osId" TEXT NOT NULL,
+    "orgId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
-    "description" TEXT,
-    "accessType" INTEGER,
-    "imageUrl" TEXT,
+    "description" TEXT NOT NULL,
+    "accessType" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
 
@@ -130,35 +142,35 @@ CREATE TABLE "Workspace" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "App_path_key" ON "App"("path" ASC);
+CREATE UNIQUE INDEX "Organization_path_key" ON "Organization"("path" ASC);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "OperatingSystem_path_key" ON "OperatingSystem"("path" ASC);
+CREATE UNIQUE INDEX "Project_path_key" ON "Project"("path" ASC);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Workspace_osId_id_key" ON "Workspace"("osId" ASC, "id" ASC);
+CREATE UNIQUE INDEX "Workspace_orgId_id_key" ON "Workspace"("orgId" ASC, "id" ASC);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Workspace_path_key" ON "Workspace"("path" ASC);
 
 -- AddForeignKey
-ALTER TABLE "App" ADD CONSTRAINT "App_osId_fkey" FOREIGN KEY ("osId") REFERENCES "OperatingSystem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Project" ADD CONSTRAINT "Project_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "App" ADD CONSTRAINT "App_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Project" ADD CONSTRAINT "Project_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "AppComponent" ADD CONSTRAINT "AppComponent_appId_fkey" FOREIGN KEY ("appId") REFERENCES "App"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "ProjectDiscussion" ADD CONSTRAINT "ProjectDiscussion_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "ProjectSubmission"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "AppDeployment" ADD CONSTRAINT "AppDeployment_appId_fkey" FOREIGN KEY ("appId") REFERENCES "App"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "ProjectForm" ADD CONSTRAINT "ProjectForm_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "AppIssue" ADD CONSTRAINT "AppIssue_appId_fkey" FOREIGN KEY ("appId") REFERENCES "App"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "ProjectService" ADD CONSTRAINT "ProjectService_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "AppSubmission" ADD CONSTRAINT "AppSubmission_appId_fkey" FOREIGN KEY ("appId") REFERENCES "App"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "ProjectSubmission" ADD CONSTRAINT "ProjectSubmission_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_osId_fkey" FOREIGN KEY ("osId") REFERENCES "OperatingSystem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
