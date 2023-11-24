2153510242b714ed881d193b808439e0464f2409
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-24 23:17:47 +0100

    Update: packages/lib/src/service/hooks/index.ts

--- packages/lib/src/service/hooks/index.ts
+++ packages/lib/src/service/hooks/index.ts
@@ -2,6 +2,8 @@ export * from "./user.service";
 export * from "./organization.service";
 export * from "./workspace.service";
 export * from "./project.service";
+export * from "./service.service";
+export * from "./submission.service";
 export * from "./planSubscription.service";
 
 // export * from "./project.service";
