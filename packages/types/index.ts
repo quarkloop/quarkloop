3736be0b589a2e26ef3e4dc64d4b8560ca6df7b9
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-11-24 23:17:22 +0100

    Update: packages/types/index.ts

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -3,6 +3,9 @@ export * from "./src/user";
 export * from "./src/organization";
 export * from "./src/workspace";
 export * from "./src/project";
+export * from "./src/service";
+export * from "./src/submission";
+
 export * from "./src/planSubscription";
 export * from "./src/thread";
 export * from "./src/thread-settings";
