e0b66272e96111ddb20b1f1cd9bba1d3473cba91
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-01 23:04:02 +0100

    Update table style

--- packages/ui/components/Table/Table.tsx
+++ packages/ui/components/Table/Table.tsx
@@ -92,7 +92,7 @@ const TableCell = React.forwardRef<
     <td
         ref={ref}
         className={cn(
-            "p-4 align-middle [&:has([role=checkbox])]:pr-0",
+            "p-3 align-middle [&:has([role=checkbox])]:pr-0",
             className
         )}
         {...props}
