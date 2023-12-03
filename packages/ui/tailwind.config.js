0ce7d9e311ced3ff7298007343444675e765f702
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-03 23:42:10 +0100

    Update ui package: packages/ui/tailwind.config.js

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
