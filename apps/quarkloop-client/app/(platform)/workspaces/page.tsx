9588cc68f7260c45d45368fc20d67afa7a529ed6
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-05 23:33:31 +0100

    [client] Push code: apps/quarkloop-client/app/(platform)/workspaces/page.tsx

---
+++ apps/quarkloop-client/app/(platform)/workspaces/page.tsx
@@ -0,0 +1,11 @@
+import { WorkspaceList } from "@quarkloop/components";
+
+const WorkspaceListPage = async () => {
+    return (
+        <div className="flex-1 flex flex-col">
+            <WorkspaceList />
+        </div>
+    );
+};
+
+export default WorkspaceListPage;
