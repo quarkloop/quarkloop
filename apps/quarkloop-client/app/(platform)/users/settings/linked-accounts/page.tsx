495f9fd78670e3c979409b681c12e61b83a621b1
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-05 23:33:31 +0100

    [client] Push code: apps/quarkloop-client/app/(platform)/users/settings/linked-accounts/page.tsx

---
+++ apps/quarkloop-client/app/(platform)/users/settings/linked-accounts/page.tsx
@@ -0,0 +1,7 @@
+import { UserLinkedAccountList } from "@quarkloop/components";
+
+export const dynamic = "force-dynamic";
+
+const UserLinkedAccountsPage = (props: any) => <UserLinkedAccountList />;
+
+export default UserLinkedAccountsPage;
