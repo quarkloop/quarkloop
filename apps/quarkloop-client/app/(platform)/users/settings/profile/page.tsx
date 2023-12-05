0fc09fe2c2a15c0773147f6e3d08a2da85005794
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-05 23:33:31 +0100

    [client] Push code: apps/quarkloop-client/app/(platform)/users/settings/profile/page.tsx

---
+++ apps/quarkloop-client/app/(platform)/users/settings/profile/page.tsx
@@ -0,0 +1,7 @@
+import { UserProfile } from "@quarkloop/components";
+
+export const dynamic = "force-dynamic";
+
+const UserProfilePage = (props: any) => <UserProfile />;
+
+export default UserProfilePage;
