3e293fb4cf1cdd30561b7c09a6a35c0c8aa85ec3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-05 23:33:30 +0100

    [client] Push code: apps/quarkloop-client/app/(platform)/page.tsx

---
+++ apps/quarkloop-client/app/(platform)/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationList } from "@quarkloop/components";
+
+const OrganizationListPage = async () => {
+    return (
+        <div className="flex-1 flex flex-col">
+            <OrganizationList />
+        </div>
+    );
+};
+
+export default OrganizationListPage;
