60ea96d01a347b3e24057f5196130860d4a23cc7
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-05 23:33:30 +0100

    [client] Push code: apps/quarkloop-client/app/(platform)/org/[orgId]/[workspaceId]/[projectId]/services/page.tsx

---
+++ apps/quarkloop-client/app/(platform)/org/[orgId]/[workspaceId]/[projectId]/services/page.tsx
@@ -0,0 +1,21 @@
+import { ServiceList } from "@quarkloop/components";
+
+interface PageProps {
+    params: {
+        orgId: string;
+        workspaceId: string;
+        projectId: string;
+    };
+    searchParams: any;
+}
+
+const Page = async (props: PageProps) => {
+    const {
+        params: { orgId, workspaceId, projectId },
+        searchParams,
+    } = props;
+
+    return <ServiceList />;
+};
+
+export default Page;
