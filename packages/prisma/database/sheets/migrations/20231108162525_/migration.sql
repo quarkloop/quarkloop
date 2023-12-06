5d15ec1c4135a6c9897baafbd136bb3e1a8dcd8d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-06 22:37:21 +0100

    [prisma] remove sheets: packages/prisma/database/sheets/migrations/20231108162525_/migration.sql

--- packages/prisma/database/sheets/migrations/20231108162525_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `rows` on the `SheetInstance` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "app"."SheetInstance" DROP COLUMN "rows",
-ADD COLUMN     "rows" JSONB NOT NULL;
