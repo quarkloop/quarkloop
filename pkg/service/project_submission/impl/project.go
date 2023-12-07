a383857aaaf7b9b65dad0950485224389357e0dd
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-07 23:30:48 +0100

    Update: pkg/service/project_submission/impl/project.go

--- pkg/service/project_submission/impl/project.go
+++ pkg/service/project_submission/impl/project.go
@@ -10,33 +10,33 @@ type projectSubmission struct {
 	dataStore *repository.Repository
 }
 
-func NewProjectSubmissionService(ds *repository.Repository) project_submission.Service {
+func NewAppSubmissionService(ds *repository.Repository) project_submission.Service {
 	return &projectSubmission{
 		dataStore: ds,
 	}
 }
 
-func (s *projectSubmission) GetProjectSubmissionList(p *project_submission.GetProjectSubmissionListParams) ([]model.ProjectSubmission, error) {
-	projectList, err := s.dataStore.ListProjectSubmissions(p.Context, p.ProjectId)
+func (s *projectSubmission) GetAppSubmissionList(p *project_submission.GetAppSubmissionListParams) ([]model.AppSubmission, error) {
+	projectList, err := s.dataStore.ListAppSubmissions(p.Context, p.ProjectId)
 	return projectList, err
 }
 
-func (s *projectSubmission) GetProjectSubmissionById(p *project_submission.GetProjectSubmissionByIdParams) (*model.ProjectSubmission, error) {
-	project, err := s.dataStore.FindUniqueProjectSubmission(p.Context, p.ProjectId, p.ProjectSubmissionId)
+func (s *projectSubmission) GetAppSubmissionById(p *project_submission.GetAppSubmissionByIdParams) (*model.AppSubmission, error) {
+	project, err := s.dataStore.FindUniqueAppSubmission(p.Context, p.ProjectId, p.AppSubmissionId)
 	return project, err
 }
 
-func (s *projectSubmission) CreateProjectSubmission(p *project_submission.CreateProjectSubmissionParams) (*model.ProjectSubmission, error) {
-	project, err := s.dataStore.CreateProjectSubmission(p.Context, p.UserId, p.ProjectId, p.ProjectSubmission)
+func (s *projectSubmission) CreateAppSubmission(p *project_submission.CreateAppSubmissionParams) (*model.AppSubmission, error) {
+	project, err := s.dataStore.CreateAppSubmission(p.Context, p.UserId, p.ProjectId, p.AppSubmission)
 	return project, err
 }
 
-func (s *projectSubmission) UpdateProjectSubmissionById(p *project_submission.UpdateProjectSubmissionByIdParams) error {
-	err := s.dataStore.UpdateProjectSubmissionById(p.Context, p.ProjectSubmissionId, p.ProjectSubmission)
+func (s *projectSubmission) UpdateAppSubmissionById(p *project_submission.UpdateAppSubmissionByIdParams) error {
+	err := s.dataStore.UpdateAppSubmissionById(p.Context, p.AppSubmissionId, p.AppSubmission)
 	return err
 }
 
-func (s *projectSubmission) DeleteProjectSubmissionById(p *project_submission.DeleteProjectSubmissionByIdParams) error {
-	err := s.dataStore.DeleteProjectSubmissionById(p.Context, p.ProjectSubmissionId)
+func (s *projectSubmission) DeleteAppSubmissionById(p *project_submission.DeleteAppSubmissionByIdParams) error {
+	err := s.dataStore.DeleteAppSubmissionById(p.Context, p.AppSubmissionId)
 	return err
 }
