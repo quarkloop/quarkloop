30e46d3c509870d2379fa92ac9e5c7e5a223293e
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-13 00:50:42 +0100

    Add: pkg/api/project_table_record/table_record.go

---
+++ pkg/api/project_table_record/table_record.go
@@ -0,0 +1,24 @@
+package table_record
+
+import (
+	"github.com/gin-gonic/gin"
+	table_record "github.com/quarkloop/quarkloop/pkg/service/project_table_record"
+)
+
+type Api interface {
+	ListTableRecords(c *gin.Context)
+	GetTableRecordById(c *gin.Context)
+	CreateTableRecord(c *gin.Context)
+	UpdateTableRecordById(c *gin.Context)
+	DeleteTableRecordById(c *gin.Context)
+}
+
+type TableRecordApi struct {
+	tableRecord table_record.Service
+}
+
+func NewTableRecordApi(service table_record.Service) *TableRecordApi {
+	return &TableRecordApi{
+		tableRecord: service,
+	}
+}
