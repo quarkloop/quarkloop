a6e821a70ebd6f182a6572a5dcf340275750ecd4
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-13 00:49:33 +0100

    Add: pkg/service/project_table_branch/model.go

---
+++ pkg/service/project_table_branch/model.go
@@ -0,0 +1,41 @@
+package table_branch
+
+import (
+	"context"
+
+	"github.com/quarkloop/quarkloop/pkg/model"
+)
+
+type GetTableListParams struct {
+	Context   context.Context
+	ProjectId string
+}
+
+type GetTableByIdParams struct {
+	Context   context.Context
+	ProjectId string
+	TableId   string
+}
+
+type CreateTableParams struct {
+	Context   context.Context
+	ProjectId string
+	Table     *model.Table
+}
+
+type CreateBulkTableParams struct {
+	Context   context.Context
+	ProjectId string
+	TableList []model.Table
+}
+
+type UpdateTableByIdParams struct {
+	Context context.Context
+	TableId string
+	Table   *model.Table
+}
+
+type DeleteTableByIdParams struct {
+	Context context.Context
+	TableId string
+}
