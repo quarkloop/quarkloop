0c02aa4478ba40c5992f6f19696470f303daaadb
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-13 23:46:43 +0100

    Update: pkg/service/project_table_record/impl/project.go

--- pkg/service/project_table_record/impl/project.go
+++ pkg/service/project_table_record/impl/project.go
@@ -1,4 +1,4 @@
-package project_table_impl
+package table_record_impl
 
 import (
 	"github.com/quarkloop/quarkloop/pkg/model"
@@ -6,42 +6,42 @@ import (
 	"github.com/quarkloop/quarkloop/pkg/store/repository"
 )
 
-type tableService struct {
+type tableRecordService struct {
 	dataStore *repository.Repository
 }
 
-func NewTableService(ds *repository.Repository) table_record.Service {
-	return &tableService{
+func NewTableRecordService(ds *repository.Repository) table_record.Service {
+	return &tableRecordService{
 		dataStore: ds,
 	}
 }
 
-func (s *tableService) ListTableRecords(p *table_record.GetTableListParams) ([]model.TableWithRelationCount, error) {
-	projectList, err := s.dataStore.ListTableRecords(p.Context, p.ProjectId)
-	return projectList, err
+func (s *tableRecordService) ListTableRecords(p *table_record.GetTableListParams) ([]model.TableWithRelationCount, error) {
+	recordList, err := s.dataStore.ListTableRecords(p.Context, p.ProjectId)
+	return recordList, err
 }
 
-func (s *tableService) GetTableRecordById(p *table_record.GetTableByIdParams) (*model.TableWithRelationCount, error) {
-	project, err := s.dataStore.GetTableRecordById(p.Context, p.ProjectId, p.TableId)
-	return project, err
+func (s *tableRecordService) GetTableRecordById(p *table_record.GetTableByIdParams) (*model.TableWithRelationCount, error) {
+	record, err := s.dataStore.GetTableRecordById(p.Context, p.ProjectId, p.TableId)
+	return record, err
 }
 
-func (s *tableService) CreateTable(p *table_record.CreateTableParams) (*model.Table, error) {
-	project, err := s.dataStore.CreateTable(p.Context, p.ProjectId, p.Table)
-	return project, err
+func (s *tableRecordService) CreateTable(p *table_record.CreateTableParams) (*model.Table, error) {
+	record, err := s.dataStore.CreateTable(p.Context, p.ProjectId, p.Table)
+	return record, err
 }
 
-func (s *tableService) CreateBulkTable(p *table_record.CreateBulkTableParams) (int64, error) {
-	project, err := s.dataStore.CreateBulkTable(p.Context, p.ProjectId, p.TableList)
-	return project, err
+func (s *tableRecordService) CreateBulkTable(p *table_record.CreateBulkTableParams) (int64, error) {
+	record, err := s.dataStore.CreateBulkTable(p.Context, p.ProjectId, p.TableList)
+	return record, err
 }
 
-func (s *tableService) UpdateTableById(p *table_record.UpdateTableByIdParams) error {
+func (s *tableRecordService) UpdateTableById(p *table_record.UpdateTableByIdParams) error {
 	err := s.dataStore.UpdateTableById(p.Context, p.TableId, p.Table)
 	return err
 }
 
-func (s *tableService) DeleteTableById(p *table_record.DeleteTableByIdParams) error {
+func (s *tableRecordService) DeleteTableById(p *table_record.DeleteTableByIdParams) error {
 	err := s.dataStore.DeleteTableById(p.Context, p.TableId)
 	return err
 }
