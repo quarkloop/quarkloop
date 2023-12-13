1aacecdc76661504f72593e9760cbfac5c005180
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-13 23:46:43 +0100

    Update: pkg/service/project_table_branch/table_branch.go

--- pkg/service/project_table_branch/table_branch.go
+++ pkg/service/project_table_branch/table_branch.go
@@ -3,9 +3,9 @@ package table_branch
 import "github.com/quarkloop/quarkloop/pkg/model"
 
 type Service interface {
-	ListTableBranches(*GetTableListParams) ([]model.TableWithRelationCount, error)
-	GetTableBranchById(*GetTableByIdParams) (*model.TableWithRelationCount, error)
-	CreateTableBranch(*CreateTableParams) (*model.Table, error)
-	UpdateTableBranchById(*UpdateTableByIdParams) error
-	DeleteTableBranchById(*DeleteTableByIdParams) error
+	ListTableBranches(*GetTableBranchListParams) ([]model.TableBranch, error)
+	GetTableBranchById(*GetTableBranchByIdParams) (*model.TableBranch, error)
+	CreateTableBranch(*CreateTableBranchParams) (*model.TableBranch, error)
+	UpdateTableBranchById(*UpdateTableBranchByIdParams) error
+	DeleteTableBranchById(*DeleteTableBranchByIdParams) error
 }
