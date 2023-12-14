e65c1d81ef3de742eb32a8d350828467ac828640
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-14 23:29:54 +0100

    Update: pkg/api/project_table_schema/table_schema.go

--- pkg/api/project_table_schema/table_schema.go
+++ pkg/api/project_table_schema/table_schema.go
@@ -6,19 +6,19 @@ import (
 )
 
 type Api interface {
-	ListTableRecords(c *gin.Context)
-	GetTableRecordById(c *gin.Context)
+	ListTableSchemas(c *gin.Context)
+	GetTableSchemaById(c *gin.Context)
 	CreateTableSchema(c *gin.Context)
 	UpdateTableSchemaById(c *gin.Context)
 	DeleteTableSchemaById(c *gin.Context)
 }
 
 type TableSchemaApi struct {
-	tableSchema table_schema.Service
+	tableSchemaService table_schema.Service
 }
 
 func NewTableSchemaApi(service table_schema.Service) *TableSchemaApi {
 	return &TableSchemaApi{
-		tableSchema: service,
+		tableSchemaService: service,
 	}
 }
