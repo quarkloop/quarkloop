4e4ab9d0866daf0414077b48b998aebfe34d96fe
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-14 23:29:54 +0100

    Update: pkg/service/project_table_record/table_record.go

--- pkg/service/project_table_record/table_record.go
+++ pkg/service/project_table_record/table_record.go
@@ -1,12 +1,9 @@
 package table_record
 
-import "github.com/quarkloop/quarkloop/pkg/model"
-
 type Service interface {
-	ListTableRecords(*GetTableListParams) ([]model.TableWithRelationCount, error)
-	GetTableRecordById(*GetTableByIdParams) (*model.TableWithRelationCount, error)
-	CreateTable(*CreateTableParams) (*model.Table, error)
-	CreateBulkTable(*CreateBulkTableParams) (int64, error)
-	UpdateTableById(*UpdateTableByIdParams) error
-	DeleteTableById(*DeleteTableByIdParams) error
+	ListTableRecords(*GetTableRecordListParams) (interface{}, error)
+	GetTableRecordById(*GetTableRecordByIdParams) (interface{}, error)
+	CreateTableRecord(*CreateTableRecordParams) (interface{}, error)
+	UpdateTableRecordById(*UpdateTableRecordByIdParams) error
+	DeleteTableRecordById(*DeleteTableRecordByIdParams) error
 }
