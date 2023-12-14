471696d4963895d84a0ddc611b495b08c7480f9d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-14 23:29:54 +0100

    Update: packages/prisma/database/system/scripts/system.down_genenerated.sql

--- packages/prisma/database/system/scripts/system.down_genenerated.sql
+++ packages/prisma/database/system/scripts/system.down_genenerated.sql
@@ -7,18 +7,6 @@ ALTER TABLE "system"."Project" DROP CONSTRAINT "Project_orgId_fkey";
 -- DropForeignKey
 ALTER TABLE "system"."Project" DROP CONSTRAINT "Project_workspaceId_fkey";
 
--- DropForeignKey
-ALTER TABLE "system"."ProjectService" DROP CONSTRAINT "ProjectService_projectId_fkey";
-
--- DropForeignKey
-ALTER TABLE "system"."ProjectSubmission" DROP CONSTRAINT "ProjectSubmission_projectId_fkey";
-
--- DropForeignKey
-ALTER TABLE "system"."ProjectDiscussion" DROP CONSTRAINT "ProjectDiscussion_projectId_fkey";
-
--- DropForeignKey
-ALTER TABLE "system"."ProjectForm" DROP CONSTRAINT "ProjectForm_projectId_fkey";
-
 -- DropTable
 DROP TABLE "system"."Organization";
 
@@ -28,27 +16,17 @@ DROP TABLE "system"."Workspace";
 -- DropTable
 DROP TABLE "system"."Project";
 
--- DropTable
-DROP TABLE "system"."ProjectService";
-
--- DropTable
-DROP TABLE "system"."ProjectSubmission";
-
--- DropTable
-DROP TABLE "system"."ProjectDiscussion";
-
--- DropTable
-DROP TABLE "system"."ProjectForm";
-
 -- CreateTable
 CREATE TABLE "Organization" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "accessType" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
+    "createdBy" TEXT NOT NULL,
+    "metadata" JSONB,
+    "updatedBy" TEXT,
 
     CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
 );
@@ -59,72 +37,15 @@ CREATE TABLE "Project" (
     "orgId" TEXT NOT NULL,
     "workspaceId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "accessType" INTEGER NOT NULL,
     "metadata" JSONB,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
+    "createdBy" TEXT NOT NULL,
+    "updatedBy" TEXT,
 
     CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ProjectDiscussion" (
-    "id" SERIAL NOT NULL,
-    "projectId" INTEGER NOT NULL,
-    "userId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "metadata" JSONB,
-    "data" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-
-    CONSTRAINT "ProjectDiscussion_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ProjectForm" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "metadata" JSONB,
-    "data" JSONB NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-    "projectId" TEXT NOT NULL,
-
-    CONSTRAINT "ProjectForm_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ProjectService" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "type" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
-    "metadata" JSONB,
-    "data" JSONB NOT NULL,
-    "isBuiltin" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-    "projectId" TEXT NOT NULL,
-
-    CONSTRAINT "ProjectService_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ProjectSubmission" (
-    "id" SERIAL NOT NULL,
-    "projectId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "metadata" JSONB,
-    "data" JSONB NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-
-    CONSTRAINT "ProjectSubmission_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -132,44 +53,37 @@ CREATE TABLE "Workspace" (
     "id" TEXT NOT NULL,
     "orgId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "accessType" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
+    "createdBy" TEXT NOT NULL,
+    "metadata" JSONB,
+    "updatedBy" TEXT,
 
     CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Organization_path_key" ON "Organization"("path" ASC);
+CREATE INDEX "Project_orgId_idx" ON "Project"("orgId" ASC);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Project_path_key" ON "Project"("path" ASC);
+CREATE UNIQUE INDEX "Project_orgId_workspaceId_id_key" ON "Project"("orgId" ASC, "workspaceId" ASC, "id" ASC);
+
+-- CreateIndex
+CREATE INDEX "Project_orgId_workspaceId_idx" ON "Project"("orgId" ASC, "workspaceId" ASC);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Workspace_orgId_id_key" ON "Workspace"("orgId" ASC, "id" ASC);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Workspace_path_key" ON "Workspace"("path" ASC);
+CREATE INDEX "Workspace_orgId_idx" ON "Workspace"("orgId" ASC);
 
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ProjectDiscussion" ADD CONSTRAINT "ProjectDiscussion_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "ProjectSubmission"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ProjectForm" ADD CONSTRAINT "ProjectForm_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ProjectService" ADD CONSTRAINT "ProjectService_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ProjectSubmission" ADD CONSTRAINT "ProjectSubmission_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
