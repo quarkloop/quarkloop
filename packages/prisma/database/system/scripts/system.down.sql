3c7a477d7a2096eb7a14aa04ee5d1cd79c53876a
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-14 23:29:53 +0100

    Update: packages/prisma/database/system/scripts/system.down.sql

--- packages/prisma/database/system/scripts/system.down.sql
+++ packages/prisma/database/system/scripts/system.down.sql
@@ -10,22 +10,6 @@ DROP CONSTRAINT "Project_orgId_fkey";
 ALTER TABLE "system"."Project"
 DROP CONSTRAINT "Project_workspaceId_fkey";
 
--- DropForeignKey
-ALTER TABLE "system"."ProjectService"
-DROP CONSTRAINT "ProjectService_projectId_fkey";
-
--- DropForeignKey
-ALTER TABLE "system"."ProjectSubmission"
-DROP CONSTRAINT "ProjectSubmission_projectId_fkey";
-
--- DropForeignKey
-ALTER TABLE "system"."ProjectDiscussion"
-DROP CONSTRAINT "ProjectDiscussion_projectId_fkey";
-
--- DropForeignKey
-ALTER TABLE "system"."ProjectForm"
-DROP CONSTRAINT "ProjectForm_projectId_fkey";
-
 -- DropTable
 DROP TABLE "system"."Organization";
 
@@ -34,18 +18,6 @@ DROP TABLE "system"."Workspace";
 
 -- DropTable
 DROP TABLE "system"."Project";
-
--- DropTable
-DROP TABLE "system"."ProjectService";
-
--- DropTable
-DROP TABLE "system"."ProjectSubmission";
-
--- DropTable
-DROP TABLE "system"."ProjectDiscussion";
-
--- DropTable
-DROP TABLE "system"."ProjectForm";
 
 -- DropSchema
 DROP SCHEMA IF EXISTS "system";
