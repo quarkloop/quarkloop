e9c3801707a6dd75045a293e76a7cb2fb4b73a27
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-15 23:44:40 +0100

    Update: pkg/service/project_table_record/model.go

--- pkg/service/project_table_record/model.go
+++ pkg/service/project_table_record/model.go
@@ -7,31 +7,31 @@ import (
 type GetTableRecordListParams struct {
 	Context   context.Context
 	TableType string
-	ProjectId string
-	BranchId  string
+	ProjectId int
+	BranchId  int
 }
 
 type GetTableRecordByIdParams struct {
 	Context   context.Context
 	TableType string
-	ProjectId string
-	BranchId  string
+	ProjectId int
+	BranchId  int
 	RecordId  string
 }
 
 type CreateTableRecordParams struct {
 	Context   context.Context
 	TableType string
-	ProjectId string
-	BranchId  string
+	ProjectId int
+	BranchId  int
 	Record    interface{}
 }
 
 type UpdateTableRecordByIdParams struct {
 	Context   context.Context
 	TableType string
-	ProjectId string
-	BranchId  string
+	ProjectId int
+	BranchId  int
 	RecordId  string
 	Record    interface{}
 }
@@ -39,7 +39,7 @@ type UpdateTableRecordByIdParams struct {
 type DeleteTableRecordByIdParams struct {
 	Context   context.Context
 	TableType string
-	ProjectId string
-	BranchId  string
+	ProjectId int
+	BranchId  int
 	RecordId  string
 }
