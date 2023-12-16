9cfba7c1d1317824a9c28a1a307abdcd8abab893
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-16 22:43:42 +0100

    Update: pkg/service/workspace/model.go

--- pkg/service/workspace/model.go
+++ pkg/service/workspace/model.go
@@ -2,9 +2,33 @@ package workspace
 
 import (
 	"context"
-
-	"github.com/quarkloop/quarkloop/pkg/model"
+	"fmt"
+	"time"
 )
+
+type Workspace struct {
+	// id
+	Id          int    `json:"id" form:"id"`
+	ScopedId    string `json:"sid"`
+	OrgId       int    `json:"orgId"`
+	OrgScopedId string `json:"orgScopedId"`
+
+	// data
+	Name        string `json:"name,omitempty" form:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	AccessType  *int   `json:"accessType,omitempty" form:"accessType,omitempty"`
+	Path        string `json:"path,omitempty"`
+
+	// history
+	CreatedAt time.Time  `json:"createdAt,omitempty" form:"createdAt,omitempty"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty" form:"updatedAt,omitempty"`
+	CreatedBy string     `json:"createdBy,omitempty"`
+	UpdatedBy *string    `json:"updatedBy,omitempty"`
+}
+
+func (w *Workspace) GeneratePath() {
+	w.Path = fmt.Sprintf("/org/%s/%s", w.OrgScopedId, w.ScopedId)
+}
 
 type GetWorkspaceListParams struct {
 	Context context.Context
@@ -19,19 +43,19 @@ type GetWorkspaceByIdParams struct {
 type GetWorkspaceParams struct {
 	Context   context.Context
 	OrgId     int
-	Workspace model.Workspace
+	Workspace Workspace
 }
 
 type CreateWorkspaceParams struct {
 	Context   context.Context
 	OrgId     int
-	Workspace model.Workspace
+	Workspace Workspace
 }
 
 type UpdateWorkspaceByIdParams struct {
 	Context     context.Context
 	WorkspaceId int
-	Workspace   model.Workspace
+	Workspace   Workspace
 }
 
 type DeleteWorkspaceByIdParams struct {
