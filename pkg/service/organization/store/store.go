357035f4e2210876a67a2a7d6f2c6d812b58b438
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-16 22:48:21 +0100

    Add: pkg/service/organization/store/store.go

---
+++ pkg/service/organization/store/store.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	org "github.com/quarkloop/quarkloop/pkg/service/organization"
+)
+
+type OrgStore interface {
+	ListOrganizations(ctx context.Context) ([]org.Organization, error)
+	GetOrganizationById(ctx context.Context, orgId int) (*org.Organization, error)
+	GetOrganization(ctx context.Context, org *org.Organization) (*org.Organization, error)
+	CreateOrganization(ctx context.Context, org *org.Organization) (*org.Organization, error)
+	UpdateOrganizationById(ctx context.Context, orgId int, org *org.Organization) error
+	DeleteOrganizationById(ctx context.Context, orgId int) error
+}
+
+type orgStore struct {
+	Conn *pgx.Conn
+}
+
+func NewOrgStore(conn *pgx.Conn) *orgStore {
+	return &orgStore{
+		Conn: conn,
+	}
+}
