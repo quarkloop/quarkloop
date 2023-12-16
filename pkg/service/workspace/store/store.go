04f30e39e3a677de7cbb5e5b3a2bcfc6b1ce7ef1
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-16 22:48:21 +0100

    Add: pkg/service/workspace/store/store.go

---
+++ pkg/service/workspace/store/store.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/quarkloop/quarkloop/pkg/service/workspace"
+)
+
+type WorkspaceStore interface {
+	ListWorkspaces(ctx context.Context, orgId []int) ([]workspace.Workspace, error)
+	GetWorkspaceById(ctx context.Context, workspaceId int) (*workspace.Workspace, error)
+	GetWorkspace(ctx context.Context, orgId int, workspace *workspace.Workspace) (*workspace.Workspace, error)
+	CreateWorkspace(ctx context.Context, orgId int, workspace *workspace.Workspace) (*workspace.Workspace, error)
+	UpdateWorkspaceById(ctx context.Context, workspaceId int, workspace *workspace.Workspace) error
+	DeleteWorkspaceById(ctx context.Context, workspaceId int) error
+}
+
+type workspaceStore struct {
+	Conn *pgx.Conn
+}
+
+func NewWorkspaceStore(conn *pgx.Conn) *workspaceStore {
+	return &workspaceStore{
+		Conn: conn,
+	}
+}
