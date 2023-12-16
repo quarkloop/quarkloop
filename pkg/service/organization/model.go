b900eed21ea2066555377a6d8dc0b463d5700f92
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-16 22:43:43 +0100

    Update: pkg/service/organization/model.go

--- pkg/service/organization/model.go
+++ pkg/service/organization/model.go
@@ -1,10 +1,32 @@
-package organization
+package org
 
 import (
 	"context"
-
-	"github.com/quarkloop/quarkloop/pkg/model"
+	"fmt"
+	"time"
 )
+
+type Organization struct {
+	// id
+	Id       int    `json:"id" form:"id"`
+	ScopedId string `json:"sid"`
+
+	// data
+	Name        string `json:"name,omitempty" form:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	AccessType  *int   `json:"accessType,omitempty" form:"accessType,omitempty"`
+	Path        string `json:"path,omitempty"`
+
+	// history
+	CreatedAt time.Time  `json:"createdAt,omitempty" form:"createdAt,omitempty"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty" form:"updatedAt,omitempty"`
+	CreatedBy string     `json:"createdBy,omitempty"`
+	UpdatedBy *string    `json:"updatedBy,omitempty"`
+}
+
+func (o *Organization) GeneratePath() {
+	o.Path = fmt.Sprintf("/org/%s", o.ScopedId)
+}
 
 type GetOrganizationListParams struct {
 	Context context.Context
@@ -17,18 +39,18 @@ type GetOrganizationByIdParams struct {
 
 type GetOrganizationParams struct {
 	Context      context.Context
-	Organization model.Organization
+	Organization Organization
 }
 
 type CreateOrganizationParams struct {
 	Context      context.Context
-	Organization model.Organization
+	Organization Organization
 }
 
 type UpdateOrganizationByIdParams struct {
 	Context      context.Context
 	OrgId        int
-	Organization model.Organization
+	Organization Organization
 }
 
 type DeleteOrganizationByIdParams struct {
