96da9a1c8847123f5fc7733a869a9def2e6e3e99
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-16 22:43:42 +0100

    Update: pkg/service/workspace/workspace.go

--- pkg/service/workspace/workspace.go
+++ pkg/service/workspace/workspace.go
@@ -1,12 +1,10 @@
 package workspace
 
-import "github.com/quarkloop/quarkloop/pkg/model"
-
 type Service interface {
-	GetWorkspaceList(*GetWorkspaceListParams) ([]model.Workspace, error)
-	GetWorkspaceById(*GetWorkspaceByIdParams) (*model.Workspace, error)
-	GetWorkspace(*GetWorkspaceParams) (*model.Workspace, error)
-	CreateWorkspace(*CreateWorkspaceParams) (*model.Workspace, error)
+	GetWorkspaceList(*GetWorkspaceListParams) ([]Workspace, error)
+	GetWorkspaceById(*GetWorkspaceByIdParams) (*Workspace, error)
+	GetWorkspace(*GetWorkspaceParams) (*Workspace, error)
+	CreateWorkspace(*CreateWorkspaceParams) (*Workspace, error)
 	UpdateWorkspaceById(*UpdateWorkspaceByIdParams) error
 	DeleteWorkspaceById(*DeleteWorkspaceByIdParams) error
 }
