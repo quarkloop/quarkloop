36d6b0be5ba9735d849fc0aea86b49fbe1fccc7b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:14 +0100

    Improve context usage: pkg/service/project_submission/impl/project.go

--- pkg/service/project_submission/impl/project.go
+++ pkg/service/project_submission/impl/project.go
@@ -1,6 +1,8 @@
 package project_submission_impl
 
 import (
+	"context"
+
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/project_submission"
 	"github.com/quarkloop/quarkloop/pkg/store/repository"
@@ -16,27 +18,27 @@ func NewAppSubmissionService(ds *repository.Repository) project_submission.Servi
 	}
 }
 
-func (s *projectSubmission) GetAppSubmissionList(p *project_submission.GetAppSubmissionListParams) ([]model.AppSubmission, error) {
-	projectList, err := s.store.ListAppSubmissions(p.Context, p.ProjectId)
+func (s *projectSubmission) GetAppSubmissionList(ctx context.Context, p *project_submission.GetAppSubmissionListParams) ([]model.AppSubmission, error) {
+	projectList, err := s.store.ListAppSubmissions(ctx, p.ProjectId)
 	return projectList, err
 }
 
-func (s *projectSubmission) GetAppSubmissionById(p *project_submission.GetAppSubmissionByIdParams) (*model.AppSubmission, error) {
-	project, err := s.store.GetAppSubmissionById(p.Context, p.ProjectId, p.AppSubmissionId)
+func (s *projectSubmission) GetAppSubmissionById(ctx context.Context, p *project_submission.GetAppSubmissionByIdParams) (*model.AppSubmission, error) {
+	project, err := s.store.GetAppSubmissionById(ctx, p.ProjectId, p.AppSubmissionId)
 	return project, err
 }
 
-func (s *projectSubmission) CreateAppSubmission(p *project_submission.CreateAppSubmissionParams) (*model.AppSubmission, error) {
-	project, err := s.store.CreateAppSubmission(p.Context, p.UserId, p.ProjectId, p.AppSubmission)
+func (s *projectSubmission) CreateAppSubmission(ctx context.Context, p *project_submission.CreateAppSubmissionParams) (*model.AppSubmission, error) {
+	project, err := s.store.CreateAppSubmission(ctx, p.UserId, p.ProjectId, p.AppSubmission)
 	return project, err
 }
 
-func (s *projectSubmission) UpdateAppSubmissionById(p *project_submission.UpdateAppSubmissionByIdParams) error {
-	err := s.store.UpdateAppSubmissionById(p.Context, p.AppSubmissionId, p.AppSubmission)
+func (s *projectSubmission) UpdateAppSubmissionById(ctx context.Context, p *project_submission.UpdateAppSubmissionByIdParams) error {
+	err := s.store.UpdateAppSubmissionById(ctx, p.AppSubmissionId, p.AppSubmission)
 	return err
 }
 
-func (s *projectSubmission) DeleteAppSubmissionById(p *project_submission.DeleteAppSubmissionByIdParams) error {
-	err := s.store.DeleteAppSubmissionById(p.Context, p.AppSubmissionId)
+func (s *projectSubmission) DeleteAppSubmissionById(ctx context.Context, p *project_submission.DeleteAppSubmissionByIdParams) error {
+	err := s.store.DeleteAppSubmissionById(ctx, p.AppSubmissionId)
 	return err
 }
