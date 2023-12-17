024864da45fb28f883d5496b515e4bba718f0307
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 10:29:36 +0100

    Delete: pkg/service/project_table_record/table_record.go

--- pkg/service/project_table_record/table_record.go
+++
@@ -1,9 +0,0 @@
-package table_record
-
-type Service interface {
-	ListTableRecords(*GetTableRecordListParams) (interface{}, error)
-	GetTableRecordById(*GetTableRecordByIdParams) (interface{}, error)
-	CreateTableRecord(*CreateTableRecordParams) (interface{}, error)
-	UpdateTableRecordById(*UpdateTableRecordByIdParams) error
-	DeleteTableRecordById(*DeleteTableRecordByIdParams) error
-}
