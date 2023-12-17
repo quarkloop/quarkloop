9d2813a381db945699dc0151a554f7539b55f9e0
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:15 +0100

    Improve context usage: pkg/service/table_branch/impl/project.go

--- pkg/service/table_branch/impl/project.go
+++ pkg/service/table_branch/impl/project.go
@@ -1,6 +1,8 @@
 package table_branch_impl
 
 import (
+	"context"
+
 	"github.com/quarkloop/quarkloop/pkg/service/table_branch"
 	"github.com/quarkloop/quarkloop/pkg/service/table_branch/store"
 )
@@ -15,8 +17,8 @@ func NewTableBranchService(ds store.TableBranchStore) table_branch.Service {
 	}
 }
 
-func (s *tableBranchService) ListTableBranches(p *table_branch.GetTableBranchListParams) ([]table_branch.TableBranch, error) {
-	branchList, err := s.store.ListTableBranches(p.Context, p.ProjectId)
+func (s *tableBranchService) ListTableBranches(ctx context.Context, p *table_branch.GetTableBranchListParams) ([]table_branch.TableBranch, error) {
+	branchList, err := s.store.ListTableBranches(ctx, p.ProjectId)
 	if err != nil {
 		return nil, err
 	}
@@ -24,8 +26,8 @@ func (s *tableBranchService) ListTableBranches(p *table_branch.GetTableBranchLis
 	return branchList, nil
 }
 
-func (s *tableBranchService) GetTableBranchById(p *table_branch.GetTableBranchByIdParams) (*table_branch.TableBranch, error) {
-	branch, err := s.store.GetTableBranchById(p.Context, p.ProjectId, p.BranchId)
+func (s *tableBranchService) GetTableBranchById(ctx context.Context, p *table_branch.GetTableBranchByIdParams) (*table_branch.TableBranch, error) {
+	branch, err := s.store.GetTableBranchById(ctx, p.ProjectId, p.BranchId)
 	if err != nil {
 		return nil, err
 	}
@@ -33,8 +35,8 @@ func (s *tableBranchService) GetTableBranchById(p *table_branch.GetTableBranchBy
 	return branch, nil
 }
 
-func (s *tableBranchService) CreateTableBranch(p *table_branch.CreateTableBranchParams) (*table_branch.TableBranch, error) {
-	branch, err := s.store.CreateTableBranch(p.Context, p.ProjectId, p.Branch)
+func (s *tableBranchService) CreateTableBranch(ctx context.Context, p *table_branch.CreateTableBranchParams) (*table_branch.TableBranch, error) {
+	branch, err := s.store.CreateTableBranch(ctx, p.ProjectId, p.Branch)
 	if err != nil {
 		return nil, err
 	}
@@ -42,12 +44,12 @@ func (s *tableBranchService) CreateTableBranch(p *table_branch.CreateTableBranch
 	return branch, nil
 }
 
-func (s *tableBranchService) UpdateTableBranchById(p *table_branch.UpdateTableBranchByIdParams) error {
-	err := s.store.UpdateTableBranchById(p.Context, p.ProjectId, p.BranchId, p.Branch)
+func (s *tableBranchService) UpdateTableBranchById(ctx context.Context, p *table_branch.UpdateTableBranchByIdParams) error {
+	err := s.store.UpdateTableBranchById(ctx, p.ProjectId, p.BranchId, p.Branch)
 	return err
 }
 
-func (s *tableBranchService) DeleteTableBranchById(p *table_branch.DeleteTableBranchByIdParams) error {
-	err := s.store.DeleteTableBranchById(p.Context, p.ProjectId, p.BranchId)
+func (s *tableBranchService) DeleteTableBranchById(ctx context.Context, p *table_branch.DeleteTableBranchByIdParams) error {
+	err := s.store.DeleteTableBranchById(ctx, p.ProjectId, p.BranchId)
 	return err
 }
