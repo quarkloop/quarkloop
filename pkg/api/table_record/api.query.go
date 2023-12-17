80e05ee059e44ce8be0b2d0e8b78780a32d289b3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:10 +0100

    Improve context usage: pkg/api/table_record/api.query.go

--- pkg/api/table_record/api.query.go
+++ pkg/api/table_record/api.query.go
@@ -14,28 +14,26 @@ type ListTableRecordsUriParams struct {
 	TableType string `uri:"tableType" binding:"required"`
 }
 
-func (s *TableRecordApi) ListTableRecords(c *gin.Context) {
+func (s *TableRecordApi) ListTableRecords(ctx *gin.Context) {
 	uriParams := &ListTableRecordsUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	recordList, err := s.tableRecordService.ListTableRecords(
-		&table_record.GetTableRecordListParams{
-			Context:   c,
-			ProjectId: uriParams.ProjectId,
-			BranchId:  uriParams.BranchId,
-			TableType: uriParams.TableType,
-		},
+	recordList, err := s.tableRecordService.ListTableRecords(ctx, &table_record.GetTableRecordListParams{
+		ProjectId: uriParams.ProjectId,
+		BranchId:  uriParams.BranchId,
+		TableType: uriParams.TableType,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, &recordList)
+	ctx.JSON(http.StatusOK, &recordList)
 }
 
 type GetTableRecordByIdUriParams struct {
@@ -44,26 +42,24 @@ type GetTableRecordByIdUriParams struct {
 	RecordId  string `uri:"recordId" binding:"required"`
 }
 
-func (s *TableRecordApi) GetTableRecordById(c *gin.Context) {
+func (s *TableRecordApi) GetTableRecordById(ctx *gin.Context) {
 	uriParams := &GetTableRecordByIdUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	record, err := s.tableRecordService.GetTableRecordById(
-		&table_record.GetTableRecordByIdParams{
-			Context:   c,
-			ProjectId: uriParams.ProjectId,
-			BranchId:  uriParams.BranchId,
-			RecordId:  uriParams.RecordId,
-		},
+	record, err := s.tableRecordService.GetTableRecordById(ctx, &table_record.GetTableRecordByIdParams{
+		ProjectId: uriParams.ProjectId,
+		BranchId:  uriParams.BranchId,
+		RecordId:  uriParams.RecordId,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, record)
+	ctx.JSON(http.StatusOK, record)
 }
