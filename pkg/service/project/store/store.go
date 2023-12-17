b38562e2b2b286868af26568aa17c8088dffbb4d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 10:32:45 +0100

    Add: pkg/service/project/store/store.go

---
+++ pkg/service/project/store/store.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/quarkloop/quarkloop/pkg/service/project"
+)
+
+type ProjectStore interface {
+	ListProjects(ctx context.Context, orgId []int, workspaceId []int) ([]project.Project, error)
+	GetProjectById(ctx context.Context, projectId int) (*project.Project, error)
+	GetProject(ctx context.Context, p *project.Project) (*project.Project, error)
+	CreateProject(ctx context.Context, orgId int, workspaceId int, p *project.Project) (*project.Project, error)
+	UpdateProjectById(ctx context.Context, projectId int, project *project.Project) error
+	DeleteProjectById(ctx context.Context, projectId int) error
+}
+
+type projectStore struct {
+	Conn *pgx.Conn
+}
+
+func NewProjectStore(conn *pgx.Conn) *projectStore {
+	return &projectStore{
+		Conn: conn,
+	}
+}
