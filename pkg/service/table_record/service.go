e654900bf1ec9af881fcea2f7f24394b817131c8
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:16 +0100

    Improve context usage: pkg/service/table_record/service.go

--- pkg/service/table_record/service.go
+++ pkg/service/table_record/service.go
@@ -1,9 +1,11 @@
 package table_record
 
+import "context"
+
 type Service interface {
-	ListTableRecords(*GetTableRecordListParams) (interface{}, error)
-	GetTableRecordById(*GetTableRecordByIdParams) (interface{}, error)
-	CreateTableRecord(*CreateTableRecordParams) (interface{}, error)
-	UpdateTableRecordById(*UpdateTableRecordByIdParams) error
-	DeleteTableRecordById(*DeleteTableRecordByIdParams) error
+	ListTableRecords(context.Context, *GetTableRecordListParams) (interface{}, error)
+	GetTableRecordById(context.Context, *GetTableRecordByIdParams) (interface{}, error)
+	CreateTableRecord(context.Context, *CreateTableRecordParams) (interface{}, error)
+	UpdateTableRecordById(context.Context, *UpdateTableRecordByIdParams) error
+	DeleteTableRecordById(context.Context, *DeleteTableRecordByIdParams) error
 }
