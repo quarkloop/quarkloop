7e359576d3dd5e279e62792686f2a08ef661fa4b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:12 +0100

    Improve context usage: pkg/api/workspace/api.query.go

--- pkg/api/workspace/api.query.go
+++ pkg/api/workspace/api.query.go
@@ -13,52 +13,48 @@ type GetWorkspaceListQueryParams struct {
 	OrgId []int `form:"orgId"`
 }
 
-func (s *WorkspaceApi) GetWorkspaceList(c *gin.Context) {
+func (s *WorkspaceApi) GetWorkspaceList(ctx *gin.Context) {
 	queryParams := &GetWorkspaceListQueryParams{}
-	if err := c.ShouldBindQuery(queryParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindQuery(queryParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	wsList, err := s.workspaceService.GetWorkspaceList(
-		&workspace.GetWorkspaceListParams{
-			Context: c,
-			OrgId:   queryParams.OrgId,
-		},
+	wsList, err := s.workspaceService.GetWorkspaceList(ctx, &workspace.GetWorkspaceListParams{
+		OrgId: queryParams.OrgId,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, &wsList)
+	ctx.JSON(http.StatusOK, &wsList)
 }
 
 type GetWorkspaceByIdUriParams struct {
 	WorkspaceId int `uri:"workspaceId" binding:"required"`
 }
 
-func (s *WorkspaceApi) GetWorkspaceById(c *gin.Context) {
+func (s *WorkspaceApi) GetWorkspaceById(ctx *gin.Context) {
 	uriParams := &GetWorkspaceByIdUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	ws, err := s.workspaceService.GetWorkspaceById(
-		&workspace.GetWorkspaceByIdParams{
-			Context:     c,
-			WorkspaceId: uriParams.WorkspaceId,
-		},
+	ws, err := s.workspaceService.GetWorkspaceById(ctx, &workspace.GetWorkspaceByIdParams{
+		WorkspaceId: uriParams.WorkspaceId,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, ws)
+	ctx.JSON(http.StatusOK, ws)
 }
 
 type GetWorkspaceQueryParams struct {
@@ -66,25 +62,23 @@ type GetWorkspaceQueryParams struct {
 	workspace.Workspace
 }
 
-func (s *WorkspaceApi) GetWorkspace(c *gin.Context) {
+func (s *WorkspaceApi) GetWorkspace(ctx *gin.Context) {
 	queryParams := &GetWorkspaceQueryParams{}
-	if err := c.ShouldBindQuery(queryParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindQuery(queryParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	ws, err := s.workspaceService.GetWorkspace(
-		&workspace.GetWorkspaceParams{
-			Context:   c,
-			OrgId:     queryParams.OrgId,
-			Workspace: queryParams.Workspace,
-		},
+	ws, err := s.workspaceService.GetWorkspace(ctx, &workspace.GetWorkspaceParams{
+		OrgId:     queryParams.OrgId,
+		Workspace: queryParams.Workspace,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, ws)
+	ctx.JSON(http.StatusOK, ws)
 }
