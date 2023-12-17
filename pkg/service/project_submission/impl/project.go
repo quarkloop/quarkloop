23a341ca8f06750741facec0b49a3e41839fdad0
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 10:30:46 +0100

    Update: pkg/service/project_submission/impl/project.go

--- pkg/service/project_submission/impl/project.go
+++ pkg/service/project_submission/impl/project.go
@@ -7,36 +7,36 @@ import (
 )
 
 type projectSubmission struct {
-	dataStore *repository.Repository
+	store *repository.Repository
 }
 
 func NewAppSubmissionService(ds *repository.Repository) project_submission.Service {
 	return &projectSubmission{
-		dataStore: ds,
+		store: ds,
 	}
 }
 
 func (s *projectSubmission) GetAppSubmissionList(p *project_submission.GetAppSubmissionListParams) ([]model.AppSubmission, error) {
-	projectList, err := s.dataStore.ListAppSubmissions(p.Context, p.ProjectId)
+	projectList, err := s.store.ListAppSubmissions(p.Context, p.ProjectId)
 	return projectList, err
 }
 
 func (s *projectSubmission) GetAppSubmissionById(p *project_submission.GetAppSubmissionByIdParams) (*model.AppSubmission, error) {
-	project, err := s.dataStore.GetAppSubmissionById(p.Context, p.ProjectId, p.AppSubmissionId)
+	project, err := s.store.GetAppSubmissionById(p.Context, p.ProjectId, p.AppSubmissionId)
 	return project, err
 }
 
 func (s *projectSubmission) CreateAppSubmission(p *project_submission.CreateAppSubmissionParams) (*model.AppSubmission, error) {
-	project, err := s.dataStore.CreateAppSubmission(p.Context, p.UserId, p.ProjectId, p.AppSubmission)
+	project, err := s.store.CreateAppSubmission(p.Context, p.UserId, p.ProjectId, p.AppSubmission)
 	return project, err
 }
 
 func (s *projectSubmission) UpdateAppSubmissionById(p *project_submission.UpdateAppSubmissionByIdParams) error {
-	err := s.dataStore.UpdateAppSubmissionById(p.Context, p.AppSubmissionId, p.AppSubmission)
+	err := s.store.UpdateAppSubmissionById(p.Context, p.AppSubmissionId, p.AppSubmission)
 	return err
 }
 
 func (s *projectSubmission) DeleteAppSubmissionById(p *project_submission.DeleteAppSubmissionByIdParams) error {
-	err := s.dataStore.DeleteAppSubmissionById(p.Context, p.AppSubmissionId)
+	err := s.store.DeleteAppSubmissionById(p.Context, p.AppSubmissionId)
 	return err
 }
