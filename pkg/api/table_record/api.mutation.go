bc0823e1e476f10535ba5c54f1f443fe820027f0
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:10 +0100

    Improve context usage: pkg/api/table_record/api.mutation.go

--- pkg/api/table_record/api.mutation.go
+++ pkg/api/table_record/api.mutation.go
@@ -15,44 +15,42 @@ type CreateTableRecordUriParams struct {
 	RecordId  string `uri:"recordId" binding:"required"`
 }
 
-func (s *TableRecordApi) CreateTableRecord(c *gin.Context) {
+func (s *TableRecordApi) CreateTableRecord(ctx *gin.Context) {
 	uriParams := &CreateTableRecordUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	var req interface{}
 	if uriParams.TableType == "main" {
 		req = table_record.MainRecordWithRelationCount{}
-		if err := c.BindJSON(req); err != nil {
-			api.AbortWithBadRequestJSON(c, err)
+		if err := ctx.BindJSON(req); err != nil {
+			api.AbortWithBadRequestJSON(ctx, err)
 			return
 		}
 	} else if uriParams.TableType == "document" {
 		req = table_record.DocumentRecord{}
-		if err := c.BindJSON(req); err != nil {
-			api.AbortWithBadRequestJSON(c, err)
+		if err := ctx.BindJSON(req); err != nil {
+			api.AbortWithBadRequestJSON(ctx, err)
 			return
 		}
 	}
 
 	// query service
-	record, err := s.tableRecordService.CreateTableRecord(
-		&table_record.CreateTableRecordParams{
-			Context:   c,
-			ProjectId: uriParams.ProjectId,
-			BranchId:  uriParams.BranchId,
-			TableType: uriParams.TableType,
-			Record:    req,
-		},
+	record, err := s.tableRecordService.CreateTableRecord(ctx, &table_record.CreateTableRecordParams{
+		ProjectId: uriParams.ProjectId,
+		BranchId:  uriParams.BranchId,
+		TableType: uriParams.TableType,
+		Record:    req,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusCreated, record)
+	ctx.JSON(http.StatusCreated, record)
 }
 
 type UpdateTableRecordByIdUriParams struct {
@@ -62,45 +60,43 @@ type UpdateTableRecordByIdUriParams struct {
 	TableType string `uri:"tableType" binding:"required"`
 }
 
-func (s *TableRecordApi) UpdateTableRecordById(c *gin.Context) {
+func (s *TableRecordApi) UpdateTableRecordById(ctx *gin.Context) {
 	uriParams := &UpdateTableRecordByIdUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	var req interface{}
 	if uriParams.TableType == "main" {
 		req = table_record.MainRecordWithRelationCount{}
-		if err := c.BindJSON(req); err != nil {
-			api.AbortWithBadRequestJSON(c, err)
+		if err := ctx.BindJSON(req); err != nil {
+			api.AbortWithBadRequestJSON(ctx, err)
 			return
 		}
 	} else if uriParams.TableType == "document" {
 		req = table_record.DocumentRecord{}
-		if err := c.BindJSON(req); err != nil {
-			api.AbortWithBadRequestJSON(c, err)
+		if err := ctx.BindJSON(req); err != nil {
+			api.AbortWithBadRequestJSON(ctx, err)
 			return
 		}
 	}
 
 	// query service
-	err := s.tableRecordService.UpdateTableRecordById(
-		&table_record.UpdateTableRecordByIdParams{
-			Context:   c,
-			ProjectId: uriParams.ProjectId,
-			BranchId:  uriParams.BranchId,
-			RecordId:  uriParams.RecordId,
-			TableType: uriParams.TableType,
-			Record:    req,
-		},
+	err := s.tableRecordService.UpdateTableRecordById(ctx, &table_record.UpdateTableRecordByIdParams{
+		ProjectId: uriParams.ProjectId,
+		BranchId:  uriParams.BranchId,
+		RecordId:  uriParams.RecordId,
+		TableType: uriParams.TableType,
+		Record:    req,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, nil)
+	ctx.JSON(http.StatusOK, nil)
 }
 
 type DeleteTableRecordByIdUriParams struct {
@@ -110,27 +106,25 @@ type DeleteTableRecordByIdUriParams struct {
 	TableType string `uri:"tableType" binding:"required"`
 }
 
-func (s *TableRecordApi) DeleteTableRecordById(c *gin.Context) {
+func (s *TableRecordApi) DeleteTableRecordById(ctx *gin.Context) {
 	uriParams := &DeleteTableRecordByIdUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	err := s.tableRecordService.DeleteTableRecordById(
-		&table_record.DeleteTableRecordByIdParams{
-			Context:   c,
-			ProjectId: uriParams.ProjectId,
-			BranchId:  uriParams.BranchId,
-			RecordId:  uriParams.RecordId,
-			TableType: uriParams.TableType,
-		},
+	err := s.tableRecordService.DeleteTableRecordById(ctx, &table_record.DeleteTableRecordByIdParams{
+		ProjectId: uriParams.ProjectId,
+		BranchId:  uriParams.BranchId,
+		RecordId:  uriParams.RecordId,
+		TableType: uriParams.TableType,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusNoContent, nil)
+	ctx.JSON(http.StatusNoContent, nil)
 }
