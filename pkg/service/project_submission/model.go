eb477ff92f19f95f27d6d9dabb17c3e06195001a
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:14 +0100

    Improve context usage: pkg/service/project_submission/model.go

--- pkg/service/project_submission/model.go
+++ pkg/service/project_submission/model.go
@@ -1,36 +1,29 @@
 package project_submission
 
 import (
-	"context"
-
 	"github.com/quarkloop/quarkloop/pkg/model"
 )
 
 type GetAppSubmissionListParams struct {
-	Context   context.Context
 	ProjectId int
 }
 
 type GetAppSubmissionByIdParams struct {
-	Context         context.Context
 	ProjectId       int
 	AppSubmissionId string
 }
 
 type CreateAppSubmissionParams struct {
-	Context       context.Context
 	UserId        string
 	ProjectId     int
 	AppSubmission *model.AppSubmission
 }
 
 type UpdateAppSubmissionByIdParams struct {
-	Context         context.Context
 	AppSubmissionId string
 	AppSubmission   *model.AppSubmission
 }
 
 type DeleteAppSubmissionByIdParams struct {
-	Context         context.Context
 	AppSubmissionId string
 }
