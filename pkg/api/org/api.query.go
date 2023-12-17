94292817e8a17a960a3940c522a56b346ed5f207
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-17 11:02:07 +0100

    Improve context usage: pkg/api/org/api.query.go

--- pkg/api/org/api.query.go
+++ pkg/api/org/api.query.go
@@ -9,69 +9,61 @@ import (
 	org "github.com/quarkloop/quarkloop/pkg/service/organization"
 )
 
-func (s *OrganizationApi) GetOrganizationList(c *gin.Context) {
+func (s *OrganizationApi) GetOrganizationList(ctx *gin.Context) {
 	// query service
-	orgList, err := s.orgService.GetOrganizationList(
-		&org.GetOrganizationListParams{
-			Context: c,
-		},
-	)
+	orgList, err := s.orgService.GetOrganizationList(ctx, &org.GetOrganizationListParams{})
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, &orgList)
+	ctx.JSON(http.StatusOK, &orgList)
 }
 
 type GetOrganizationByIdUriParams struct {
 	OrgId int `uri:"orgId" binding:"required"`
 }
 
-func (s *OrganizationApi) GetOrganizationById(c *gin.Context) {
+func (s *OrganizationApi) GetOrganizationById(ctx *gin.Context) {
 	uriParams := &GetOrganizationByIdUriParams{}
-	if err := c.ShouldBindUri(uriParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	org, err := s.orgService.GetOrganizationById(
-		&org.GetOrganizationByIdParams{
-			Context: c,
-			OrgId:   uriParams.OrgId,
-		},
+	org, err := s.orgService.GetOrganizationById(ctx, &org.GetOrganizationByIdParams{
+		OrgId: uriParams.OrgId,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, org)
+	ctx.JSON(http.StatusOK, org)
 }
 
 type GetOrganizationQueryParams struct {
 	org.Organization
 }
 
-func (s *OrganizationApi) GetOrganization(c *gin.Context) {
+func (s *OrganizationApi) GetOrganization(ctx *gin.Context) {
 	queryParams := &GetOrganizationQueryParams{}
-	if err := c.ShouldBindQuery(queryParams); err != nil {
-		api.AbortWithBadRequestJSON(c, err)
+	if err := ctx.ShouldBindQuery(queryParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
 		return
 	}
 
 	// query service
-	org, err := s.orgService.GetOrganization(
-		&org.GetOrganizationParams{
-			Context:      c,
-			Organization: queryParams.Organization,
-		},
+	org, err := s.orgService.GetOrganization(ctx, &org.GetOrganizationParams{
+		Organization: queryParams.Organization,
+	},
 	)
 	if err != nil {
-		api.AbortWithInternalServerErrorJSON(c, err)
+		api.AbortWithInternalServerErrorJSON(ctx, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, org)
+	ctx.JSON(http.StatusOK, org)
 }
