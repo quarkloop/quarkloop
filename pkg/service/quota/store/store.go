5afbca8f9d928faf15f541a0bdb5e556d757d626
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-19 23:52:39 +0100

    Add quota service: pkg/service/quota/store/store.go

---
+++ pkg/service/quota/store/store.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/quarkloop/quarkloop/pkg/service/quota"
+)
+
+type QuotaStore interface {
+	// allocation quotas
+	GetQuotasByUserId(context.Context, int) (quota.Quota, error)
+	GetQuotasByOrgId(context.Context, int) ([]quota.Quota, error)
+	GetQuotasByWorkspaceId(context.Context, int) ([]quota.Quota, error)
+
+	// quota overrides
+	UpdateQuotaLimits(context.Context, int, QuoataLimit) error
+}
+
+type quotaStore struct {
+	Conn *pgx.Conn
+}
+
+func NewQuotaStore(conn *pgx.Conn) *quotaStore {
+	return &quotaStore{
+		Conn: conn,
+	}
+}
+
+type QuoataLimit struct {
+	OrgLimit                 int
+	OrgUserLimit             int
+	WorkspacePerOrgLimit     int
+	ProjectPerWorkspaceLimit int
+}
