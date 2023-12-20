14761bc4ded472d50b1355670d5f9d1255687e4d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-20 23:30:04 +0100

    [Client] add: apps/quarkloop-client/app/(platform)/org/[orgId]/[workspaceId]/[projectId]/branches/[branchId]/page.tsx

---
+++ apps/quarkloop-client/app/(platform)/org/[orgId]/[workspaceId]/[projectId]/branches/[branchId]/page.tsx
@@ -0,0 +1,21 @@
+import { Database } from "@quarkloop/components";
+
+interface PageProps {
+    params: {
+        orgId: string;
+        workspaceId: string;
+        projectId: string;
+    };
+    searchParams: any;
+}
+
+const Page = async (props: PageProps) => {
+    const {
+        params: { orgId, workspaceId, projectId },
+        searchParams,
+    } = props;
+
+    return <Database />;
+};
+
+export default Page;
