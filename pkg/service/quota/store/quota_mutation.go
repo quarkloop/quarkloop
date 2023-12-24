2a0d007e61edda17a54669c5db706f9efa05debd
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-24 23:47:23 +0100

    Update: pkg/service/quota/store/quota_mutation.go

--- pkg/service/quota/store/quota_mutation.go
+++ pkg/service/quota/store/quota_mutation.go
@@ -13,17 +13,27 @@ import (
 /// UpdateQuotaLimit
 
 const updateQuotaLimitMutation = `
-UPDATE
-  "system"."Quota" AS q
-SET
-  "orgLimit"                 = CASE WHEN  @orgLimit                 > 0 THEN  @orgLimit                 ELSE q."orgLimit" END,
-  "orgUserLimit"             = CASE WHEN  @orgUserLimit             > 0 THEN  @orgUserLimit             ELSE q."orgUserLimit" END,
-  "workspacePerOrgLimit"     = CASE WHEN  @workspacePerOrgLimit     > 0 THEN  @workspacePerOrgLimit     ELSE q."workspacePerOrgLimit" END,
-  "projectPerWorkspaceLimit" = CASE WHEN  @projectPerWorkspaceLimit > 0 THEN  @projectPerWorkspaceLimit ELSE q."projectPerWorkspaceLimit" END,
-  "updatedAt"                = @updatedAt,
-  "updatedBy"                = @updatedBy
-WHERE
-  "userId" = @userId;
+UPDATE "system"."Quota" AS q
+SET 
+    "orgLimit" = CASE
+        WHEN @orgLimit > 0 THEN @orgLimit
+        ELSE q."orgLimit"
+    END,
+    "orgUserLimit" = CASE
+        WHEN @orgUserLimit > 0 THEN @orgUserLimit
+        ELSE q."orgUserLimit"
+    END,
+    "workspacePerOrgLimit" = CASE
+        WHEN @workspacePerOrgLimit > 0 THEN @workspacePerOrgLimit
+        ELSE q."workspacePerOrgLimit"
+    END,
+    "projectPerWorkspaceLimit" = CASE
+        WHEN @projectPerWorkspaceLimit > 0 THEN @projectPerWorkspaceLimit
+        ELSE q."projectPerWorkspaceLimit"
+    END,
+    "updatedAt" = @updatedAt,
+    "updatedBy" = @updatedBy
+WHERE "userId" = @userId;
 `
 
 func (store *quotaStore) UpdateQuotaLimits(ctx context.Context, userId int, limit QuoataLimit) error {
