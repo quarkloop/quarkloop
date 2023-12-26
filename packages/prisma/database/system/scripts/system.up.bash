76ee97356d8e68522be3dea93f2136b449bb704b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-26 23:41:46 +0100

    Update: packages/prisma/database/system/scripts/system.up.bash

--- packages/prisma/database/system/scripts/system.up.bash
+++ packages/prisma/database/system/scripts/system.up.bash
@@ -6,3 +6,11 @@ npx prisma migrate dev \
     --skip-seed \
     --skip-generate \
     --schema QuarkloopSystem.prisma
+
+echo '
+ALTER TABLE 
+    "system"."UserAssignment" 
+ADD CONSTRAINT 
+    "UserId_UserGroupId_Check_NotNull" CHECK ("userId" IS NOT NULL OR "userGroupId" IS NOT NULL);
+' | \
+npx prisma db execute --stdin --schema QuarkloopSystem.prisma
