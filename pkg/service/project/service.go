210338de36e9dcdc146344dac90ce9c197dce393
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-28 02:53:47 +0100

    Update: pkg/service/project/service.go

--- pkg/service/project/service.go
+++ pkg/service/project/service.go
@@ -5,7 +5,8 @@ import "context"
 type Service interface {
 	GetProjectList(context.Context, *GetProjectListParams) ([]Project, error)
 	GetProjectById(context.Context, *GetProjectByIdParams) (*Project, error)
-	GetProject(context.Context, *GetProjectParams) (*Project, error)
+	// TODO: rewrite
+	//GetProject(context.Context, *GetProjectParams) (*Project, error)
 	CreateProject(context.Context, *CreateProjectParams) (*Project, error)
 	UpdateProjectById(context.Context, *UpdateProjectByIdParams) error
 	DeleteProjectById(context.Context, *DeleteProjectByIdParams) error
