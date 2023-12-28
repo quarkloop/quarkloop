44a9671929079e13ad2e87609c11efb668d29f7c
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-28 02:53:47 +0100

    Update: pkg/service/organization/service.go

--- pkg/service/organization/service.go
+++ pkg/service/organization/service.go
@@ -7,7 +7,8 @@ import (
 type Service interface {
 	GetOrganizationList(context.Context, *GetOrganizationListParams) ([]Organization, error)
 	GetOrganizationById(context.Context, *GetOrganizationByIdParams) (*Organization, error)
-	GetOrganization(context.Context, *GetOrganizationParams) (*Organization, error)
+	// TODO: rewrite
+	// GetOrganization(context.Context, *GetOrganizationParams) (*Organization, error)
 	CreateOrganization(context.Context, *CreateOrganizationParams) (*Organization, error)
 	UpdateOrganizationById(context.Context, *UpdateOrganizationByIdParams) error
 	DeleteOrganizationById(context.Context, *DeleteOrganizationByIdParams) error
