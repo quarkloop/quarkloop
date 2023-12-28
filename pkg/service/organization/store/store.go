f0f35238c3379eb296a723fc077926fec92e13a1
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-28 23:39:29 +0100

    Update: pkg/service/organization/store/store.go

--- pkg/service/organization/store/store.go
+++ pkg/service/organization/store/store.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/quarkloop/quarkloop/pkg/model"
 	org "github.com/quarkloop/quarkloop/pkg/service/organization"
 )
 
 type OrgStore interface {
-	ListOrganizations(ctx context.Context) ([]org.Organization, error)
+	ListOrganizations(ctx context.Context, visibility model.ScopeVisibility) ([]org.Organization, error)
 	GetOrganizationById(ctx context.Context, orgId int) (*org.Organization, error)
 	GetOrganization(ctx context.Context, org *org.Organization) (*org.Organization, error)
 	CreateOrganization(ctx context.Context, org *org.Organization) (*org.Organization, error)
