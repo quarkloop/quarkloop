c43c0e502f49d821560626b60223fa26941623c5
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-28 23:39:29 +0100

    Update: pkg/contextdata/user.go

--- pkg/contextdata/user.go
+++ pkg/contextdata/user.go
@@ -1,29 +1,34 @@
 package contextdata
 
 import (
-	"context"
-
+	"github.com/gin-gonic/gin"
 	"github.com/quarkloop/quarkloop/pkg/service/user"
 )
 
-func GetUser(ctx context.Context) *user.User {
-	user, ok := ctx.Value(userKey).(*user.User)
-	if !ok || user == nil {
+func GetUser(ctx *gin.Context) *user.User {
+	val, exists := ctx.Get(userKey)
+	if !exists || val == nil {
 		panic("user must be available")
 	}
 
-	return user
+	u, ok := val.(*user.User)
+	if !ok {
+		panic("*user.User type assertion failed")
+	}
+
+	return u
 }
 
-func SetUser(ctx context.Context, u *user.User) context.Context {
-	return context.WithValue(ctx, userKey, u)
+func SetUser(ctx *gin.Context, u *user.User) {
+	ctx.Set(userKey, u)
 }
 
-func IsUserAnonymous(ctx context.Context) bool {
-	user, ok := ctx.Value(userKey).(*user.User)
-	if !ok || user == nil {
+func IsUserAnonymous(ctx *gin.Context) bool {
+	val, exists := ctx.Get(userKey)
+	if !exists || val == nil {
 		return true
 	}
 
-	return false
+	_, ok := val.(*user.User)
+	return !ok
 }
