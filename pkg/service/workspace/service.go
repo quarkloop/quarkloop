affe9a88a4240e11f0370353b53a35d947bae6a0
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-28 02:53:47 +0100

    Update: pkg/service/workspace/service.go

--- pkg/service/workspace/service.go
+++ pkg/service/workspace/service.go
@@ -5,7 +5,8 @@ import "context"
 type Service interface {
 	GetWorkspaceList(context.Context, *GetWorkspaceListParams) ([]Workspace, error)
 	GetWorkspaceById(context.Context, *GetWorkspaceByIdParams) (*Workspace, error)
-	GetWorkspace(context.Context, *GetWorkspaceParams) (*Workspace, error)
+	// TODO: rewrite
+	// GetWorkspace(context.Context, *GetWorkspaceParams) (*Workspace, error)
 	CreateWorkspace(context.Context, *CreateWorkspaceParams) (*Workspace, error)
 	UpdateWorkspaceById(context.Context, *UpdateWorkspaceByIdParams) error
 	DeleteWorkspaceById(context.Context, *DeleteWorkspaceByIdParams) error
