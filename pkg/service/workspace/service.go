ba142ac0c00fda6de5ef21fba3fd784e98a4a871
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-28 23:39:29 +0100

    Update: pkg/service/workspace/service.go

--- pkg/service/workspace/service.go
+++ pkg/service/workspace/service.go
@@ -1,13 +1,13 @@
 package workspace
 
-import "context"
+import "github.com/gin-gonic/gin"
 
 type Service interface {
-	GetWorkspaceList(context.Context, *GetWorkspaceListParams) ([]Workspace, error)
-	GetWorkspaceById(context.Context, *GetWorkspaceByIdParams) (*Workspace, error)
+	GetWorkspaceList(*gin.Context, *GetWorkspaceListParams) ([]Workspace, error)
+	GetWorkspaceById(*gin.Context, *GetWorkspaceByIdParams) (*Workspace, error)
 	// TODO: rewrite
 	// GetWorkspace(context.Context, *GetWorkspaceParams) (*Workspace, error)
-	CreateWorkspace(context.Context, *CreateWorkspaceParams) (*Workspace, error)
-	UpdateWorkspaceById(context.Context, *UpdateWorkspaceByIdParams) error
-	DeleteWorkspaceById(context.Context, *DeleteWorkspaceByIdParams) error
+	CreateWorkspace(*gin.Context, *CreateWorkspaceParams) (*Workspace, error)
+	UpdateWorkspaceById(*gin.Context, *UpdateWorkspaceByIdParams) error
+	DeleteWorkspaceById(*gin.Context, *DeleteWorkspaceByIdParams) error
 }
