27c90a2d7d5666de61ea613d32684902461ead76
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 01:25:02 +0100

    Update: pkg/service/organization/service.go

--- pkg/service/organization/service.go
+++ pkg/service/organization/service.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Service interface {
-	GetOrganizationList(*gin.Context) ([]Organization, error)
-	GetOrganizationById(*gin.Context, *GetOrganizationByIdParams) (*Organization, error)
+	GetOrganizationList(*gin.Context) ([]*Organization, error)
+	GetOrganizationById(*gin.Context, *GetOrganizationByIdQuery) (*Organization, error)
 	// TODO: rewrite
-	// GetOrganization(context.Context, *GetOrganizationParams) (*Organization, error)
-	CreateOrganization(*gin.Context, *CreateOrganizationParams) (*Organization, error)
-	UpdateOrganizationById(*gin.Context, *UpdateOrganizationByIdParams) error
-	DeleteOrganizationById(*gin.Context, *DeleteOrganizationByIdParams) error
+	// GetOrganization(context.Context, *GetOrganizationQuery) (*Organization, error)
+	CreateOrganization(*gin.Context, *CreateOrganizationCommand) (*Organization, error)
+	UpdateOrganizationById(*gin.Context, *UpdateOrganizationByIdCommand) error
+	DeleteOrganizationById(*gin.Context, *DeleteOrganizationByIdCommand) error
 }
