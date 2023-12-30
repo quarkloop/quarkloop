bb418120dd0f30fafea93e8507d72b6a59b9aaee
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:29 +0100

    Update: pkg/service/workspace/service.go

--- pkg/service/workspace/service.go
+++ pkg/service/workspace/service.go
@@ -1,8 +1,12 @@
 package workspace
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/service/project"
+)
 
 type Service interface {
+	// workspace
 	GetWorkspaceList(*gin.Context, *GetWorkspaceListQuery) ([]*Workspace, error)
 	GetWorkspaceById(*gin.Context, *GetWorkspaceByIdQuery) (*Workspace, error)
 	// TODO: rewrite
@@ -10,4 +14,7 @@ type Service interface {
 	CreateWorkspace(*gin.Context, *CreateWorkspaceCommand) (*Workspace, error)
 	UpdateWorkspaceById(*gin.Context, *UpdateWorkspaceByIdCommand) error
 	DeleteWorkspaceById(*gin.Context, *DeleteWorkspaceByIdCommand) error
+
+	// project
+	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*project.Project, error)
 }
