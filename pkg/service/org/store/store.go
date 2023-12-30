03dd5c318f457bbd1bfda09a70adfb54e2d1abea
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 23:46:34 +0100

    Update: pkg/service/org/store/store.go

--- pkg/service/org/store/store.go
+++ pkg/service/org/store/store.go
@@ -7,16 +7,20 @@ import (
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/org"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
+	"github.com/quarkloop/quarkloop/pkg/service/workspace"
 )
 
 type OrgStore interface {
 	// org
-	GetOrgList(ctx context.Context, visibility model.ScopeVisibility) ([]*org.Org, error)
+	GetOrgList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*org.Org, error)
 	GetOrgById(ctx context.Context, orgId int) (*org.Org, error)
 	GetOrg(ctx context.Context, org *org.Org) (*org.Org, error)
 	CreateOrg(ctx context.Context, org *org.Org) (*org.Org, error)
 	UpdateOrgById(ctx context.Context, orgId int, org *org.Org) error
 	DeleteOrgById(ctx context.Context, orgId int) error
+
+	// workspace
+	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*workspace.Workspace, error)
 
 	// project
 	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*project.Project, error)
