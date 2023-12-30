cc239b49a7ba7fa8686740e9e65924bf5f7d7cfc
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:28 +0100

    Update: pkg/service/accesscontrol/impl/user_group.go

--- pkg/service/accesscontrol/impl/user_group.go
+++ pkg/service/accesscontrol/impl/user_group.go
@@ -6,27 +6,27 @@ import (
 	"github.com/quarkloop/quarkloop/pkg/service/accesscontrol"
 )
 
-func (s *aclService) ListUserGroups(ctx context.Context, orgId int) ([]accesscontrol.UserGroup, error) {
-	ugList, err := s.store.ListUserGroups(ctx, orgId)
+func (s *aclService) GetUserGroupList(ctx context.Context, query *accesscontrol.GetUserGroupListQuery) ([]accesscontrol.UserGroup, error) {
+	ugList, err := s.store.GetUserGroupList(ctx, query.OrgId)
 	return ugList, err
 }
 
-func (s *aclService) GetUserGroupById(ctx context.Context, userGroupId int) (*accesscontrol.UserGroup, error) {
-	ug, err := s.store.GetUserGroupById(ctx, userGroupId)
+func (s *aclService) GetUserGroupById(ctx context.Context, query *accesscontrol.GetUserGroupByIdQuery) (*accesscontrol.UserGroup, error) {
+	ug, err := s.store.GetUserGroupById(ctx, query.UserGroupId)
 	return ug, err
 }
 
-func (s *aclService) CreateUserGroup(ctx context.Context, orgId int, userGroup *accesscontrol.UserGroup) (*accesscontrol.UserGroup, error) {
-	ug, err := s.store.CreateUserGroup(ctx, orgId, userGroup)
+func (s *aclService) CreateUserGroup(ctx context.Context, cmd *accesscontrol.CreateUserGroupCommand) (*accesscontrol.UserGroup, error) {
+	ug, err := s.store.CreateUserGroup(ctx, cmd.OrgId, cmd.UserGroup)
 	return ug, err
 }
 
-func (s *aclService) UpdateUserGroupById(ctx context.Context, userGroupId int, userGroup *accesscontrol.UserGroup) error {
-	err := s.store.UpdateUserGroupById(ctx, userGroupId, userGroup)
+func (s *aclService) UpdateUserGroupById(ctx context.Context, cmd *accesscontrol.UpdateUserGroupByIdCommand) error {
+	err := s.store.UpdateUserGroupById(ctx, cmd.UserGroupId, cmd.UserGroup)
 	return err
 }
 
-func (s *aclService) DeleteUserGroupById(ctx context.Context, orgId int, userGroupId int) error {
-	err := s.store.DeleteUserGroupById(ctx, orgId, userGroupId)
+func (s *aclService) DeleteUserGroupById(ctx context.Context, cmd *accesscontrol.DeleteUserGroupByIdCommand) error {
+	err := s.store.DeleteUserGroupById(ctx, cmd.OrgId, cmd.UserGroupId)
 	return err
 }
