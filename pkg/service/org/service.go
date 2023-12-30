98c5cb163e05c7992e42725bbbafa58ce95b45b6
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:28 +0100

    Update: pkg/service/org/service.go

--- pkg/service/org/service.go
+++ pkg/service/org/service.go
@@ -2,9 +2,11 @@ package org
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/service/project"
 )
 
 type Service interface {
+	// org
 	GetOrganizationList(*gin.Context) ([]*Organization, error)
 	GetOrganizationById(*gin.Context, *GetOrganizationByIdQuery) (*Organization, error)
 	// TODO: rewrite
@@ -12,4 +14,7 @@ type Service interface {
 	CreateOrganization(*gin.Context, *CreateOrganizationCommand) (*Organization, error)
 	UpdateOrganizationById(*gin.Context, *UpdateOrganizationByIdCommand) error
 	DeleteOrganizationById(*gin.Context, *DeleteOrganizationByIdCommand) error
+
+	// project
+	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*project.Project, error)
 }
