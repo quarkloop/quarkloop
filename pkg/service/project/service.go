bc0a25a41ce5ca047e3555892aeb673543723326
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 01:25:02 +0100

    Update: pkg/service/project/service.go

--- pkg/service/project/service.go
+++ pkg/service/project/service.go
@@ -3,11 +3,11 @@ package project
 import "github.com/gin-gonic/gin"
 
 type Service interface {
-	GetProjectList(*gin.Context, *GetProjectListParams) ([]Project, error)
-	GetProjectById(*gin.Context, *GetProjectByIdParams) (*Project, error)
+	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*Project, error)
+	GetProjectById(*gin.Context, *GetProjectByIdQuery) (*Project, error)
 	// TODO: rewrite
-	//GetProject(context.Context, *GetProjectParams) (*Project, error)
-	CreateProject(*gin.Context, *CreateProjectParams) (*Project, error)
-	UpdateProjectById(*gin.Context, *UpdateProjectByIdParams) error
-	DeleteProjectById(*gin.Context, *DeleteProjectByIdParams) error
+	//GetProject(context.Context, *GetProjectQuery) (*Project, error)
+	CreateProject(*gin.Context, *CreateProjectCommand) (*Project, error)
+	UpdateProjectById(*gin.Context, *UpdateProjectByIdCommand) error
+	DeleteProjectById(*gin.Context, *DeleteProjectByIdCommand) error
 }
