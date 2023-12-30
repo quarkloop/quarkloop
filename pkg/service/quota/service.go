950c5e591232a39291783c8d9b44b6d38fd89a7b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:28 +0100

    Update: pkg/service/quota/service.go

--- pkg/service/quota/service.go
+++ pkg/service/quota/service.go
@@ -4,13 +4,13 @@ import "context"
 
 type Service interface {
 	// allocation quotas
-	GetQuotasByUserId(context.Context, int) (Quota, error)
-	GetQuotasByOrgId(context.Context, int) ([]Quota, error)
+	GetQuotasByUserId(context.Context, *GetQuotasByUserIdQuery) (Quota, error)
+	GetQuotasByOrgId(context.Context, *GetQuotasByOrgIdQuery) ([]Quota, error)
 
-	CheckCreateOrgQuotaReached(ctx context.Context, userId int) error
-	CheckCreateOrgUserQuotaReached(ctx context.Context, orgId int) error
-	CheckCreateWorkspaceQuotaReached(ctx context.Context, orgId int) error
-	CheckCreateProjectQuotaReached(ctx context.Context, orgId int) error
+	CheckCreateOrgQuotaReached(context.Context, *CheckCreateOrgQuotaReachedQuery) error
+	CheckCreateOrgUserQuotaReached(context.Context, *CheckCreateOrgUserQuotaReachedQuery) error
+	CheckCreateWorkspaceQuotaReached(context.Context, *CheckCreateWorkspaceQuotaReachedQuery) error
+	CheckCreateProjectQuotaReached(context.Context, *CheckCreateProjectQuotaReachedQuery) error
 
 	// overrides
 	// OverrideOrgUserQuotaLimit(context.Context, int) error
