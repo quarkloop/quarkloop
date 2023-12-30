c6bb4b8c6b667cb5310a51b415a2b5b22d2fa443
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:28 +0100

    Update: pkg/service/project/service.go

--- pkg/service/project/service.go
+++ pkg/service/project/service.go
@@ -3,7 +3,6 @@ package project
 import "github.com/gin-gonic/gin"
 
 type Service interface {
-	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*Project, error)
 	GetProjectById(*gin.Context, *GetProjectByIdQuery) (*Project, error)
 	// TODO: rewrite
 	//GetProject(context.Context, *GetProjectQuery) (*Project, error)
