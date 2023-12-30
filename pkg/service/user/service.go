ad387994dcea0cc7fc60b6085120f73d7229b6c1
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:28 +0100

    Update: pkg/service/user/service.go

--- pkg/service/user/service.go
+++ pkg/service/user/service.go
@@ -4,14 +4,14 @@ import "context"
 
 type Service interface {
 	// user
-	GetUserById(context.Context, *GetUserByIdParams) (*User, error)
-	GetUserByEmail(context.Context, *GetUserByEmailParams) (*User, error)
-	UpdateUserById(context.Context, *UpdateUserByIdParams) error
-	DeleteUserById(context.Context, *DeleteUserByIdParams) error
+	GetUserById(context.Context, *GetUserByIdQuery) (*User, error)
+	GetUserByEmail(context.Context, *GetUserByEmailQuery) (*User, error)
+	UpdateUserById(context.Context, *UpdateUserByIdCommand) error
+	DeleteUserById(context.Context, *DeleteUserByIdCommand) error
 
 	// account
-	GetUserAccountByUserId(context.Context, *GetUserAccountByUserIdParams) (*UserAccount, error)
+	GetUserAccountByUserId(context.Context, *GetUserAccountByUserIdQuery) (*UserAccount, error)
 
 	// session
-	GetUserSessionByUserId(context.Context, *GetUserSessionByUserIdParams) (*UserSession, error)
+	GetUserSessionByUserId(context.Context, *GetUserSessionByUserIdQuery) (*UserSession, error)
 }
