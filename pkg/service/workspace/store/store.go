7a69643220749e92b658160a2951694234de06cf
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 16:09:28 +0100

    Update: pkg/service/workspace/store/store.go

--- pkg/service/workspace/store/store.go
+++ pkg/service/workspace/store/store.go
@@ -5,16 +5,21 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/quarkloop/quarkloop/pkg/model"
+	"github.com/quarkloop/quarkloop/pkg/service/project"
 	"github.com/quarkloop/quarkloop/pkg/service/workspace"
 )
 
 type WorkspaceStore interface {
-	ListWorkspaces(ctx context.Context, visibility model.ScopeVisibility, orgId []int) ([]*workspace.Workspace, error)
+	// workspace
+	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, orgId []int) ([]*workspace.Workspace, error)
 	GetWorkspaceById(ctx context.Context, workspaceId int) (*workspace.Workspace, error)
 	GetWorkspace(ctx context.Context, orgId int, workspace *workspace.Workspace) (*workspace.Workspace, error)
 	CreateWorkspace(ctx context.Context, orgId int, workspace *workspace.Workspace) (*workspace.Workspace, error)
 	UpdateWorkspaceById(ctx context.Context, workspaceId int, workspace *workspace.Workspace) error
 	DeleteWorkspaceById(ctx context.Context, workspaceId int) error
+
+	// project
+	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int, workspaceId int) ([]*project.Project, error)
 }
 
 type workspaceStore struct {
