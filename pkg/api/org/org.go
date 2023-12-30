032ddcd83b30c61fa84f652fa0a68de29019f7f3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-30 15:31:28 +0100

    Update: pkg/api/org/org.go

--- pkg/api/org/org.go
+++ pkg/api/org/org.go
@@ -2,17 +2,21 @@ package org
 
 import (
 	"github.com/gin-gonic/gin"
-	org "github.com/quarkloop/quarkloop/pkg/service/organization"
+	"github.com/quarkloop/quarkloop/pkg/service/org"
 )
 
 type Api interface {
-	GetOrganizationList(c *gin.Context)
-	GetOrganizationById(c *gin.Context)
+	// org
+	GetOrganizationList(*gin.Context)
+	GetOrganizationById(*gin.Context)
 	// TODO: rewrite
-	// GetOrganization(c *gin.Context)
-	CreateOrganization(c *gin.Context)
-	UpdateOrganizationById(c *gin.Context)
-	DeleteOrganizationById(c *gin.Context)
+	// GetOrganization(*gin.Context)
+	CreateOrganization(*gin.Context)
+	UpdateOrganizationById(*gin.Context)
+	DeleteOrganizationById(*gin.Context)
+
+	// project
+	GetProjectList(*gin.Context)
 }
 
 type OrganizationApi struct {
