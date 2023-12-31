3009603eecd7c2aea48fd4ede6a67934dc88348c
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2023-12-31 02:40:34 +0100

    Update: pkg/api/project/project_query.go

--- pkg/api/project/project_query.go
+++ pkg/api/project/project_query.go
@@ -60,3 +60,32 @@ func (s *ProjectApi) GetProjectById(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, project)
 }
+
+// GET /orgs/:orgId/workspaces/:workspaceId/projects/:projectId/users
+//
+// Get project user list.
+//
+// Response status:
+// 200: StatusOK
+// 500: StatusInternalServerError
+
+func (s *ProjectApi) GetUserList(ctx *gin.Context) {
+	uriParams := &project.GetUserListUriParams{}
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
+		return
+	}
+
+	// query service
+	userList, err := s.projectService.GetUserList(ctx, &project.GetUserListQuery{
+		OrgId:       uriParams.OrgId,
+		WorkspaceId: uriParams.WorkspaceId,
+		ProjectId:   uriParams.ProjectId,
+	})
+	if err != nil {
+		api.AbortWithInternalServerErrorJSON(ctx, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &userList)
+}
