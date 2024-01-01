b4f641952ed447a43efe8976b3fa33dacaebcdce
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-01 18:27:37 +0100

    Update: pkg/api/org/org.go

--- pkg/api/org/org.go
+++ pkg/api/org/org.go
@@ -2,32 +2,32 @@ package org
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/service/accesscontrol"
 	"github.com/quarkloop/quarkloop/pkg/service/org"
 )
 
 type Api interface {
-	// org
-	GetOrgList(*gin.Context)
+	// query
 	GetOrgById(*gin.Context)
-	// TODO: rewrite
-	// GetOrg(*gin.Context)
+	GetOrgList(*gin.Context)
+	GetWorkspaceList(*gin.Context)
+	GetProjectList(*gin.Context)
+	GetUserList(*gin.Context)
+
+	// mutation
 	CreateOrg(*gin.Context)
 	UpdateOrgById(*gin.Context)
 	DeleteOrgById(*gin.Context)
-
-	// project
-	GetProjectList(*gin.Context)
-
-	// user
-	GetUserList(*gin.Context)
 }
 
 type OrgApi struct {
 	orgService org.Service
+	aclService accesscontrol.Service
 }
 
-func NewOrgApi(service org.Service) *OrgApi {
+func NewOrgApi(service org.Service, aclService accesscontrol.Service) *OrgApi {
 	return &OrgApi{
 		orgService: service,
+		aclService: aclService,
 	}
 }
