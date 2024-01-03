6dfc0f07d58863e260be65cf7c8de20170b07d10
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:26:56 +0100

    Update: pkg/service/workspace/store/store.go

--- pkg/service/workspace/store/store.go
+++ pkg/service/workspace/store/store.go
@@ -6,20 +6,21 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
 	"github.com/quarkloop/quarkloop/pkg/service/workspace"
 )
 
 type WorkspaceStore interface {
-	// workspace
-	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*workspace.Workspace, error)
+	// query
 	GetWorkspaceById(ctx context.Context, workspaceId int) (*workspace.Workspace, error)
-	GetWorkspace(ctx context.Context, orgId int, workspace *workspace.Workspace) (*workspace.Workspace, error)
+	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*workspace.Workspace, error)
+	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int, workspaceId int) ([]*project.Project, error)
+	GetUserAssignmentList(ctx context.Context, orgId, workspaceId int) ([]*user.UserAssignment, error)
+
+	// mutation
 	CreateWorkspace(ctx context.Context, orgId int, workspace *workspace.Workspace) (*workspace.Workspace, error)
 	UpdateWorkspaceById(ctx context.Context, workspaceId int, workspace *workspace.Workspace) error
 	DeleteWorkspaceById(ctx context.Context, workspaceId int) error
-
-	// project
-	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int, workspaceId int) ([]*project.Project, error)
 }
 
 type workspaceStore struct {
