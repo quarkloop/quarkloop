11cf133413031580c714179495e0b39835d241f2
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:26:56 +0100

    Update: pkg/service/user/service.go

--- pkg/service/user/service.go
+++ pkg/service/user/service.go
@@ -3,15 +3,13 @@ package user
 import "context"
 
 type Service interface {
-	// user
+	// query
 	GetUserById(context.Context, *GetUserByIdQuery) (*User, error)
 	GetUserByEmail(context.Context, *GetUserByEmailQuery) (*User, error)
+	GetUserAccountByUserId(context.Context, *GetUserAccountByUserIdQuery) (*UserAccount, error)
+	GetUserSessionByUserId(context.Context, *GetUserSessionByUserIdQuery) (*UserSession, error)
+
+	// mutation
 	UpdateUserById(context.Context, *UpdateUserByIdCommand) error
 	DeleteUserById(context.Context, *DeleteUserByIdCommand) error
-
-	// account
-	GetUserAccountByUserId(context.Context, *GetUserAccountByUserIdQuery) (*UserAccount, error)
-
-	// session
-	GetUserSessionByUserId(context.Context, *GetUserSessionByUserIdQuery) (*UserSession, error)
 }
