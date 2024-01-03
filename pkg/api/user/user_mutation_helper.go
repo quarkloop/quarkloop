c490775bcafcc5f7663238c7213d75cdf73d4b4c
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:28:31 +0100

    [user] add api: pkg/api/user/user_mutation_helper.go

---
+++ pkg/api/user/user_mutation_helper.go
@@ -0,0 +1,27 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/api"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
+)
+
+func (s *UserApi) updateUserById(ctx *gin.Context, cmd *user.UpdateUserByIdCommand) api.Response {
+	err := s.userService.UpdateUserById(ctx, cmd)
+	if err != nil {
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	return api.Success(http.StatusOK, nil)
+}
+
+func (s *UserApi) deleteUserById(ctx *gin.Context, cmd *user.DeleteUserByIdCommand) api.Response {
+	err := s.userService.DeleteUserById(ctx, cmd)
+	if err != nil {
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	return api.Success(http.StatusNoContent, nil)
+}
