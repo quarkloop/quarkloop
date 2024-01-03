c60788e9aefc2f913d59e24d62310764dfeb2043
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:26:56 +0100

    Update: pkg/service/project/store/store.go

--- pkg/service/project/store/store.go
+++ pkg/service/project/store/store.go
@@ -6,13 +6,17 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
 )
 
 type ProjectStore interface {
-	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*project.Project, error)
+	// query
 	GetProjectById(ctx context.Context, projectId int) (*project.Project, error)
-	GetProject(ctx context.Context, p *project.Project) (*project.Project, error)
-	CreateProject(ctx context.Context, orgId int, workspaceId int, p *project.Project) (*project.Project, error)
+	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*project.Project, error)
+	GetUserAssignmentList(ctx context.Context, orgId, workspaceId, projectId int) ([]*user.UserAssignment, error)
+
+	// mutation
+	CreateProject(ctx context.Context, orgId, workspaceId int, p *project.Project) (*project.Project, error)
 	UpdateProjectById(ctx context.Context, projectId int, project *project.Project) error
 	DeleteProjectById(ctx context.Context, projectId int) error
 }
