6f5af3cef4d1e6e0d39ffc40b737668d14fe873d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:26:56 +0100

    Update: pkg/api/workspace/workspace.go

--- pkg/api/workspace/workspace.go
+++ pkg/api/workspace/workspace.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/quarkloop/quarkloop/pkg/service/accesscontrol"
 	"github.com/quarkloop/quarkloop/pkg/service/quota"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
 	"github.com/quarkloop/quarkloop/pkg/service/workspace"
 )
 
@@ -12,7 +13,7 @@ type Api interface {
 	GetWorkspaceById(*gin.Context)
 	GetWorkspaceList(*gin.Context)
 	GetProjectList(*gin.Context)
-	GetUserList(*gin.Context)
+	GetMemberList(*gin.Context)
 
 	// mutation
 	CreateWorkspace(*gin.Context)
@@ -22,13 +23,21 @@ type Api interface {
 
 type WorkspaceApi struct {
 	workspaceService workspace.Service
-	aclService       accesscontrol.Service
-	quotaService     quota.Service
+
+	userService  user.Service
+	aclService   accesscontrol.Service
+	quotaService quota.Service
 }
 
-func NewWorkspaceApi(service workspace.Service, aclService accesscontrol.Service, quotaService quota.Service) *WorkspaceApi {
+func NewWorkspaceApi(
+	service workspace.Service,
+	userService user.Service,
+	aclService accesscontrol.Service,
+	quotaService quota.Service,
+) *WorkspaceApi {
 	return &WorkspaceApi{
 		workspaceService: service,
+		userService:      userService,
 		aclService:       aclService,
 		quotaService:     quotaService,
 	}
