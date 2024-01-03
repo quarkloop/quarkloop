12c569a9190e46f89b4e1e0f3eba0ad9613d6f89
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:28:31 +0100

    [user] add api: pkg/api/user/user_query.go

---
+++ pkg/api/user/user_query.go
@@ -0,0 +1,28 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
+)
+
+// GET /users/:userId
+//
+// Get user by id.
+//
+// Response status:
+// 200: StatusOK
+// 500: StatusInternalServerError
+
+func (s *UserApi) GetUserById(ctx *gin.Context) {
+	uriParams := &user.GetUserByIdUriParams{}
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	query := &user.GetUserByIdQuery{UserId: uriParams.UserId}
+	res := s.getUserById(ctx, query)
+	ctx.JSON(res.Status(), res.Body())
+}
