5ef8f8f4c2642bfc0c89d76f06aca0c83311fe43
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 14:33:34 +0100

    Update: pkg/service/org/store/store.go

--- pkg/service/org/store/store.go
+++ pkg/service/org/store/store.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/org"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
 	"github.com/quarkloop/quarkloop/pkg/service/user"
@@ -13,17 +12,16 @@ import (
 
 type OrgStore interface {
 	// query
-	GetOrgById(ctx context.Context, orgId int) (*org.Org, error)
-	GetOrg(ctx context.Context, org *org.Org) (*org.Org, error)
-	GetOrgList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*org.Org, error)
-	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*workspace.Workspace, error)
-	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*project.Project, error)
-	GetUserAssignmentList(ctx context.Context, orgId int) ([]*user.UserAssignment, error)
+	GetOrgById(context.Context, *org.GetOrgByIdQuery) (*org.Org, error)
+	GetOrgList(context.Context, *org.GetOrgListQuery) ([]*org.Org, error)
+	GetWorkspaceList(context.Context, *org.GetWorkspaceListQuery) ([]*workspace.Workspace, error)
+	GetProjectList(context.Context, *org.GetProjectListQuery) ([]*project.Project, error)
+	GetUserAssignmentList(context.Context, *org.GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
 
 	// mutation
-	CreateOrg(ctx context.Context, org *org.Org) (*org.Org, error)
-	UpdateOrgById(ctx context.Context, orgId int, org *org.Org) error
-	DeleteOrgById(ctx context.Context, orgId int) error
+	CreateOrg(context.Context, *org.CreateOrgCommand) (*org.Org, error)
+	UpdateOrgById(context.Context, *org.UpdateOrgByIdCommand) error
+	DeleteOrgById(context.Context, *org.DeleteOrgByIdCommand) error
 }
 
 type orgStore struct {
