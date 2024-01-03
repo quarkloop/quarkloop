265cae12cef9478f822330f4f90bf96a88e9a108
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:26:56 +0100

    Update: pkg/service/org/store/store.go

--- pkg/service/org/store/store.go
+++ pkg/service/org/store/store.go
@@ -7,23 +7,23 @@ import (
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/org"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
 	"github.com/quarkloop/quarkloop/pkg/service/workspace"
 )
 
 type OrgStore interface {
-	// org
-	GetOrgList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*org.Org, error)
+	// query
 	GetOrgById(ctx context.Context, orgId int) (*org.Org, error)
 	GetOrg(ctx context.Context, org *org.Org) (*org.Org, error)
+	GetOrgList(ctx context.Context, visibility model.ScopeVisibility, userId int) ([]*org.Org, error)
+	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*workspace.Workspace, error)
+	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*project.Project, error)
+	GetUserAssignmentList(ctx context.Context, orgId int) ([]*user.UserAssignment, error)
+
+	// mutation
 	CreateOrg(ctx context.Context, org *org.Org) (*org.Org, error)
 	UpdateOrgById(ctx context.Context, orgId int, org *org.Org) error
 	DeleteOrgById(ctx context.Context, orgId int) error
-
-	// workspace
-	GetWorkspaceList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*workspace.Workspace, error)
-
-	// project
-	GetProjectList(ctx context.Context, visibility model.ScopeVisibility, orgId int) ([]*project.Project, error)
 }
 
 type orgStore struct {
