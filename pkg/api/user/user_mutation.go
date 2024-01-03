320e3fb108cdf8c4e7195fb5f4a571fc52ba5381
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-03 01:28:31 +0100

    [user] add api: pkg/api/user/user_mutation.go

---
+++ pkg/api/user/user_mutation.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quarkloop/quarkloop/pkg/api"
+	"github.com/quarkloop/quarkloop/pkg/service/user"
+)
+
+// PUT /users/:userId
+//
+// Update user by id.
+//
+// Response status:
+// 200: StatusOK
+// 500: StatusInternalServerError
+
+func (s *UserApi) UpdateUserById(ctx *gin.Context) {
+	uriParams := &user.UpdateUserByIdUriParams{}
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	cmd := &user.UpdateUserByIdCommand{UserId: uriParams.UserId}
+	if err := ctx.ShouldBindJSON(cmd); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res := s.updateUserById(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+// DELETE /users/:userId
+//
+// Delete user by id.
+//
+// Response status:
+// 204: StatusNoContent
+// 500: StatusInternalServerError
+
+func (s *UserApi) DeleteUserById(ctx *gin.Context) {
+	uriParams := &user.DeleteUserByIdUriParams{}
+	if err := ctx.ShouldBindUri(uriParams); err != nil {
+		api.AbortWithBadRequestJSON(ctx, err)
+		return
+	}
+
+	cmd := &user.DeleteUserByIdCommand{UserId: uriParams.UserId}
+	res := s.deleteUserById(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
