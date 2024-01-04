42848c7ba67aa4a68528cf27090f78a3534ec063
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-04 17:36:50 +0100

    Update: pkg/service/user/store/user_query.go

--- pkg/service/user/store/user_query.go
+++ pkg/service/user/store/user_query.go
@@ -189,3 +189,257 @@ func (store *orgStore) GetUserSessionByUserId(ctx context.Context, userId int) (
 
 	return &session, nil
 }
+
+/// GetUser
+
+const getUserQuery = `
+SELECT 
+    "id",
+	"username",
+    "name",
+    "email",
+    "emailVerified",
+    "password",
+    "birthdate",
+    "country",
+    "image",
+    "status",
+    "createdAt",
+    "createdBy",
+    "updatedAt",
+    "updatedBy"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetUser(ctx context.Context, query *user.GetUserQuery) (*user.User, error) {
+
+}
+
+/// GetUsername
+
+const getUsernameQuery = `
+SELECT 
+	"username"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetUsername(ctx context.Context, query *user.GetUsernameQuery) (string, error) {
+
+}
+
+/// GetEmail
+
+const getEmailQuery = `
+SELECT 
+	"email"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetEmail(ctx context.Context, query *user.GetEmailQuery) (string, error) {
+
+}
+
+/// GetStatus
+
+const getStatusQuery = `
+SELECT 
+	"status"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetStatus(ctx context.Context, query *user.GetStatusQuery) (any, error) {
+
+}
+
+/// GetPreferences
+
+const getPreferencesQuery = ``
+
+func (store *orgStore) GetPreferences(ctx context.Context, query *user.GetPreferencesQuery) (any, error) {
+
+}
+
+/// GetSessions
+
+const getSessionsQuery = `
+SELECT 
+    "id",
+    "sessionToken",
+    "expires"
+FROM 
+    "auth"."Session"
+WHERE 
+    "userId" = @userId;
+`
+
+func (store *orgStore) GetSessions(ctx context.Context, query *user.GetSessionsQuery) ([]*user.UserSession, error) {
+
+}
+
+/// GetAccounts
+
+const getAccountsQuery = `
+SELECT 
+    "id",
+    "type",
+    "provider",
+    "providerAccountId",
+    "refresh_token",
+    "access_token",
+    "expires_at",
+    "token_type",
+    "scope",
+    "id_token",
+    "session_state"
+FROM 
+    "auth"."Account"
+WHERE 
+    "userId" = @userId;
+`
+
+func (store *orgStore) GetAccounts(ctx context.Context, query *user.GetAccountsQuery) ([]*user.UserAccount, error) {
+
+}
+
+/// GetUserById
+
+const getUserByIdQuery = `
+SELECT 
+    "id",
+	"username",
+    "name",
+    "email",
+    "emailVerified",
+    "password",
+    "birthdate",
+    "country",
+    "image",
+    "status",
+    "createdAt",
+    "createdBy",
+    "updatedAt",
+    "updatedBy"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetUserById(ctx context.Context, query *user.GetUserByIdQuery) (*user.User, error) {
+
+}
+
+/// GetUsernameByUserId
+
+const getUsernameByUserIdQuery = `
+SELECT 
+	"username"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetUsernameByUserId(ctx context.Context, query *user.GetUsernameByUserIdQuery) (string, error) {
+
+}
+
+/// GetEmailByUserId
+
+const getEmailByUserIdQuery = `
+SELECT 
+	"email"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetEmailByUserId(ctx context.Context, query *user.GetEmailByUserIdQuery) (string, error) {
+
+}
+
+/// GetStatusByUserId
+
+const getStatusByUserIdQuery = `
+SELECT 
+	"status"
+FROM 
+    "auth"."User"
+WHERE 
+    "id" = @id;
+`
+
+func (store *orgStore) GetStatusByUserId(ctx context.Context, query *user.GetStatusByUserIdQuery) (any, error) {
+
+}
+
+/// GetPreferencesByUserId
+
+const getPreferencesByUserIdQuery = ``
+
+func (store *orgStore) GetPreferencesByUserId(ctx context.Context, query *user.GetPreferencesByUserIdQuery) (any, error) {
+
+}
+
+/// GetSessionsByUserId
+
+const getSessionsByUserIdQuery = `
+SELECT 
+    "id",
+    "sessionToken",
+    "expires"
+FROM 
+    "auth"."Session"
+WHERE 
+    "userId" = @userId;
+`
+
+func (store *orgStore) GetSessionsByUserId(ctx context.Context, query *user.GetSessionsByUserIdQuery) ([]*user.UserSession, error) {
+
+}
+
+/// GetAccountsByUserId
+
+const getAccountsByUserIdQuery = `
+SELECT 
+    "id",
+    "type",
+    "provider",
+    "providerAccountId",
+    "refresh_token",
+    "access_token",
+    "expires_at",
+    "token_type",
+    "scope",
+    "id_token",
+    "session_state"
+FROM 
+    "auth"."Account"
+WHERE 
+    "userId" = @userId;
+`
+
+func (store *orgStore) GetAccountsByUserId(ctx context.Context, query *user.GetAccountsByUserIdQuery) ([]*user.UserAccount, error) {
+
+}
+
+/// GetUsers
+
+const getUsersQuery = ``
+
+func (store *orgStore) GetUsers(ctx context.Context, query *user.GetUsersQuery) ([]*user.User, error) {
+
+}
