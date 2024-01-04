3fd2a532f3f581e186cb43e8c20b36379b654503
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-04 18:12:26 +0100

    Update: pkg/service/user/impl/user_mutation.go

--- pkg/service/user/impl/user_mutation.go
+++ pkg/service/user/impl/user_mutation.go
@@ -17,7 +17,57 @@ func NewUserService(ds store.OrgStore) user.Service {
 	}
 }
 
+func (s *userService) UpdateUser(ctx context.Context, cmd *user.UpdateUserCommand) error {
+	err := s.store.UpdateUser(ctx, cmd)
+	return err
+}
+
+func (s *userService) UpdateUsername(ctx context.Context, cmd *user.UpdateUsernameCommand) error {
+	err := s.store.UpdateUsername(ctx, cmd)
+	return err
+}
+
+func (s *userService) UpdatePassword(ctx context.Context, cmd *user.UpdatePasswordCommand) error {
+	err := s.store.UpdatePassword(ctx, cmd)
+	return err
+}
+
+func (s *userService) UpdatePreferences(ctx context.Context, cmd *user.UpdatePreferencesCommand) error {
+	err := s.store.UpdatePreferences(ctx, cmd)
+	return err
+}
+
 func (s *userService) UpdateUserById(ctx context.Context, cmd *user.UpdateUserByIdCommand) error {
-	err := s.store.UpdateUserById(ctx, cmd.UserId, &cmd.User)
+	err := s.store.UpdateUserById(ctx, cmd)
+	return err
+}
+
+func (s *userService) UpdateUsernameByUserId(ctx context.Context, cmd *user.UpdateUsernameByUserIdCommand) error {
+	err := s.store.UpdateUsernameByUserId(ctx, cmd)
+	return err
+}
+
+func (s *userService) UpdatePasswordByUserId(ctx context.Context, cmd *user.UpdatePasswordByUserIdCommand) error {
+	err := s.store.UpdatePasswordByUserId(ctx, cmd)
+	return err
+}
+
+func (s *userService) UpdatePreferencesByUserId(ctx context.Context, cmd *user.UpdatePreferencesByUserIdCommand) error {
+	err := s.store.UpdatePreferencesByUserId(ctx, cmd)
+	return err
+}
+
+func (s *userService) DeleteUserById(ctx context.Context, cmd *user.DeleteUserByIdCommand) error {
+	err := s.store.DeleteUserById(ctx, cmd)
+	return err
+}
+
+func (s *userService) DeleteSessionById(ctx context.Context, cmd *user.DeleteSessionByIdCommand) error {
+	err := s.store.DeleteSessionById(ctx, cmd)
+	return err
+}
+
+func (s *userService) DeleteAccountById(ctx context.Context, cmd *user.DeleteAccountByIdCommand) error {
+	err := s.store.DeleteAccountById(ctx, cmd)
 	return err
 }
