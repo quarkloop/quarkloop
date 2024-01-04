3cb47db925c9ba27f9053f7d2ee392e761054ee2
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-04 14:37:30 +0100

    Update: pkg/api/user/user_mutation.go

--- pkg/api/user/user_mutation.go
+++ pkg/api/user/user_mutation.go
@@ -16,7 +16,7 @@ import (
 // 200: StatusOK
 // 500: StatusInternalServerError
 
-func (s *UserApi) UpdateUserById(ctx *gin.Context) {
+func (s *userApi) UpdateUserById(ctx *gin.Context) {
 	uriParams := &user.UpdateUserByIdUriParams{}
 	if err := ctx.ShouldBindUri(uriParams); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, err)
@@ -41,7 +41,7 @@ func (s *UserApi) UpdateUserById(ctx *gin.Context) {
 // 204: StatusNoContent
 // 500: StatusInternalServerError
 
-func (s *UserApi) DeleteUserById(ctx *gin.Context) {
+func (s *userApi) DeleteUserById(ctx *gin.Context) {
 	uriParams := &user.DeleteUserByIdUriParams{}
 	if err := ctx.ShouldBindUri(uriParams); err != nil {
 		api.AbortWithBadRequestJSON(ctx, err)
@@ -50,5 +50,71 @@ func (s *UserApi) DeleteUserById(ctx *gin.Context) {
 
 	cmd := &user.DeleteUserByIdCommand{UserId: uriParams.UserId}
 	res := s.deleteUserById(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdateUser(ctx *gin.Context) {
+	cmd := &user.UpdateUserCommand{}
+	res := s.updateUser(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdateUsername(ctx *gin.Context) {
+	cmd := &user.UpdateUsernameCommand{}
+	res := s.updateUsername(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdatePassword(ctx *gin.Context) {
+	cmd := &user.UpdatePasswordCommand{}
+	res := s.updatePassword(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdatePreferences(ctx *gin.Context) {
+	cmd := &user.UpdatePreferencesCommand{}
+	res := s.updatePreferences(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdateUserById(ctx *gin.Context) {
+	cmd := &user.UpdateUserByIdCommand{}
+	res := s.updateUserById(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdateUsernameByUserId(ctx *gin.Context) {
+	cmd := &user.UpdateUsernameByUserIdCommand{}
+	res := s.updateUsernameByUserId(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdatePasswordByUserId(ctx *gin.Context) {
+	cmd := &user.UpdatePasswordByUserIdCommand{}
+	res := s.updatePasswordByUserId(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) UpdatePreferencesByUserId(ctx *gin.Context) {
+	cmd := &user.UpdatePreferencesByUserIdCommand{}
+	res := s.updatePreferencesByUserId(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) DeleteUserById(ctx *gin.Context) {
+	cmd := &user.DeleteUserByIdCommand{}
+	res := s.deleteUserById(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) DeleteSessionById(ctx *gin.Context) {
+	cmd := &user.DeleteSessionByIdCommand{}
+	res := s.deleteSessionById(ctx, cmd)
+	ctx.JSON(res.Status(), res.Body())
+}
+
+func (s *userApi) DeleteAccountById(ctx *gin.Context) {
+	cmd := &user.DeleteAccountByIdCommand{}
+	res := s.deleteAccountById(ctx, cmd)
 	ctx.JSON(res.Status(), res.Body())
 }
