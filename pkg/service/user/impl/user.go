d8862db419bf99c8c6b5dfae7316aee699d6c548
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-04 17:36:50 +0100

    Update: pkg/service/user/impl/user.go

--- pkg/service/user/impl/user.go
+++ pkg/service/user/impl/user.go
@@ -1,8 +1,6 @@
 package user_impl
 
 import (
-	"context"
-
 	"github.com/quarkloop/quarkloop/pkg/service/user"
 	"github.com/quarkloop/quarkloop/pkg/service/user/store"
 )
@@ -15,34 +13,4 @@ func NewUserService(ds store.OrgStore) user.Service {
 	return &userService{
 		store: ds,
 	}
-}
-
-func (s *userService) GetUserById(ctx context.Context, query *user.GetUserByIdQuery) (*user.User, error) {
-	u, err := s.store.GetUserById(ctx, query.UserId)
-	return u, err
-}
-
-func (s *userService) GetUserByEmail(ctx context.Context, query *user.GetUserByEmailQuery) (*user.User, error) {
-	u, err := s.store.GetUserByEmail(ctx, query.Email)
-	return u, err
-}
-
-func (s *userService) GetUserAccountByUserId(ctx context.Context, query *user.GetUserAccountByUserIdQuery) (*user.UserAccount, error) {
-	u, err := s.store.GetUserAccountByUserId(ctx, query.UserId)
-	return u, err
-}
-
-func (s *userService) GetUserSessionByUserId(ctx context.Context, query *user.GetUserSessionByUserIdQuery) (*user.UserSession, error) {
-	u, err := s.store.GetUserSessionByUserId(ctx, query.UserId)
-	return u, err
-}
-
-func (s *userService) UpdateUserById(ctx context.Context, cmd *user.UpdateUserByIdCommand) error {
-	err := s.store.UpdateUserById(ctx, cmd.UserId, &cmd.User)
-	return err
-}
-
-func (s *userService) DeleteUserById(ctx context.Context, cmd *user.DeleteUserByIdCommand) error {
-	err := s.store.DeleteUserById(ctx, cmd.UserId)
-	return err
 }
