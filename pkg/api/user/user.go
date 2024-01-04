9f530f612dc488e806a87f49ac11775ad363675d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-04 14:37:30 +0100

    Update: pkg/api/user/user.go

--- pkg/api/user/user.go
+++ pkg/api/user/user.go
@@ -7,19 +7,44 @@ import (
 )
 
 type Api interface {
+	// query
+	GetUser(*gin.Context)
+	GetUsername(*gin.Context)
+	GetEmail(*gin.Context)
+	GetStatus(*gin.Context)
+	GetPreferences(*gin.Context)
+	GetSessions(*gin.Context)
+	GetAccounts(*gin.Context)
 	GetUserById(*gin.Context)
-	CreateUser(*gin.Context)
+	GetUsernameByUserId(*gin.Context)
+	GetEmailByUserId(*gin.Context)
+	GetStatusByUserId(*gin.Context)
+	GetPreferencesByUserId(*gin.Context)
+	GetSessionsByUserId(*gin.Context)
+	GetAccountsByUserId(*gin.Context)
+	GetUsers(*gin.Context)
+
+	// mutation
+	UpdateUser(*gin.Context)
+	UpdateUsername(*gin.Context)
+	UpdatePassword(*gin.Context)
+	UpdatePreferences(*gin.Context)
 	UpdateUserById(*gin.Context)
+	UpdateUsernameByUserId(*gin.Context)
+	UpdatePasswordByUserId(*gin.Context)
+	UpdatePreferencesByUserId(*gin.Context)
 	DeleteUserById(*gin.Context)
+	DeleteSessionById(*gin.Context)
+	DeleteAccountById(*gin.Context)
 }
 
-type UserApi struct {
+type userApi struct {
 	userService user.Service
 	aclService  accesscontrol.Service
 }
 
-func NewUserApi(service user.Service, aclService accesscontrol.Service) *UserApi {
-	return &UserApi{
+func NewUserApi(service user.Service, aclService accesscontrol.Service) *userApi {
+	return &userApi{
 		userService: service,
 		aclService:  aclService,
 	}
