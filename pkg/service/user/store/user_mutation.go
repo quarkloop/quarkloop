1b44f22baeacec1207d9a418c95b3a1401fbca44
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-05 01:47:09 +0100

    Update: pkg/service/user/store/user_mutation.go

--- pkg/service/user/store/user_mutation.go
+++ pkg/service/user/store/user_mutation.go
@@ -11,16 +11,50 @@ import (
 	"github.com/quarkloop/quarkloop/pkg/service/user"
 )
 
-/// UpdateUserById
+/// UpdateUserQuery
 
-const updateUserByIdMutation = `
+const updateUserQuery = `
+`
+
+func (store *userStore) UpdateUser(ctx context.Context, cmd *user.UpdateUserCommand) error {
+	panic("not implemented")
+}
+
+/// UpdateUsernameQuery
+
+const updateUsernameQuery = `
+`
+
+func (store *userStore) UpdateUsername(ctx context.Context, cmd *user.UpdateUsernameCommand) error {
+	panic("not implemented")
+}
+
+/// UpdatePasswordQuery
+
+const updatePasswordQuery = `
+`
+
+func (store *userStore) UpdatePassword(ctx context.Context, cmd *user.UpdatePasswordCommand) error {
+	panic("not implemented")
+}
+
+/// UpdatePreferencesQuery
+
+const updatePreferencesQuery = `
+`
+
+func (store *userStore) UpdatePreferences(ctx context.Context, cmd *user.UpdatePreferencesCommand) error {
+	panic("not implemented")
+}
+
+/// UpdateUserByIdQuery
+
+const updateUserByIdQuery = `
 UPDATE
     "auth"."User"
 SET
-    "sid"           = @scopeId,
     "email"         = @email,
     "emailVerified" = @emailVerified,
-    "password"      = @password,
     "name"          = @name,
     "birthdate"     = @birthdate,
     "country"       = @country,
@@ -32,22 +66,19 @@ WHERE
     "id" = @id;
 `
 
-func (store *orgStore) UpdateUserById(ctx context.Context, userId int, user *user.User) error {
-	commandTag, err := store.Conn.Exec(ctx, updateUserByIdMutation, pgx.NamedArgs{
-		"id":            userId,
-		"scopeId":       user.ScopeId,
-		"email":         user.Email,
-		"emailVerified": user.EmailVerified,
-		"password":      user.Password,
-		"name":          user.Name,
-		"birthdate":     user.Birthdate,
-		"country":       user.Country,
-		"image":         user.Image,
-		"status":        user.Status,
+func (store *userStore) UpdateUserById(ctx context.Context, cmd *user.UpdateUserByIdCommand) error {
+	commandTag, err := store.Conn.Exec(ctx, updateUserByIdQuery, pgx.NamedArgs{
+		"id":            cmd.UserId,
+		"email":         cmd.Email,
+		"emailVerified": cmd.EmailVerified,
+		"name":          cmd.Name,
+		"birthdate":     cmd.Birthdate,
+		"country":       cmd.Country,
+		"image":         cmd.Image,
+		"status":        cmd.Status,
+		"updatedBy":     cmd.UpdatedBy,
 		"updatedAt":     time.Now(),
-		"updatedBy":     user.UpdatedBy,
-	},
-	)
+	})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "[UPDATE] failed: %v\n", err)
 		return err
@@ -62,17 +93,94 @@ func (store *orgStore) UpdateUserById(ctx context.Context, userId int, user *use
 	return nil
 }
 
-/// DeleteUserById
+/// UpdateUsernameByUserIdQuery
 
-const deleteUserByIdMutation = `
+const updateUsernameByUserIdQuery = `
+UPDATE
+    "auth"."User"
+SET
+    "username"  = @username,
+    "updatedAt" = @updatedAt,
+    "updatedBy" = @updatedBy,
+WHERE
+    "id" = @id;
+`
+
+func (store *userStore) UpdateUsernameByUserId(ctx context.Context, cmd *user.UpdateUsernameByUserIdCommand) error {
+	commandTag, err := store.Conn.Exec(ctx, updateUsernameByUserIdQuery, pgx.NamedArgs{
+		"id":        cmd.UserId,
+		"username":  cmd.Username,
+		"updatedBy": cmd.UpdatedBy,
+		"updatedAt": time.Now(),
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[UPDATE] failed: %v\n", err)
+		return err
+	}
+
+	if commandTag.RowsAffected() != 1 {
+		notFoundErr := errors.New("cannot find to update")
+		fmt.Fprintf(os.Stderr, "[UPDATE] failed: %v\n", notFoundErr)
+		return notFoundErr
+	}
+
+	return nil
+}
+
+/// UpdatePasswordByUserIdQuery
+
+const updatePasswordByUserIdQuery = `
+UPDATE
+    "auth"."User"
+SET
+    "password"  = @password,
+    "updatedAt" = @updatedAt,
+    "updatedBy" = @updatedBy,
+WHERE
+    "id" = @id;
+`
+
+func (store *userStore) UpdatePasswordByUserId(ctx context.Context, cmd *user.UpdatePasswordByUserIdCommand) error {
+	commandTag, err := store.Conn.Exec(ctx, updatePasswordByUserIdQuery, pgx.NamedArgs{
+		"id":        cmd.UserId,
+		"password":  cmd.Password,
+		"updatedBy": cmd.UpdatedBy,
+		"updatedAt": time.Now(),
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[UPDATE] failed: %v\n", err)
+		return err
+	}
+
+	if commandTag.RowsAffected() != 1 {
+		notFoundErr := errors.New("cannot find to update")
+		fmt.Fprintf(os.Stderr, "[UPDATE] failed: %v\n", notFoundErr)
+		return notFoundErr
+	}
+
+	return nil
+}
+
+/// UpdatePreferencesByUserIdQuery
+
+const updatePreferencesByUserIdQuery = `
+`
+
+func (store *userStore) UpdatePreferencesByUserId(ctx context.Context, cmd *user.UpdatePreferencesByUserIdCommand) error {
+	panic("not implemented")
+}
+
+/// DeleteUserByIdQuery
+
+const deleteUserByIdQuery = `
 DELETE FROM
     "auth"."User"
 WHERE
     "id" = @id;
 `
 
-func (store *orgStore) DeleteUserById(ctx context.Context, userId int) error {
-	commandTag, err := store.Conn.Exec(ctx, deleteUserByIdMutation, pgx.NamedArgs{"id": userId})
+func (store *userStore) DeleteUserById(ctx context.Context, cmd *user.DeleteUserByIdCommand) error {
+	commandTag, err := store.Conn.Exec(ctx, deleteUserByIdQuery, pgx.NamedArgs{"id": cmd.UserId})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "[DELETE] failed: %v\n", err)
 		return err
@@ -87,101 +195,62 @@ func (store *orgStore) DeleteUserById(ctx context.Context, userId int) error {
 	return nil
 }
 
-/// UpdateUserQuery
-
-const updateUserQuery = `
-`
-
-func (store *orgStore) UpdateUser(ctx context.Context, cmd *user.UpdateUserCommand) error {
-
-}
-
-/// UpdateUsernameQuery
-
-const updateUsernameQuery = `
-`
-
-func (store *orgStore) UpdateUsername(ctx context.Context, cmd *user.UpdateUsernameCommand) error {
-
-}
-
-/// UpdatePasswordQuery
-
-const updatePasswordQuery = `
-`
-
-func (store *orgStore) UpdatePassword(ctx context.Context, cmd *user.UpdatePasswordCommand) error {
-
-}
-
-/// UpdatePreferencesQuery
-
-const updatePreferencesQuery = `
-`
-
-func (store *orgStore) UpdatePreferences(ctx context.Context, cmd *user.UpdatePreferencesCommand) error {
-
-}
-
-/// UpdateUserByIdQuery
-
-const updateUserByIdQuery = `
-`
-
-func (store *orgStore) UpdateUserById(ctx context.Context, cmd *user.UpdateUserByIdCommand) error {
-
-}
-
-/// UpdateUsernameByUserIdQuery
-
-const updateUsernameByUserIdQuery = `
-`
-
-func (store *orgStore) UpdateUsernameByUserId(ctx context.Context, cmd *user.UpdateUsernameByUserIdCommand) error {
-
-}
-
-/// UpdatePasswordByUserIdQuery
-
-const updatePasswordByUserIdQuery = `
-`
-
-func (store *orgStore) UpdatePasswordByUserId(ctx context.Context, cmd *user.UpdatePasswordByUserIdCommand) error {
-
-}
-
-/// UpdatePreferencesByUserIdQuery
-
-const updatePreferencesByUserIdQuery = `
-`
-
-func (store *orgStore) UpdatePreferencesByUserId(ctx context.Context, cmd *user.UpdatePreferencesByUserIdCommand) error {
-
-}
-
-/// DeleteUserByIdQuery
-
-const deleteUserByIdQuery = `
-`
-
-func (store *orgStore) DeleteUserById(ctx context.Context, cmd *user.DeleteUserByIdCommand) error {
-
-}
-
 /// DeleteSessionByIdQuery
 
 const deleteSessionByIdQuery = `
+DELETE FROM
+    "auth"."Session"
+WHERE
+    "id" = @id
+AND
+    "userId" = @userId;
 `
 
-func (store *orgStore) DeleteSessionById(ctx context.Context, cmd *user.DeleteSessionByIdCommand) error {
+func (store *userStore) DeleteSessionById(ctx context.Context, cmd *user.DeleteSessionByIdCommand) error {
+	commandTag, err := store.Conn.Exec(ctx, deleteSessionByIdQuery, pgx.NamedArgs{
+		"id":     cmd.SessionId,
+		"userId": cmd.UserId,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[DELETE] failed: %v\n", err)
+		return err
+	}
 
+	if commandTag.RowsAffected() != 1 {
+		notFoundErr := errors.New("cannot find to delete")
+		fmt.Fprintf(os.Stderr, "[DELETE] failed: %v\n", notFoundErr)
+		return notFoundErr
+	}
+
+	return nil
 }
 
 /// deleteAccountByIdQuery
 
 const DeleteAccountByIdQuery = `
+DELETE FROM
+    "auth"."Account"
+WHERE
+    "id" = @id
+AND
+    "userId" = @userId;
 `
 
-func (store *orgStore) DeleteAccountById(ctx context.Context, cmd *user.DeleteAccountByIdCommand) error {
+func (store *userStore) DeleteAccountById(ctx context.Context, cmd *user.DeleteAccountByIdCommand) error {
+	commandTag, err := store.Conn.Exec(ctx, deleteSessionByIdQuery, pgx.NamedArgs{
+		"id":     cmd.AccountId,
+		"userId": cmd.UserId,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[DELETE] failed: %v\n", err)
+		return err
+	}
 
+	if commandTag.RowsAffected() != 1 {
+		notFoundErr := errors.New("cannot find to delete")
+		fmt.Fprintf(os.Stderr, "[DELETE] failed: %v\n", notFoundErr)
+		return notFoundErr
+	}
+
+	return nil
 }
