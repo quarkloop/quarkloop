676a0727989c78eac90c4cf20110513b37fdecab
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-07 15:33:16 +0100

    [test] Add: pkg/service/project/errors.go

---
+++ pkg/service/project/errors.go
@@ -0,0 +1,24 @@
+package project
+
+import (
+	"errors"
+
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+func HandleError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if pgErr, ok := err.(*pgconn.PgError); ok {
+		switch pgErr.Code {
+		case "22012":
+			return errors.New("rows.Err()" + err.Error())
+		case "23505":
+			return ErrProjectAlreadyExists
+		}
+	}
+
+	return err
+}
