d6e29c3a4ff8d6338507d00265ff72e365748d62
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-08 16:38:02 +0100

    Update: packages/prisma/database/auth/scripts/auth.up.bash

--- packages/prisma/database/auth/scripts/auth.up.bash
+++ packages/prisma/database/auth/scripts/auth.up.bash
@@ -1,8 +1,47 @@
 #!/bin/bash
 
-source ../../.env 
+set -a # automatically export all variables
 
-npx prisma migrate dev \
-    --skip-seed \
-    --skip-generate \
-    --schema QuarkloopAuth.prisma
+mkdir -p ./migrations/test ./migrations/dev
+
+create_test_db() {
+    source ../../.env.test
+    rm -rf ./migrations/test/*
+    cp QuarkloopAuth.prisma ./migrations/test/QuarkloopAuth.prisma
+
+    # db down
+    npx prisma db execute \
+        --file scripts/auth.down.sql \
+        --schema ./migrations/test/QuarkloopAuth.prisma
+
+    # db up
+    npx prisma migrate dev \
+        --skip-seed \
+        --skip-generate \
+        --schema ./migrations/test/QuarkloopAuth.prisma
+}
+
+create_dev_db() {
+    source ../../.env 
+    cp QuarkloopAuth.prisma ./migrations/dev/QuarkloopAuth.prisma
+
+    npx prisma migrate dev \
+        --skip-seed \
+        --skip-generate \
+        --schema ./migrations/dev/QuarkloopAuth.prisma
+}
+
+case "$1" in
+  "test")
+    create_test_db
+    ;;
+  "dev")
+    create_dev_db
+    ;;
+  *)
+    echo "You have failed to specify what to do correctly."
+    exit 1
+    ;;
+esac
+
+set +a # automatically unexport all variables
