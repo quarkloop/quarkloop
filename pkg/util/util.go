ceec25a803a68bc0323e6a87ee0e1eb1a5e42157
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-08 02:39:50 +0100

    Update: pkg/util/util.go

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -11,6 +11,10 @@ func GetProjectRootPath() string {
 	return filepath.Join(filepath.Dir(b), "../..")
 }
 
+func GetTestEnvFilePath() string {
+	return fmt.Sprintf("%s/.env.test", GetProjectRootPath())
+}
+
 func GetDevEnvFilePath() string {
 	return fmt.Sprintf("%s/.env.development", GetProjectRootPath())
 }
