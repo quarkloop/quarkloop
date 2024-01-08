f153ff002f40d246b72f75e1f1334a7dceb0e605
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-08 23:28:41 +0100

    Update: pkg/service/accesscontrol/impl/user_role.go

--- pkg/service/accesscontrol/impl/user_role.go
+++ pkg/service/accesscontrol/impl/user_role.go
@@ -7,26 +7,26 @@ import (
 )
 
 func (s *aclService) GetUserRoleList(ctx context.Context, query *accesscontrol.GetUserRoleListQuery) ([]accesscontrol.UserRole, error) {
-	urList, err := s.store.ListUserRoles(ctx, query.OrgId)
+	urList, err := s.store.GetUserRoleList(ctx, query)
 	return urList, err
 }
 
 func (s *aclService) GetUserRoleById(ctx context.Context, query *accesscontrol.GetUserRoleByIdQuery) (*accesscontrol.UserRole, error) {
-	ur, err := s.store.GetUserRoleById(ctx, query.UserRoleId)
+	ur, err := s.store.GetUserRoleById(ctx, query)
 	return ur, err
 }
 
 func (s *aclService) CreateUserRole(ctx context.Context, cmd *accesscontrol.CreateUserRoleCommand) (*accesscontrol.UserRole, error) {
-	ur, err := s.store.CreateUserRole(ctx, cmd.OrgId, cmd.UserRole)
+	ur, err := s.store.CreateUserRole(ctx, cmd)
 	return ur, err
 }
 
 func (s *aclService) UpdateUserRoleById(ctx context.Context, cmd *accesscontrol.UpdateUserRoleByIdCommand) error {
-	err := s.store.UpdateUserRoleById(ctx, cmd.UserRoleId, cmd.UserRole)
+	err := s.store.UpdateUserRoleById(ctx, cmd)
 	return err
 }
 
 func (s *aclService) DeleteUserRoleById(ctx context.Context, cmd *accesscontrol.DeleteUserRoleByIdCommand) error {
-	err := s.store.DeleteUserRoleById(ctx, cmd.OrgId, cmd.UserRoleId)
+	err := s.store.DeleteUserRoleById(ctx, cmd)
 	return err
 }
