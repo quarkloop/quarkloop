5d4f02447f47630e5a3d855a5d5665141b2e60ed
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-08 16:38:02 +0100

    Update: packages/prisma/database/system/scripts/system.down.sql

--- packages/prisma/database/system/scripts/system.down.sql
+++ packages/prisma/database/system/scripts/system.down.sql
@@ -2,6 +2,22 @@
 ALTER TABLE "system"."Workspace"
 DROP CONSTRAINT "Workspace_orgId_fkey";
 
+-- DropConstraint
+ALTER TABLE "system"."UserAssignment"
+DROP CONSTRAINT "UserId_UserGroupId_Check_NotNull";
+
+-- Permission
+DROP TABLE "system"."Permission";
+
+-- UserAssignment
+DROP TABLE "system"."UserAssignment";
+
+-- UserRole
+DROP TABLE "system"."UserRole";
+
+-- UserAssignment
+DROP TABLE "system"."UserGroup";
+
 -- DropForeignKey
 ALTER TABLE "system"."Project"
 DROP CONSTRAINT "Project_orgId_fkey";
@@ -11,13 +27,16 @@ ALTER TABLE "system"."Project"
 DROP CONSTRAINT "Project_workspaceId_fkey";
 
 -- DropTable
-DROP TABLE "system"."Organization";
+DROP TABLE "system"."Project";
 
 -- DropTable
 DROP TABLE "system"."Workspace";
 
 -- DropTable
-DROP TABLE "system"."Project";
+DROP TABLE "system"."Organization";
+
+-- _prisma_migrations
+DROP TABLE "system"."_prisma_migrations";
 
 -- DropSchema
 DROP SCHEMA IF EXISTS "system";
