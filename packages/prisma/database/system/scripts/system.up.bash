87519ed76952c8bbc9c0bd9681dc93d8a6b21292
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-08 16:38:02 +0100

    Update: packages/prisma/database/system/scripts/system.up.bash

--- packages/prisma/database/system/scripts/system.up.bash
+++ packages/prisma/database/system/scripts/system.up.bash
@@ -1,16 +1,65 @@
 #!/bin/bash
 
-source ../../.env 
+set -a # automatically export all variables
 
-npx prisma migrate dev \
-    --skip-seed \
-    --skip-generate \
-    --schema QuarkloopSystem.prisma
+mkdir -p ./migrations/test ./migrations/dev
 
-echo '
-ALTER TABLE 
-    "system"."UserAssignment" 
-ADD CONSTRAINT 
-    "UserId_UserGroupId_Check_NotNull" CHECK ("userId" IS NOT NULL OR "userGroupId" IS NOT NULL);
-' | \
-npx prisma db execute --stdin --schema QuarkloopSystem.prisma
+
+create_test_db() {
+    source ../../.env.test
+    rm -rf ./migrations/test/*
+    cp QuarkloopSystem.prisma ./migrations/test/QuarkloopSystem.prisma
+
+    # db down
+    npx prisma db execute \
+        --file scripts/system.down.sql \
+        --schema ./migrations/test/QuarkloopSystem.prisma
+
+    # db up
+    npx prisma migrate dev \
+        --skip-seed \
+        --skip-generate \
+        --schema ./migrations/test/QuarkloopSystem.prisma
+
+    echo '
+    ALTER TABLE 
+        "system"."UserAssignment" 
+    ADD CONSTRAINT 
+        "UserId_UserGroupId_Check_NotNull" CHECK ("userId" IS NOT NULL OR "userGroupId" IS NOT NULL);
+    ' | \
+    npx prisma db execute --stdin --schema ./migrations/test/QuarkloopSystem.prisma       
+}
+
+create_dev_db() {
+    source ../../.env 
+    cp QuarkloopSystem.prisma ./migrations/dev/QuarkloopSystem.prisma
+
+    # db up
+    npx prisma migrate dev \
+        --skip-seed \
+        --skip-generate \
+        --schema ./migrations/dev/QuarkloopSystem.prisma
+
+    echo '
+    ALTER TABLE 
+        "system"."UserAssignment" 
+    ADD CONSTRAINT 
+        "UserId_UserGroupId_Check_NotNull" CHECK ("userId" IS NOT NULL OR "userGroupId" IS NOT NULL);
+    ' | \
+    npx prisma db execute --stdin --schema ./migrations/dev/QuarkloopSystem.prisma
+}
+
+case "$1" in
+  "test")
+    create_test_db
+    ;;
+  "dev")
+    create_dev_db
+    ;;
+  *)
+    echo "You have failed to specify what to do correctly."
+    exit 1
+    ;;
+esac
+
+set +a # automatically unexport all variables
