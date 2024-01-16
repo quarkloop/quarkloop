05322ab035e18db1a3ccc9aafd2ebb11af2e56bf
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-16 23:40:44 +0100

    Update: pkg/service/org/service.go

--- pkg/service/org/service.go
+++ pkg/service/org/service.go
@@ -1,24 +1,27 @@
 package org
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/quarkloop/quarkloop/pkg/model"
-	"github.com/quarkloop/quarkloop/pkg/service/project"
-	"github.com/quarkloop/quarkloop/pkg/service/user"
-	"github.com/quarkloop/quarkloop/pkg/service/workspace"
+	// "github.com/gin-gonic/gin"
+	// "github.com/quarkloop/quarkloop/pkg/model"
+	// "github.com/quarkloop/quarkloop/pkg/service/project"
+	// "github.com/quarkloop/quarkloop/pkg/service/user"
+	// "github.com/quarkloop/quarkloop/pkg/service/workspace"
+	"github.com/quarkloop/quarkloop/service/system"
 )
 
-type Service interface {
-	// query
-	GetOrgById(*gin.Context, *GetOrgByIdQuery) (*Org, error)
-	GetOrgVisibilityById(*gin.Context, *GetOrgVisibilityByIdQuery) (model.ScopeVisibility, error)
-	GetOrgList(*gin.Context, *GetOrgListQuery) ([]*Org, error)
-	GetWorkspaceList(*gin.Context, *GetWorkspaceListQuery) ([]*workspace.Workspace, error)
-	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*project.Project, error)
-	GetUserAssignmentList(*gin.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+type Service system.OrgServiceServer
 
-	// mutation
-	CreateOrg(*gin.Context, *CreateOrgCommand) (*Org, error)
-	UpdateOrgById(*gin.Context, *UpdateOrgByIdCommand) error
-	DeleteOrgById(*gin.Context, *DeleteOrgByIdCommand) error
-}
+// type Service interface {
+// 	// query
+// 	GetOrgById(*gin.Context, *GetOrgByIdQuery) (*Org, error)
+// 	GetOrgVisibilityById(*gin.Context, *GetOrgVisibilityByIdQuery) (model.ScopeVisibility, error)
+// 	GetOrgList(*gin.Context, *GetOrgListQuery) ([]*Org, error)
+// 	GetWorkspaceList(*gin.Context, *GetWorkspaceListQuery) ([]*workspace.Workspace, error)
+// 	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*project.Project, error)
+// 	GetUserAssignmentList(*gin.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+
+// 	// mutation
+// 	CreateOrg(*gin.Context, *CreateOrgCommand) (*Org, error)
+// 	UpdateOrgById(*gin.Context, *UpdateOrgByIdCommand) error
+// 	DeleteOrgById(*gin.Context, *DeleteOrgByIdCommand) error
+// }
