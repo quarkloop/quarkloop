3fae6ded8614aa032c055c896c4d134a1e31e643
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-16 23:40:44 +0100

    Update: pkg/service/org/model.go

--- pkg/service/org/model.go
+++ pkg/service/org/model.go
@@ -2,8 +2,6 @@ package org
 
 import (
 	"errors"
-	"fmt"
-	"time"
 
 	"github.com/quarkloop/quarkloop/pkg/model"
 )
@@ -14,51 +12,18 @@ var (
 	ErrOrgAlreadyExists  = errors.New("org with same scopeId already exists")
 )
 
-type Org struct {
-	// id
-	Id      int    `json:"id"`
-	ScopeId string `json:"sid"`
-
-	// data
-	Name        string                `json:"name"`
-	Description string                `json:"description"`
-	Visibility  model.ScopeVisibility `json:"visibility"`
-	Path        string                `json:"path"`
-
-	// history
-	CreatedAt time.Time  `json:"createdAt"`
-	CreatedBy string     `json:"createdBy"`
-	UpdatedAt *time.Time `json:"updatedAt"`
-	UpdatedBy *string    `json:"updatedBy"`
-}
-
-func (o *Org) GeneratePath() {
-	o.Path = fmt.Sprintf("/org/%s", o.ScopeId)
-}
-
-// GetOrgList
-
 type GetOrgListQuery struct {
-	UserId     int
+	UserId     int32
 	Visibility model.ScopeVisibility
 }
 
-// GetOrgById
-
-type GetOrgByIdUriParams struct {
-	OrgId int `uri:"orgId" binding:"required"`
-}
-
 type GetOrgByIdQuery struct {
-	OrgId int
+	OrgId int32 `uri:"orgId" binding:"required"`
 }
 
-// GetOrgVisibilityById
 type GetOrgVisibilityByIdQuery struct {
-	OrgId int
+	OrgId int32
 }
-
-// CreateOrg
 
 type CreateOrgCommand struct {
 	CreatedBy string
@@ -69,15 +34,9 @@ type CreateOrgCommand struct {
 	Visibility  model.ScopeVisibility `json:"visibility"`
 }
 
-// UpdateOrgById
-
-type UpdateOrgByIdUriParams struct {
-	OrgId int `uri:"orgId" binding:"required"`
-}
-
 type UpdateOrgByIdCommand struct {
 	UpdatedBy string
-	OrgId     int
+	OrgId     int32 `uri:"orgId" binding:"required"`
 
 	ScopeId     string                `json:"sid,omitempty"`
 	Name        string                `json:"name,omitempty"`
@@ -85,50 +44,24 @@ type UpdateOrgByIdCommand struct {
 	Visibility  model.ScopeVisibility `json:"visibility,omitempty"`
 }
 
-// DeleteOrgById
-
-type DeleteOrgByIdUriParams struct {
-	OrgId int `uri:"orgId" binding:"required"`
-}
-
 type DeleteOrgByIdCommand struct {
-	OrgId int
-}
-
-// GetWorkspaceList
-
-type GetWorkspaceListUriParams struct {
-	OrgId int `uri:"orgId" binding:"required"`
+	OrgId int32 `uri:"orgId" binding:"required"`
 }
 
 type GetWorkspaceListQuery struct {
-	OrgId      int
+	OrgId      int32 `uri:"orgId" binding:"required"`
 	Visibility model.ScopeVisibility
-}
-
-// GetProjectList
-
-type GetProjectListUriParams struct {
-	OrgId int `uri:"orgId" binding:"required"`
 }
 
 type GetProjectListQuery struct {
-	OrgId      int
+	OrgId      int32 `uri:"orgId" binding:"required"`
 	Visibility model.ScopeVisibility
 }
 
-// GetMemberList
-
-type GetMemberListUriParams struct {
-	OrgId int `uri:"orgId" binding:"required"`
-}
-
 type GetMemberListQuery struct {
-	OrgId int
+	OrgId int32 `uri:"orgId" binding:"required"`
 }
-
-// GetUserAssignmentList
 
 type GetUserAssignmentListQuery struct {
-	OrgId int
+	OrgId int32
 }
