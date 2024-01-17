0273d7249171d152171daad6b6f82bbd7db140b6
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-17 23:49:02 +0100

    Update: pkg/test/util.go

--- pkg/test/util.go
+++ pkg/test/util.go
@@ -22,6 +22,18 @@ func InitTestAuthDB() (context.Context, *pgx.Conn) {
 	return context.Background(), db.GetConnection()
 }
 
+func InitTestAuthzDB() (context.Context, *pgx.Conn) {
+	err := godotenv.Load(util.GetTestEnvFilePath())
+	if err != nil {
+		log.Fatal("Error loading .env file", err.Error())
+	}
+
+	db := db.NewAuthzDatabase()
+	db.Connect()
+
+	return context.Background(), db.GetConnection()
+}
+
 func InitTestSystemDB() (context.Context, *pgx.Conn) {
 	err := godotenv.Load(util.GetTestEnvFilePath())
 	if err != nil {
