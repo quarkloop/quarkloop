5c693eaa113d86c86f0eaec3e2aed642b8ba331b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-22 23:37:32 +0100

    Update: pkg/service/accesscontrol/service.go

--- pkg/service/accesscontrol/service.go
+++ pkg/service/accesscontrol/service.go
@@ -4,20 +4,19 @@ import "context"
 
 type Service interface {
 	// user access query and mutation
-	EvaluateUserAccess(context.Context, *EvaluateQuery) error
+	EvaluateUserAccess(context.Context, *EvaluateQuery) (bool, error)
 	GrantUserAccess(context.Context, *GrantUserAccessCommand) (bool, error)
 	RevokeUserAccess(context.Context, *RevokeUserAccessCommand) error
 
 	// member query
-	GetOrgMemberList(context.Context, *GetOrgMemberListQuery) ([]*OrgMember, error)
-	GetOrgMemberById(context.Context, *GetOrgMemberByIdQuery) (*OrgMember, error)
-	GetOrgMemberByUserId(context.Context, *GetOrgMemberByUserIdQuery) (*OrgMember, error)
-	GetWorkspaceMemberList(context.Context, *GetWorkspaceMemberListQuery) ([]*WorkspaceMember, error)
-	GetWorkspaceMemberById(context.Context, *GetWorkspaceMemberByIdQuery) (*WorkspaceMember, error)
-	GetWorkspaceMemberByUserId(context.Context, *GetWorkspaceMemberByUserIdQuery) (*WorkspaceMember, error)
-	GetProjectMemberList(context.Context, *GetProjectMemberListQuery) ([]*ProjectMember, error)
-	GetProjectMemberById(context.Context, *GetProjectMemberByIdQuery) (*ProjectMember, error)
-	GetProjectMemberByUserId(context.Context, *GetProjectMemberByUserIdQuery) (*ProjectMember, error)
+	GetOrgMemberList(context.Context, *GetOrgMemberListQuery) ([]*Member, error)
+	GetWorkspaceMemberList(context.Context, *GetWorkspaceMemberListQuery) ([]*Member, error)
+	GetProjectMemberList(context.Context, *GetProjectMemberListQuery) ([]*Member, error)
+
+	// resource query
+	GetOrgList(context.Context, *GetOrgListQuery) ([]int32, error)
+	GetWorkspaceList(context.Context, *GetWorkspaceListQuery) ([]int32, error)
+	GetProjectList(context.Context, *GetProjectListQuery) ([]int32, error)
 
 	// member mutation
 	// CreateOrgMember(context.Context, *CreateOrgMemberCommand) (*OrgMember, error)
@@ -31,10 +30,10 @@ type Service interface {
 	// DeleteProjectMemberById(context.Context, *DeleteProjectMemberByIdCommand) error
 
 	// user role query and mutation
-	GetRoleList(context.Context) ([]*Role, error)
-	GetRoleById(context.Context, *GetRoleByIdQuery) (*Role, error)
-	CreateRole(context.Context, *CreateRoleCommand) (*Role, error)
-	DeleteRoleById(context.Context, *DeleteRoleByIdCommand) error
+	// GetRoleList(context.Context) ([]*Role, error)
+	// GetRoleById(context.Context, *GetRoleByIdQuery) (*Role, error)
+	// CreateRole(context.Context, *CreateRoleCommand) (*Role, error)
+	// DeleteRoleById(context.Context, *DeleteRoleByIdCommand) error
 }
 
 // type UserGroupService interface {
