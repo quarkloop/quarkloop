927f16120748c0db7cd81b15a1296af908c8fa3d
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-24 23:14:07 +0100

    Update: pkg/service/org/impl/org.go

--- pkg/service/org/impl/org.go
+++ pkg/service/org/impl/org.go
@@ -8,29 +8,30 @@ import (
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/org"
 	"github.com/quarkloop/quarkloop/pkg/service/org/store"
-	"github.com/quarkloop/quarkloop/service/system"
+	"github.com/quarkloop/quarkloop/service/v1/system"
+	grpc "github.com/quarkloop/quarkloop/service/v1/system/org"
 )
 
 type orgService struct {
 	store store.OrgStore
 
-	system.UnimplementedOrgServiceServer
+	grpc.UnimplementedOrgServiceServer
 }
 
 func NewOrgService(ds store.OrgStore) org.Service {
 	return &orgService{store: ds}
 }
 
-func (s *orgService) GetOrgList(ctx context.Context, query *system.GetOrgListQuery) (*system.GetOrgListReply, error) {
+func (s *orgService) GetOrgList(ctx context.Context, query *grpc.GetOrgListQuery) (*grpc.GetOrgListReply, error) {
 	orgList, err := s.store.GetOrgList(ctx, &org.GetOrgListQuery{
-		UserId:     query.UserId,
+		OrgIdList:  query.OrgIdList,
 		Visibility: model.ScopeVisibility(query.Visibility),
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	res := &system.GetOrgListReply{OrgList: make([]*system.Org, len(orgList))}
+	res := &grpc.GetOrgListReply{OrgList: make([]*system.Org, len(orgList))}
 	for i, org := range orgList {
 		if org == nil {
 			continue
@@ -44,7 +45,7 @@ func (s *orgService) GetOrgList(ctx context.Context, query *system.GetOrgListQue
 	return res, nil
 }
 
-func (s *orgService) GetOrgById(ctx context.Context, query *system.GetOrgByIdQuery) (*system.GetOrgByIdReply, error) {
+func (s *orgService) GetOrgById(ctx context.Context, query *grpc.GetOrgByIdQuery) (*grpc.GetOrgByIdReply, error) {
 	org, err := s.store.GetOrgById(ctx, &org.GetOrgByIdQuery{OrgId: query.OrgId})
 	if err != nil {
 		return nil, err
@@ -53,21 +54,21 @@ func (s *orgService) GetOrgById(ctx context.Context, query *system.GetOrgByIdQue
 	org.GeneratePath()
 	proto := org.Proto()
 
-	reply := &system.GetOrgByIdReply{Org: proto}
+	reply := &grpc.GetOrgByIdReply{Org: proto}
 	return reply, nil
 }
 
-func (s *orgService) GetOrgVisibilityById(ctx context.Context, query *system.GetOrgVisibilityByIdQuery) (*system.GetOrgVisibilityByIdReply, error) {
+func (s *orgService) GetOrgVisibilityById(ctx context.Context, query *grpc.GetOrgVisibilityByIdQuery) (*grpc.GetOrgVisibilityByIdReply, error) {
 	visibility, err := s.store.GetOrgVisibilityById(ctx, &org.GetOrgVisibilityByIdQuery{OrgId: query.OrgId})
 	if err != nil {
 		return nil, err
 	}
 
-	reply := &system.GetOrgVisibilityByIdReply{Visibility: int32(visibility)}
+	reply := &grpc.GetOrgVisibilityByIdReply{Visibility: int32(visibility)}
 	return reply, nil
 }
 
-func (s *orgService) CreateOrg(ctx context.Context, cmd *system.CreateOrgCommand) (*system.CreateOrgReply, error) {
+func (s *orgService) CreateOrg(ctx context.Context, cmd *grpc.CreateOrgCommand) (*grpc.CreateOrgReply, error) {
 	org, err := s.store.CreateOrg(ctx, &org.CreateOrgCommand{
 		CreatedBy:   cmd.CreatedBy,
 		ScopeId:     cmd.ScopeId,
@@ -82,11 +83,11 @@ func (s *orgService) CreateOrg(ctx context.Context, cmd *system.CreateOrgCommand
 	org.GeneratePath()
 	proto := org.Proto()
 
-	reply := &system.CreateOrgReply{Org: proto}
+	reply := &grpc.CreateOrgReply{Org: proto}
 	return reply, nil
 }
 
-func (s *orgService) UpdateOrgById(ctx context.Context, cmd *system.UpdateOrgByIdCommand) (*emptypb.Empty, error) {
+func (s *orgService) UpdateOrgById(ctx context.Context, cmd *grpc.UpdateOrgByIdCommand) (*emptypb.Empty, error) {
 	err := s.store.UpdateOrgById(ctx, &org.UpdateOrgByIdCommand{
 		UpdatedBy:   cmd.UpdatedBy,
 		ScopeId:     cmd.ScopeId,
@@ -97,12 +98,12 @@ func (s *orgService) UpdateOrgById(ctx context.Context, cmd *system.UpdateOrgByI
 	return &emptypb.Empty{}, err
 }
 
-func (s *orgService) DeleteOrgById(ctx context.Context, cmd *system.DeleteOrgByIdCommand) (*emptypb.Empty, error) {
+func (s *orgService) DeleteOrgById(ctx context.Context, cmd *grpc.DeleteOrgByIdCommand) (*emptypb.Empty, error) {
 	err := s.store.DeleteOrgById(ctx, &org.DeleteOrgByIdCommand{OrgId: cmd.OrgId})
 	return &emptypb.Empty{}, err
 }
 
-func (s *orgService) GetWorkspaceList(ctx context.Context, query *system.GetWorkspaceListQuery) (*system.GetWorkspaceListReply, error) {
+func (s *orgService) GetWorkspaceList(ctx context.Context, query *grpc.GetWorkspaceListQuery) (*grpc.GetWorkspaceListReply, error) {
 	workspaceList, err := s.store.GetWorkspaceList(ctx, &org.GetWorkspaceListQuery{
 		OrgId:      query.OrgId,
 		Visibility: model.ScopeVisibility(query.Visibility),
@@ -111,7 +112,7 @@ func (s *orgService) GetWorkspaceList(ctx context.Context, query *system.GetWork
 		return nil, err
 	}
 
-	reply := &system.GetWorkspaceListReply{WorkspaceList: make([]*system.Workspace, len(workspaceList))}
+	reply := &grpc.GetWorkspaceListReply{WorkspaceList: make([]*system.Workspace, len(workspaceList))}
 	for i, ws := range workspaceList {
 		if ws == nil {
 			continue
@@ -125,7 +126,7 @@ func (s *orgService) GetWorkspaceList(ctx context.Context, query *system.GetWork
 	return reply, nil
 }
 
-func (s *orgService) GetProjectList(ctx context.Context, query *system.GetProjectListQuery) (*system.GetProjectListReply, error) {
+func (s *orgService) GetProjectList(ctx context.Context, query *grpc.GetProjectListQuery) (*grpc.GetProjectListReply, error) {
 	projectList, err := s.store.GetProjectList(ctx, &org.GetProjectListQuery{
 		OrgId:      query.OrgId,
 		Visibility: model.ScopeVisibility(query.Visibility),
@@ -134,7 +135,7 @@ func (s *orgService) GetProjectList(ctx context.Context, query *system.GetProjec
 		return nil, err
 	}
 
-	reply := &system.GetProjectListReply{ProjectList: make([]*system.Project, len(projectList))}
+	reply := &grpc.GetProjectListReply{ProjectList: make([]*system.Project, len(projectList))}
 	for i, project := range projectList {
 		if project == nil {
 			continue
@@ -148,7 +149,7 @@ func (s *orgService) GetProjectList(ctx context.Context, query *system.GetProjec
 	return reply, nil
 }
 
-// func (s *orgService) GetUserAssignmentList(ctx context.Context, query *system.GetUserAssignmentListQuery) (*system.GetUserAssignmentListReply, error) {
+// func (s *orgService) GetUserAssignmentList(ctx context.Context, query *grpc.GetUserAssignmentListQuery) (*grpc.GetUserAssignmentListReply, error) {
 // 	uaList, err := s.store.GetUserAssignmentList(ctx, query)
 // 	if err != nil {
 // 		return nil, err
