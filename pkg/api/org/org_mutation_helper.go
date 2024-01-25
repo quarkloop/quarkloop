1b73c69a31999b610e0c07592780301d67b042de
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-25 23:53:42 +0100

    Update: pkg/api/org/org_mutation_helper.go

--- pkg/api/org/org_mutation_helper.go
+++ pkg/api/org/org_mutation_helper.go
@@ -1,9 +1,12 @@
 package org
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/quarkloop/quarkloop/pkg/api"
 	"github.com/quarkloop/quarkloop/pkg/contextdata"
@@ -14,21 +17,12 @@ import (
 )
 
 func (s *orgApi) createOrg(ctx *gin.Context, cmd *org.CreateOrgCommand) api.Response {
-	// check permissions
-	access, err := s.evaluateCreatePermission(ctx, accesscontrol.ActionOrgCreate)
-	if err != nil {
-		return api.Error(http.StatusInternalServerError, err) // TODO: change status
-	}
-	if !access {
-		// unauthorized user (permission denied) => return org not found error
-		return api.Error(http.StatusNotFound, org.ErrOrgNotFound) // TODO: change status code and error
-	}
-
 	// check quotas
 	user := contextdata.GetUser(ctx)
 	quotaQuery := &quota.CheckCreateOrgQuotaQuery{UserId: user.GetId()}
 	if err := s.quotaService.CheckCreateOrgQuota(ctx, quotaQuery); err != nil {
-		return api.Error(http.StatusInternalServerError, err) // TODO: change status
+		fmt.Printf("\nOrgQuota => %+v\n\n", err)
+		return api.Error(http.StatusTooManyRequests, err) // TODO: change status
 	}
 
 	org, err := s.orgService.CreateOrg(ctx, &grpc.CreateOrgCommand{
@@ -38,16 +32,38 @@ func (s *orgApi) createOrg(ctx *gin.Context, cmd *org.CreateOrgCommand) api.Resp
 		Description: cmd.Description,
 		Visibility:  int32(cmd.Visibility),
 	})
+	fmt.Printf("\nCreateOrg => %+v => %+v\n\n", err, org)
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.AlreadyExists:
+				return api.Error(http.StatusConflict, err)
+			case codes.Internal:
+				return api.Error(http.StatusInternalServerError, err)
+			case codes.InvalidArgument:
+				return api.Error(http.StatusBadRequest, err)
+			}
+		}
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	aclCommand := &accesscontrol.GrantUserAccessCommand{
+		UserId: user.Id,
+		OrgId:  org.Org.Id,
+		Role:   accesscontrol.RoleOwner,
+	}
+	err = s.aclService.GrantUserAccess(ctx, aclCommand)
+	fmt.Printf("\nGrantUserAccess => %+v \n\n", err)
 	if err != nil {
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
-	return api.Success(http.StatusCreated, org)
+	return api.Success(http.StatusCreated, org.GetOrg())
 }
 
 func (s *orgApi) updateOrgById(ctx *gin.Context, cmd *org.UpdateOrgByIdCommand) api.Response {
 	// check permissions
-	access, err := s.evaluatePermission(ctx, accesscontrol.ActionProjectUpdate, cmd.OrgId)
+	access, err := s.evaluatePermission(ctx, accesscontrol.ActionOrgUpdate, cmd.OrgId)
 	if err != nil {
 		return api.Error(http.StatusInternalServerError, err) // TODO: change status
 	}
@@ -72,8 +88,10 @@ func (s *orgApi) updateOrgById(ctx *gin.Context, cmd *org.UpdateOrgByIdCommand) 
 }
 
 func (s *orgApi) deleteOrgById(ctx *gin.Context, cmd *org.DeleteOrgByIdCommand) api.Response {
+	user := contextdata.GetUser(ctx)
+
 	// check permissions
-	access, err := s.evaluatePermission(ctx, accesscontrol.ActionProjectDelete, cmd.OrgId)
+	access, err := s.evaluatePermission(ctx, accesscontrol.ActionOrgDelete, cmd.OrgId)
 	if err != nil {
 		return api.Error(http.StatusInternalServerError, err) // TODO: change status
 	}
@@ -81,23 +99,25 @@ func (s *orgApi) deleteOrgById(ctx *gin.Context, cmd *org.DeleteOrgByIdCommand) 
 		// unauthorized user (permission denied) => return org not found error
 		return api.Error(http.StatusNotFound, org.ErrOrgNotFound) // TODO: change status code
 	}
+	fmt.Printf("\nRevokeUserAccess (evaluatePermission) => %+v => %+v \n\n", access, err)
 
 	_, err = s.orgService.DeleteOrgById(ctx, &grpc.DeleteOrgByIdCommand{OrgId: cmd.OrgId})
 	if err != nil {
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
-	return api.Success(http.StatusNoContent, nil)
-}
-
-func (s *orgApi) evaluateCreatePermission(ctx *gin.Context, permission string) (bool, error) {
-	user := contextdata.GetUser(ctx)
-	query := &accesscontrol.EvaluateQuery{
-		Permission: permission,
-		UserId:     user.GetId(),
+	aclCommand := &accesscontrol.RevokeUserAccessCommand{
+		UserId: user.Id,
+		OrgId:  cmd.OrgId,
+		Role:   accesscontrol.RoleOwner,
+	}
+	err = s.aclService.RevokeUserAccess(ctx, aclCommand)
+	fmt.Printf("\nRevokeUserAccess => %+v \n\n", err)
+	if err != nil {
+		return api.Error(http.StatusInternalServerError, err)
 	}
 
-	return s.aclService.EvaluateUserAccess(ctx, query)
+	return api.Success(http.StatusNoContent, nil)
 }
 
 func (s *orgApi) evaluatePermission(ctx *gin.Context, permission string, orgId int32) (bool, error) {
