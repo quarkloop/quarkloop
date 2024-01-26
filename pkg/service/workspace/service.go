8a173e438d63f0f510ce7c1dbd3e5465800ea480
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-26 19:06:55 +0100

    Update: pkg/service/workspace/service.go

--- pkg/service/workspace/service.go
+++ pkg/service/workspace/service.go
@@ -1,22 +1,25 @@
 package workspace
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/quarkloop/quarkloop/pkg/model"
-	"github.com/quarkloop/quarkloop/pkg/service/project"
-	"github.com/quarkloop/quarkloop/pkg/service/user"
+	// "github.com/gin-gonic/gin"
+	// "github.com/quarkloop/quarkloop/pkg/model"
+	// "github.com/quarkloop/quarkloop/pkg/service/project"
+	// "github.com/quarkloop/quarkloop/pkg/service/user"
+	"github.com/quarkloop/quarkloop/service/v1/system/workspace"
 )
 
-type Service interface {
-	// query
-	GetWorkspaceById(*gin.Context, *GetWorkspaceByIdQuery) (*Workspace, error)
-	GetWorkspaceVisibilityById(*gin.Context, *GetWorkspaceVisibilityByIdQuery) (model.ScopeVisibility, error)
-	GetWorkspaceList(*gin.Context, *GetWorkspaceListQuery) ([]*Workspace, error)
-	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*project.Project, error)
-	GetUserAssignmentList(*gin.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+type Service workspace.WorkspaceServiceServer
 
-	// mutation
-	CreateWorkspace(*gin.Context, *CreateWorkspaceCommand) (*Workspace, error)
-	UpdateWorkspaceById(*gin.Context, *UpdateWorkspaceByIdCommand) error
-	DeleteWorkspaceById(*gin.Context, *DeleteWorkspaceByIdCommand) error
-}
+// type Service interface {
+// 	// query
+// 	GetWorkspaceById(*gin.Context, *GetWorkspaceByIdQuery) (*Workspace, error)
+// 	GetWorkspaceVisibilityById(*gin.Context, *GetWorkspaceVisibilityByIdQuery) (model.ScopeVisibility, error)
+// 	GetWorkspaceList(*gin.Context, *GetWorkspaceListQuery) ([]*Workspace, error)
+// 	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*project.Project, error)
+// 	GetUserAssignmentList(*gin.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+
+// 	// mutation
+// 	CreateWorkspace(*gin.Context, *CreateWorkspaceCommand) (*Workspace, error)
+// 	UpdateWorkspaceById(*gin.Context, *UpdateWorkspaceByIdCommand) error
+// 	DeleteWorkspaceById(*gin.Context, *DeleteWorkspaceByIdCommand) error
+// }
