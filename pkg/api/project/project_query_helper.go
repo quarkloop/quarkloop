159e0b507a082bcf3970614ecb98aa9ad5bbca7c
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-28 23:48:51 +0100

    Update: pkg/api/project/project_query_helper.go

--- pkg/api/project/project_query_helper.go
+++ pkg/api/project/project_query_helper.go
@@ -1,19 +1,22 @@
 package project
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
 	"github.com/quarkloop/quarkloop/pkg/api"
 	"github.com/quarkloop/quarkloop/pkg/contextdata"
 	"github.com/quarkloop/quarkloop/pkg/model"
 	"github.com/quarkloop/quarkloop/pkg/service/accesscontrol"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
 	"github.com/quarkloop/quarkloop/pkg/service/user"
+	grpc "github.com/quarkloop/quarkloop/service/v1/system/project"
 )
 
 func (s *ProjectApi) getProjectById(ctx *gin.Context, query *project.GetProjectByIdQuery) api.Response {
-	visibility, err := s.projectService.GetProjectVisibilityById(ctx, &project.GetProjectVisibilityByIdQuery{
+	visibility, err := s.projectService.GetProjectVisibilityById(ctx, &grpc.GetProjectVisibilityByIdQuery{
 		OrgId:       query.OrgId,
 		WorkspaceId: query.WorkspaceId,
 		ProjectId:   query.ProjectId,
@@ -25,7 +28,7 @@ func (s *ProjectApi) getProjectById(ctx *gin.Context, query *project.GetProjectB
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
-	isPrivate := visibility == model.PrivateVisibility
+	isPrivate := model.ScopeVisibility(visibility.GetVisibility()) == model.PrivateVisibility
 
 	// anonymous user => return project not found error
 	if isPrivate && contextdata.IsUserAnonymous(ctx) {
@@ -51,54 +54,7 @@ func (s *ProjectApi) getProjectById(ctx *gin.Context, query *project.GetProjectB
 		}
 	}
 
-	p, err := s.projectService.GetProjectById(ctx, query)
-	if err != nil {
-		if err == project.ErrProjectNotFound {
-			return api.Error(http.StatusNotFound, err)
-		}
-		return api.Error(http.StatusInternalServerError, err)
-	}
-
-	// anonymous and unauthorized user => return public project
-	// authorized user => return public or private project
-	return api.Success(http.StatusOK, p)
-}
-
-func (s *ProjectApi) getProjectList(ctx *gin.Context) api.Response {
-	query := &project.GetProjectListQuery{Visibility: model.PublicVisibility}
-	if !contextdata.IsUserAnonymous(ctx) {
-		// check permissions
-		user := contextdata.GetUser(ctx)
-		evalQuery := &accesscontrol.EvaluateQuery{
-			Permission: accesscontrol.ActionProjectList,
-			UserId:     user.GetId(),
-		}
-		access, err := s.aclService.EvaluateUserAccess(ctx, evalQuery)
-		if err != nil {
-			return api.Error(http.StatusInternalServerError, err)
-		}
-		if !access {
-			// unauthorized user (permission denied) => return project not found error
-			return api.Error(http.StatusNotFound, project.ErrProjectNotFound) // TODO: change status code
-		}
-
-		query.UserId = user.GetId()
-		query.Visibility = model.AllVisibility
-	}
-
-	projectList, err := s.projectService.GetProjectList(ctx, query)
-	if err != nil {
-		return api.Error(http.StatusInternalServerError, err)
-	}
-
-	// anonymous user => return public projects
-	// unauthorized user (permission denied) => return public projects
-	// authorized user => return public + private projects
-	return api.Success(http.StatusOK, &projectList)
-}
-
-func (s *ProjectApi) getMemberList(ctx *gin.Context, query *project.GetMemberListQuery) api.Response {
-	visibility, err := s.projectService.GetProjectVisibilityById(ctx, &project.GetProjectVisibilityByIdQuery{
+	p, err := s.projectService.GetProjectById(ctx, &grpc.GetProjectByIdQuery{
 		OrgId:       query.OrgId,
 		WorkspaceId: query.WorkspaceId,
 		ProjectId:   query.ProjectId,
@@ -110,7 +66,57 @@ func (s *ProjectApi) getMemberList(ctx *gin.Context, query *project.GetMemberLis
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
-	isPrivate := visibility == model.PrivateVisibility
+	// anonymous and unauthorized user => return public project
+	// authorized user => return public or private project
+	return api.Success(http.StatusOK, p.GetProject())
+}
+
+func (s *ProjectApi) getProjectList(ctx *gin.Context) api.Response {
+	query := &grpc.GetProjectListQuery{Visibility: int32(model.PublicVisibility)}
+	if !contextdata.IsUserAnonymous(ctx) {
+		// check permissions
+		user := contextdata.GetUser(ctx)
+		aclQuery := &accesscontrol.GetProjectListQuery{
+			Permission: "all",
+			UserId:     user.GetId(),
+		}
+		list, err := s.aclService.GetProjectList(ctx, aclQuery)
+		fmt.Printf("\n[aclService] GetProjectList => %+v => %+v\n", list, err)
+		if err != nil {
+			return api.Error(http.StatusInternalServerError, err)
+		}
+
+		if len(list) > 0 {
+			query.ProjectIdList = list
+			query.Visibility = int32(model.AllVisibility)
+		}
+	}
+
+	projectList, err := s.projectService.GetProjectList(ctx, query)
+	if err != nil {
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	// anonymous user => return public projects
+	// unauthorized user (permission denied) => return public projects
+	// authorized user => return public + private projects
+	return api.Success(http.StatusOK, transformGrpcSlice(projectList.ProjectList))
+}
+
+func (s *ProjectApi) getMemberList(ctx *gin.Context, query *project.GetMemberListQuery) api.Response {
+	visibility, err := s.projectService.GetProjectVisibilityById(ctx, &grpc.GetProjectVisibilityByIdQuery{
+		OrgId:       query.OrgId,
+		WorkspaceId: query.WorkspaceId,
+		ProjectId:   query.ProjectId,
+	})
+	if err != nil {
+		if err == project.ErrProjectNotFound {
+			return api.Error(http.StatusNotFound, err)
+		}
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	isPrivate := model.ScopeVisibility(visibility.GetVisibility()) == model.PrivateVisibility
 
 	// anonymous user => return project not found error
 	if isPrivate && contextdata.IsUserAnonymous(ctx) {
@@ -136,11 +142,10 @@ func (s *ProjectApi) getMemberList(ctx *gin.Context, query *project.GetMemberLis
 		}
 	}
 
-	uaList, err := s.projectService.GetUserAssignmentList(ctx, &project.GetUserAssignmentListQuery{
-		OrgId:       query.OrgId,
-		WorkspaceId: query.WorkspaceId,
-		ProjectId:   query.ProjectId,
-	})
+	uaList, err := s.aclService.GetProjectMemberList(ctx, &accesscontrol.GetProjectMemberListQuery{ProjectId: query.ProjectId})
+	if err != nil {
+		return api.Error(http.StatusInternalServerError, err)
+	}
 	if err != nil {
 		return api.Error(http.StatusInternalServerError, err)
 	}
