5dc6c688c5fe5ffa3d16ca9c6b38a38d2e44b789
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-28 23:48:51 +0100

    Update: pkg/api/project/project_mutation_helper.go

--- pkg/api/project/project_mutation_helper.go
+++ pkg/api/project/project_mutation_helper.go
@@ -1,21 +1,21 @@
 package project
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/quarkloop/quarkloop/pkg/api"
 	"github.com/quarkloop/quarkloop/pkg/contextdata"
 	"github.com/quarkloop/quarkloop/pkg/service/accesscontrol"
 	"github.com/quarkloop/quarkloop/pkg/service/project"
 	"github.com/quarkloop/quarkloop/pkg/service/quota"
-	"github.com/quarkloop/quarkloop/pkg/service/table_branch"
+	grpc "github.com/quarkloop/quarkloop/service/v1/system/project"
 )
 
 func (s *ProjectApi) createProject(ctx *gin.Context, cmd *project.CreateProjectCommand) api.Response {
-	user := contextdata.GetUser(ctx)
-
 	// check permissions
 	access, err := s.evaluateCreatePermission(ctx, accesscontrol.ActionProjectCreate, cmd.OrgId, cmd.WorkspaceId)
 	if err != nil {
@@ -23,80 +23,109 @@ func (s *ProjectApi) createProject(ctx *gin.Context, cmd *project.CreateProjectC
 	}
 	if !access {
 		// unauthorized user (permission denied) => return project not found error
-		return api.Error(http.StatusNotFound, project.ErrProjectNotFound) // TODO: change status code and error
+		return api.Error(http.StatusForbidden, accesscontrol.ErrPermissionDenied) // TODO: change status code and error
 	}
 
 	// check quotas
 	quotaQuery := &quota.CheckCreateProjectQuotaQuery{OrgId: cmd.OrgId}
 	if err := s.quotaService.CheckCreateProjectQuota(ctx, quotaQuery); err != nil {
-		return api.Error(http.StatusInternalServerError, err) // TODO: change status
+		return api.Error(http.StatusTooManyRequests, err)
 	}
 
-	p, err := s.projectService.CreateProject(ctx, cmd)
+	p, err := s.projectService.CreateProject(ctx, &grpc.CreateProjectCommand{
+		OrgId:       cmd.OrgId,
+		WorkspaceId: cmd.WorkspaceId,
+		CreatedBy:   cmd.CreatedBy,
+		ScopeId:     cmd.ScopeId,
+		Name:        cmd.Name,
+		Description: cmd.Description,
+		Visibility:  int32(cmd.Visibility),
+	})
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.AlreadyExists:
+				return api.Error(http.StatusConflict, err)
+			case codes.Internal:
+				return api.Error(http.StatusInternalServerError, err)
+			case codes.InvalidArgument:
+				return api.Error(http.StatusBadRequest, err)
+			}
+		}
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	// mainBranch, err := s.branchService.CreateTableBranch(ctx, &table_branch.CreateTableBranchParams{
+	// 	ProjectId: p.Project.Id,
+	// 	Branch: &table_branch.TableBranch{
+	// 		Name:        "main",
+	// 		Type:        "main",
+	// 		Default:     true,
+	// 		Description: "main branch",
+	// 		CreatedBy:   "admin", // TODO
+	// 	},
+	// })
+	// if err != nil {
+	// 	joinedErr := errors.Join(err)
+
+	// 	_, deleteErr := s.projectService.DeleteProjectById(ctx, &grpc.DeleteProjectByIdCommand{
+	// 		OrgId:       cmd.OrgId,
+	// 		WorkspaceId: cmd.WorkspaceId,
+	// 		ProjectId:   p.Project.Id,
+	// 	})
+	// 	if deleteErr != nil {
+	// 		joinedErr = errors.Join(deleteErr)
+	// 	}
+
+	// 	return api.Error(http.StatusInternalServerError, joinedErr)
+	// }
+
+	// submissionBranch, err := s.branchService.CreateTableBranch(ctx, &table_branch.CreateTableBranchParams{
+	// 	ProjectId: p.Project.Id,
+	// 	Branch: &table_branch.TableBranch{
+	// 		Name:        "submission",
+	// 		Type:        "submission",
+	// 		Default:     false,
+	// 		Description: "submission branch",
+	// 		CreatedBy:   "user", // TODO
+	// 	},
+	// })
+	// if err != nil {
+	// 	joinedErr := errors.Join(err)
+
+	// 	_, deleteErr := s.projectService.DeleteProjectById(ctx, &grpc.DeleteProjectByIdCommand{
+	// 		OrgId:       cmd.OrgId,
+	// 		WorkspaceId: cmd.WorkspaceId,
+	// 		ProjectId:   p.Project.Id,
+	// 	})
+	// 	if deleteErr != nil {
+	// 		joinedErr = errors.Join(deleteErr)
+	// 	}
+	// 	deleteErr = s.branchService.DeleteTableBranchById(ctx, &table_branch.DeleteTableBranchByIdParams{
+	// 		ProjectId: p.Project.Id,
+	// 		BranchId:  mainBranch.Id,
+	// 	})
+	// 	if deleteErr != nil {
+	// 		joinedErr = errors.Join(deleteErr)
+	// 	}
+
+	// 	return api.Error(http.StatusInternalServerError, joinedErr)
+	// }
+
+	//p.Branches = append(p.Project.Branches, mainBranch, submissionBranch)
+
+	relationCmd := &accesscontrol.MakeParentResourceCommand{
+		ParentResource:   "workspace",
+		ParentResourceId: cmd.WorkspaceId,
+		ChildResource:    "project",
+		ChildResourceId:  p.Project.Id,
+	}
+	err = s.aclService.MakeParentResource(ctx, relationCmd)
 	if err != nil {
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
-	mainBranch, err := s.branchService.CreateTableBranch(ctx, &table_branch.CreateTableBranchParams{
-		ProjectId: p.Id,
-		Branch: &table_branch.TableBranch{
-			Name:        "main",
-			Type:        "main",
-			Default:     true,
-			Description: "main branch",
-			CreatedBy:   user.Name, // TODO
-		},
-	})
-	if err != nil {
-		joinedErr := errors.Join(err)
-
-		deleteErr := s.projectService.DeleteProjectById(ctx, &project.DeleteProjectByIdCommand{
-			OrgId:       cmd.OrgId,
-			WorkspaceId: cmd.WorkspaceId,
-			ProjectId:   p.Id,
-		})
-		if deleteErr != nil {
-			joinedErr = errors.Join(deleteErr)
-		}
-
-		return api.Error(http.StatusInternalServerError, joinedErr)
-	}
-
-	submissionBranch, err := s.branchService.CreateTableBranch(ctx, &table_branch.CreateTableBranchParams{
-		ProjectId: p.Id,
-		Branch: &table_branch.TableBranch{
-			Name:        "submission",
-			Type:        "submission",
-			Default:     false,
-			Description: "submission branch",
-			CreatedBy:   "user", // TODO
-		},
-	})
-	if err != nil {
-		joinedErr := errors.Join(err)
-
-		deleteErr := s.projectService.DeleteProjectById(ctx, &project.DeleteProjectByIdCommand{
-			OrgId:       cmd.OrgId,
-			WorkspaceId: cmd.WorkspaceId,
-			ProjectId:   p.Id,
-		})
-		if deleteErr != nil {
-			joinedErr = errors.Join(deleteErr)
-		}
-		deleteErr = s.branchService.DeleteTableBranchById(ctx, &table_branch.DeleteTableBranchByIdParams{
-			ProjectId: p.Id,
-			BranchId:  mainBranch.Id,
-		})
-		if deleteErr != nil {
-			joinedErr = errors.Join(deleteErr)
-		}
-
-		return api.Error(http.StatusInternalServerError, joinedErr)
-	}
-
-	p.Branches = append(p.Branches, mainBranch, submissionBranch)
-
-	return api.Success(http.StatusOK, p)
+	return api.Success(http.StatusCreated, p.GetProject())
 }
 
 func (s *ProjectApi) updateProjectById(ctx *gin.Context, cmd *project.UpdateProjectByIdCommand) api.Response {
@@ -110,7 +139,17 @@ func (s *ProjectApi) updateProjectById(ctx *gin.Context, cmd *project.UpdateProj
 		return api.Error(http.StatusNotFound, project.ErrProjectNotFound) // TODO: change status code and error
 	}
 
-	if err := s.projectService.UpdateProjectById(ctx, cmd); err != nil {
+	_, err = s.projectService.UpdateProjectById(ctx, &grpc.UpdateProjectByIdCommand{
+		OrgId:       cmd.OrgId,
+		WorkspaceId: cmd.WorkspaceId,
+		ProjectId:   cmd.ProjectId,
+		UpdatedBy:   cmd.UpdatedBy,
+		ScopeId:     cmd.ScopeId,
+		Name:        cmd.Name,
+		Description: cmd.Description,
+		Visibility:  int32(cmd.Visibility),
+	})
+	if err != nil {
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
@@ -128,7 +167,22 @@ func (s *ProjectApi) deleteProjectById(ctx *gin.Context, cmd *project.DeleteProj
 		return api.Error(http.StatusNotFound, project.ErrProjectNotFound) // TODO: change status code and error
 	}
 
-	if err := s.projectService.DeleteProjectById(ctx, cmd); err != nil {
+	_, err = s.projectService.DeleteProjectById(ctx, &grpc.DeleteProjectByIdCommand{
+		OrgId:       cmd.OrgId,
+		WorkspaceId: cmd.WorkspaceId,
+		ProjectId:   cmd.ProjectId,
+	})
+	if err != nil {
+		return api.Error(http.StatusInternalServerError, err)
+	}
+
+	aclCommand := &accesscontrol.RevokeUserAccessCommand{
+		OrgId:       cmd.OrgId,
+		WorkspaceId: cmd.WorkspaceId,
+		ProjectId:   cmd.ProjectId,
+	}
+	err = s.aclService.RevokeUserAccess(ctx, aclCommand)
+	if err != nil {
 		return api.Error(http.StatusInternalServerError, err)
 	}
 
