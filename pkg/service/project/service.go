f3425dd7a7ada8db1c4c5e90963b10bc9ecce7b3
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-28 23:48:51 +0100

    Update: pkg/service/project/service.go

--- pkg/service/project/service.go
+++ pkg/service/project/service.go
@@ -1,20 +1,23 @@
 package project
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/quarkloop/quarkloop/pkg/model"
-	"github.com/quarkloop/quarkloop/pkg/service/user"
+	// "github.com/gin-gonic/gin"
+	// "github.com/quarkloop/quarkloop/pkg/model"
+	// "github.com/quarkloop/quarkloop/pkg/service/user"
+	"github.com/quarkloop/quarkloop/service/v1/system/project"
 )
 
-type Service interface {
-	// query
-	GetProjectById(*gin.Context, *GetProjectByIdQuery) (*Project, error)
-	GetProjectVisibilityById(*gin.Context, *GetProjectVisibilityByIdQuery) (model.ScopeVisibility, error)
-	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*Project, error)
-	GetUserAssignmentList(*gin.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+type Service project.ProjectServiceServer
 
-	// mutation
-	CreateProject(*gin.Context, *CreateProjectCommand) (*Project, error)
-	UpdateProjectById(*gin.Context, *UpdateProjectByIdCommand) error
-	DeleteProjectById(*gin.Context, *DeleteProjectByIdCommand) error
-}
+// type Service interface {
+// 	// query
+// 	GetProjectById(*gin.Context, *GetProjectByIdQuery) (*model.Project, error)
+// 	GetProjectVisibilityById(*gin.Context, *GetProjectVisibilityByIdQuery) (model.ScopeVisibility, error)
+// 	GetProjectList(*gin.Context, *GetProjectListQuery) ([]*model.Project, error)
+// 	GetUserAssignmentList(*gin.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+
+// 	// mutation
+// 	CreateProject(*gin.Context, *CreateProjectCommand) (*model.Project, error)
+// 	UpdateProjectById(*gin.Context, *UpdateProjectByIdCommand) error
+// 	DeleteProjectById(*gin.Context, *DeleteProjectByIdCommand) error
+// }
