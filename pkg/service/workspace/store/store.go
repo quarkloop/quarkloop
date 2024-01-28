1d8df848f4f6bf4bf2b50e045129ea62d95e5e07
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-01-28 23:48:51 +0100

    Update: pkg/service/workspace/store/store.go

--- pkg/service/workspace/store/store.go
+++ pkg/service/workspace/store/store.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/quarkloop/quarkloop/pkg/model"
-	"github.com/quarkloop/quarkloop/pkg/service/project"
 	"github.com/quarkloop/quarkloop/pkg/service/user"
 	"github.com/quarkloop/quarkloop/pkg/service/workspace"
 )
@@ -15,7 +14,7 @@ type WorkspaceStore interface {
 	GetWorkspaceById(context.Context, *workspace.GetWorkspaceByIdQuery) (*model.Workspace, error)
 	GetWorkspaceVisibilityById(context.Context, *workspace.GetWorkspaceVisibilityByIdQuery) (model.ScopeVisibility, error)
 	GetWorkspaceList(context.Context, *workspace.GetWorkspaceListQuery) ([]*model.Workspace, error)
-	GetProjectList(context.Context, *workspace.GetProjectListQuery) ([]*project.Project, error)
+	GetProjectList(context.Context, *workspace.GetProjectListQuery) ([]*model.Project, error)
 	GetUserAssignmentList(context.Context, *workspace.GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
 
 	// mutation
