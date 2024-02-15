dcabaa906ac00277763e373250a0540badf6d784
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-02-15 23:54:04 +0100

    Delete: apps/quarkloop-client/app/(platform)/users/settings/profile/page.tsx

--- apps/quarkloop-client/app/(platform)/users/settings/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-import { UserProfile } from "@quarkloop/components";
-
-export const dynamic = "force-dynamic";
-
-const UserProfilePage = (props: any) => <UserProfile />;
-
-export default UserProfilePage;
