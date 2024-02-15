f9728fac7d580207c2f5337b81d759c0686d67ec
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-02-15 23:54:04 +0100

    Delete: apps/quarkloop-client/app/(platform)/users/settings/linked-accounts/page.tsx

--- apps/quarkloop-client/app/(platform)/users/settings/linked-accounts/page.tsx
+++
@@ -1,7 +0,0 @@
-import { UserLinkedAccountList } from "@quarkloop/components";
-
-export const dynamic = "force-dynamic";
-
-const UserLinkedAccountsPage = (props: any) => <UserLinkedAccountList />;
-
-export default UserLinkedAccountsPage;
