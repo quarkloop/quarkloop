6b54807fd4a3183059506d6f8bab1bef552b52ef
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-02-15 23:53:07 +0100

    Delete: packages/lib/src/service/hook.ts

--- packages/lib/src/service/hook.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import type { AppState, AppDispatch } from "./store";
-
-// Use throughout your project instead of plain `useDispatch` and `useSelector`
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
-export const useAppDispatch: () => AppDispatch = useDispatch;
