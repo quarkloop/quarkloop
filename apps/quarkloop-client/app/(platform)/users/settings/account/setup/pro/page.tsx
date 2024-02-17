a04ef4d1790f68d38bb4bbc50f2288fc31b451f9
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-02-17 23:42:55 +0100

    Delete: apps/quarkloop-client/app/(platform)/users/settings/account/setup/pro/page.tsx

--- apps/quarkloop-client/app/(platform)/users/settings/account/setup/pro/page.tsx
+++
@@ -1,9 +0,0 @@
-const ProfessionalAccountSetup = (props: any) => {
-  const {
-    searchParams: { plan, next },
-  } = props;
-
-  return <div>Professional Account Setup: {JSON.stringify(props)}</div>;
-};
-
-export default ProfessionalAccountSetup;
