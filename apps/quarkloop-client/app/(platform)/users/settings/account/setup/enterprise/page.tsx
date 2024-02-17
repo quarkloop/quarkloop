0694377f51b57af8c2eb6beeac110d9790462448
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-02-17 23:42:55 +0100

    Delete: apps/quarkloop-client/app/(platform)/users/settings/account/setup/enterprise/page.tsx

--- apps/quarkloop-client/app/(platform)/users/settings/account/setup/enterprise/page.tsx
+++
@@ -1,9 +0,0 @@
-const EnterpriseAccountSetup = (props: any) => {
-  const {
-    searchParams: { plan, next },
-  } = props;
-
-  return <div>Enterprise Account Setup: {JSON.stringify(props)}</div>;
-};
-
-export default EnterpriseAccountSetup;
