a17c2650696c5604701ac46693fa1fb5b8b1f4a4
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-02-19 23:48:08 +0100

    [CLI] add: cli/command/image/cmd.go

---
+++ cli/command/image/cmd.go
@@ -0,0 +1,20 @@
+package image
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func NewImageCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "image",
+		Short: "Manage images",
+		Args:  cobra.NoArgs,
+	}
+
+	cmd.AddCommand(
+		NewImageCreateCommand(),
+		NewImageRemoveCommand(),
+		NewImageListCommand(),
+	)
+	return cmd
+}
