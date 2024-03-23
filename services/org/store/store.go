c56e813bdd94cd301420cac33714d7ff2c54cd82
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-03-23 23:40:40 +0100

    Add: services/org/store/store.go

---
+++ services/org/store/store.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/quarkloop/quarkloop/pkg/model"
+)
+
+type OrgStore interface {
+	// query
+	GetOrgId(context.Context, string) (int64, error)
+	GetOrgById(context.Context, *GetOrgByIdQuery) (*model.Org, error)
+	GetOrgVisibilityById(context.Context, *GetOrgVisibilityByIdQuery) (model.ScopeVisibility, error)
+	GetOrgList(context.Context, *GetOrgListQuery) ([]*model.Org, error)
+	GetWorkspaceList(context.Context, *GetWorkspaceListQuery) ([]*model.Workspace, error)
+	GetProjectList(context.Context, *GetProjectListQuery) ([]*model.Project, error)
+	//GetUserAssignmentList(context.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+
+	// mutation
+	CreateOrg(context.Context, *CreateOrgCommand) (*model.Org, error)
+	UpdateOrgById(context.Context, *UpdateOrgByIdCommand) error
+	DeleteOrgById(context.Context, *DeleteOrgByIdCommand) error
+}
+
+type orgStore struct {
+	Conn *pgx.Conn
+}
+
+func NewOrgStore(conn *pgx.Conn) *orgStore {
+	return &orgStore{
+		Conn: conn,
+	}
+}
