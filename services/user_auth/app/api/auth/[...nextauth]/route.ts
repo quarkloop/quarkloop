684087e23b542ffe8b35c987fb015d89138098ea
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-03-24 23:47:46 +0100

    Add: services/user_auth/app/api/auth/[...nextauth]/route.ts

---
+++ services/user_auth/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+
+import { authOptions } from "@/lib";
+
+const handler = NextAuth(authOptions);
+
+export async function GET(request: Request, params: { params: any }) {
+    return handler(request, params);
+}
+
+export async function POST(request: Request, params: { params: any }) {
+    return handler(request, params);
+}
+
+// resources
+// https://stackoverflow.com/a/73510917/1789729
+// https://github.com/nextauthjs/next-auth/discussions/4378
+// https://github.com/wpcodevo/nextauth-nextjs13-prisma/tree/main
+// https://stackoverflow.com/questions/73507135/nextauth-handle-refresh-token-rotation-reddit-with-database
+// https://stackoverflow.com/questions/71363829/next-auth-js-i-cant-get-token-with-gettokenreq
+// https://stackoverflow.com/questions/74984195/how-to-decrypt-session-token-next-auth-jwt-token
+// https://stackoverflow.com/questions/69068495/how-to-get-the-provider-access-token-in-next-auth
