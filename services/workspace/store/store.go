f5f369d184db0c1bf264b90be1ecbfcc5586effb
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-03-24 23:47:46 +0100

    Add: services/workspace/store/store.go

---
+++ services/workspace/store/store.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/quarkloop/quarkloop/pkg/model"
+)
+
+type WorkspaceStore interface {
+	// query
+	GetWorkspaceId(ctx context.Context, query *GetWorkspaceIdQuery) (int64, int64, error)
+	GetWorkspaceById(context.Context, *GetWorkspaceByIdQuery) (*model.Workspace, error)
+	GetWorkspaceVisibilityById(context.Context, *GetWorkspaceVisibilityByIdQuery) (model.ScopeVisibility, error)
+	GetWorkspaceList(context.Context, *GetWorkspaceListQuery) ([]*model.Workspace, error)
+	GetProjectList(context.Context, *GetProjectListQuery) ([]*model.Project, error)
+	//GetUserAssignmentList(context.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+
+	// mutation
+	CreateWorkspace(context.Context, *CreateWorkspaceCommand) (*model.Workspace, error)
+	UpdateWorkspaceById(context.Context, *UpdateWorkspaceByIdCommand) error
+	DeleteWorkspaceById(context.Context, *DeleteWorkspaceByIdCommand) error
+}
+
+type workspaceStore struct {
+	Conn *pgx.Conn
+}
+
+func NewWorkspaceStore(conn *pgx.Conn) *workspaceStore {
+	return &workspaceStore{
+		Conn: conn,
+	}
+}
