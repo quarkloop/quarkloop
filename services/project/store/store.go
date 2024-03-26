83d0b41051bcea406deb991d650229d08cc8e136
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-03-26 23:47:46 +0100

    Add: services/project/store/store.go

---
+++ services/project/store/store.go
@@ -0,0 +1,32 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/quarkloop/quarkloop/pkg/model"
+)
+
+type ProjectStore interface {
+	// query
+	GetProjectId(ctx context.Context, query *GetProjectIdQuery) (int64, int64, int64, error)
+	GetProjectById(context.Context, *GetProjectByIdQuery) (*model.Project, error)
+	GetProjectVisibilityById(context.Context, *GetProjectVisibilityByIdQuery) (model.ScopeVisibility, error)
+	GetProjectList(context.Context, *GetProjectListQuery) ([]*model.Project, error)
+	//GetUserAssignmentList(context.Context, *GetUserAssignmentListQuery) ([]*user.UserAssignment, error)
+
+	// mutation
+	CreateProject(context.Context, *CreateProjectCommand) (*model.Project, error)
+	UpdateProjectById(context.Context, *UpdateProjectByIdCommand) error
+	DeleteProjectById(context.Context, *DeleteProjectByIdCommand) error
+}
+
+type projectStore struct {
+	Conn *pgx.Conn
+}
+
+func NewProjectStore(conn *pgx.Conn) *projectStore {
+	return &projectStore{
+		Conn: conn,
+	}
+}
