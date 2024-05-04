a90dbaf6f840a5cdd8ef7f3777c29ae2a5e650b5
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-04 23:57:35 +0200

    Update: services/user_auth/app/api/auth/check/route.ts

--- services/user_auth/app/api/auth/check/route.ts
+++ services/user_auth/app/api/auth/check/route.ts
@@ -6,7 +6,12 @@ import { authOptions } from "@/lib";
 export async function GET(request: Request, params: { params: any }) {
     const session = await getServerSession(authOptions);
     const body = session?.user ?? {};
+    // console.log(request.headers.get("cookie"));
 
+    console.log(body);
+
+    // 401: Unauthenticated
+    // 200: OK
     return NextResponse.json(body, {
         status: session ? 200 : 401,
     });
