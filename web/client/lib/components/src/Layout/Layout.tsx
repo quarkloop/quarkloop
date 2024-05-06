2fd24fed98e19470ba5261193ac0b1479a4e6ca4
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-06 16:57:14 +0200

    Add Layout: web/client/lib/components/src/Layout/Layout.tsx

---
+++ web/client/lib/components/src/Layout/Layout.tsx
@@ -0,0 +1,40 @@
+import { Role } from "@/components/Auth";
+import { getUserAccess } from "@/components/Auth/Role/server";
+
+import { Header } from "./Layout.Header";
+import { Sidebar } from "./Layout.sidebar";
+import { getLinks } from "./Layout.links";
+
+interface LayoutProps {
+    children: React.ReactNode;
+    userId?: string;
+    orgSid?: string;
+    workspaceSid?: string;
+}
+
+const Layout = async (props: LayoutProps) => {
+    const { children, userId, orgSid, workspaceSid } = props;
+
+    let role: Role | null = null;
+    if (orgSid) {
+        role = await getUserAccess({ orgSid, workspaceSid });
+    }
+    const links = getLinks(role, userId, orgSid, workspaceSid);
+
+    return (
+        <main className="flex-1 flex">
+            <Sidebar
+                links={links}
+                role={role}
+            />
+            <div className="flex-1 flex flex-col dark:bg-[#343434]">
+                <nav className="basis-14 max-h-14 flex flex-col border-b border-b-neutral-200">
+                    <Header />
+                </nav>
+                <div className="flex-1 flex">{children}</div>
+            </div>
+        </main>
+    );
+};
+
+export { Layout };
