0754277759f712900acc1a159cb31982cfd5397f
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-06 16:56:20 +0200

    Add Auth: web/client/lib/components/src/Auth/Role/Role.endpoint.ts

---
+++ web/client/lib/components/src/Auth/Role/Role.endpoint.ts
@@ -0,0 +1,72 @@
+"use client";
+
+import { enpointApi } from "@quarkloop/lib";
+import {
+    GetUserAccessApiResponse,
+    GetUserAccessApiArgs,
+    getUserAccessApiArgsSchema,
+    getUserAccessApiResponseSchema,
+} from "./Role.endpoint.schema";
+
+const endpoint = enpointApi
+    .enhanceEndpoints({ addTagTypes: ["role"] })
+    .injectEndpoints({
+        endpoints: (builder) => ({
+            // getUserAccess endpoint
+            getUserAccess: builder.query<
+                GetUserAccessApiResponse,
+                GetUserAccessApiArgs
+            >({
+                query: (queryArg) => ({
+                    url: `/auth/role`,
+                    method: "GET",
+                    params: {
+                        orgSid: queryArg.orgSid,
+                        workspaceSid: queryArg.workspaceSid,
+                    },
+                }),
+                providesTags: ["role"],
+                extraOptions: {
+                    argSchema: getUserAccessApiArgsSchema,
+                    responseSchema: getUserAccessApiResponseSchema,
+                },
+            }),
+
+            // // grantUserAccess endpoint
+            // grantUserAccess: builder.mutation<GrantUserAccessApiResponse, GrantUserAccessApiArgs>(
+            //     {
+            //         query: (queryArg) => ({
+            //             url: `/manage/orgs`,
+            //             method: "POST",
+            //             body: queryArg,
+            //         }),
+            //         invalidatesTags: ["org"],
+            //         extraOptions: {
+            //             argSchema: grantUserAccessApiArgsSchema,
+            //             responseSchema: grantUserAccessApiResponseSchema,
+            //         },
+            //     }
+            // ),
+
+            // // revokeUserAccess endpoint
+            // revokeUserAccess: builder.mutation<
+            //     DeleteUserAccessApiResponse,
+            //     DeleteUserAccessApiArgs
+            // >({
+            //     query: (queryArg) => ({
+            //         url: `/manage/${queryArg.orgSid}`,
+            //         method: "DELETE",
+            //     }),
+            //     invalidatesTags: ["org"],
+            //     extraOptions: {
+            //         argSchema: revokeUserAccessApiArgsSchema,
+            //     },
+            // }),
+        }),
+    });
+
+export const {
+    useGetUserAccessQuery,
+    // useGrantUserAccessMutation,
+    // useRevokeUserAccessMutation,
+} = endpoint;
