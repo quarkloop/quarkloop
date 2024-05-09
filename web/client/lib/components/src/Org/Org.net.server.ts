666cd502ae23902d18fa847a75f52155d65f842f
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-09 23:33:52 +0200

    Add: web/client/lib/components/src/Org/Org.net.server.ts

---
+++ web/client/lib/components/src/Org/Org.net.server.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { ZodSchema } from "zod";
+
+import {
+    GetOrgByIdApiArgs,
+    GetOrgByIdApiResponse,
+    getOrgByIdSchema,
+} from "./Org.net.schema";
+
+interface Link {
+    query: (params: any) => string;
+    queryFn: (link: Link, params: any) => Promise<unknown | null>;
+    schema: {
+        arg?: ZodSchema;
+        response: ZodSchema;
+    };
+}
+
+interface ServerLink {
+    [key: string]: Link;
+}
+
+const serverLinks: ServerLink = {
+    getOrgById: {
+        query: ({ orgSid }: GetOrgByIdApiArgs) =>
+            `http://localhost:8000/api/v1/manage/${orgSid}`,
+        queryFn: getFn,
+        schema: {
+            response: getOrgByIdSchema,
+        },
+    },
+};
+
+const getOrgById = async (
+    params: GetOrgByIdApiArgs
+): Promise<GetOrgByIdApiResponse | null> => {
+    const link = serverLinks.getOrgById;
+    return (await link.queryFn(link, params)) as GetOrgByIdApiResponse;
+};
+
+export async function getFn(link: Link, params: any): Promise<unknown | null> {
+    const res = await fetch(link.query(params), {
+        credentials: "include",
+        headers: { Cookie: cookies().toString() },
+    });
+    if (res.status !== 200) {
+        return null;
+    }
+
+    const jsonBody = await res.json();
+    return await link.schema.response.parseAsync(jsonBody);
+}
+
+export { getOrgById };
