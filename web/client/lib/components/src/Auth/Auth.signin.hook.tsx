4ddbc0564fe6ea25366a54840bf389c77f3f0128
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-09 23:32:47 +0200

    Add: web/client/lib/components/src/Auth/Auth.signin.hook.tsx

---
+++ web/client/lib/components/src/Auth/Auth.signin.hook.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useCallback, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useForm } from "@mantine/form";
+
+import { UseHookReturnType } from "@quarkloop/lib";
+import { SigninProps } from "./Auth.signin.schema";
+
+interface SigninForm {
+    email: string;
+    password: string;
+}
+
+export const useSignin = (props: SigninProps): any => {
+    // : UseHookReturnType<SigninForm, Session>
+    const { variant } = props;
+    const router = useRouter();
+
+    const form = useForm<SigninForm>({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+    });
+    const { data: session } = useSession();
+
+    const redirectTo = useCallback(
+        (path: string) => {
+            router.push(path);
+        },
+        [router]
+    );
+
+    const signInWithCredentials = useCallback(async (values: SigninForm) => {
+        // const response = await signIn("credentials", {
+        //     redirect: false,
+        //     username: values.email,
+        //     password: values.password,
+        // });
+    }, []);
+
+    const submitForm = useCallback(
+        async (values: SigninForm) => {
+            return signInWithCredentials(values);
+        },
+        [signInWithCredentials]
+    );
+
+    useEffect(() => {
+        console.log(
+            "getSession",
+            session,
+            process.env.NEXTAUTH_URL_INTERNAL,
+            process.env.NEXTAUTH_SECRET
+        );
+
+        if (session) {
+            redirectTo("/");
+        }
+    }, [session, redirectTo]);
+
+    return {
+        auth: session,
+        triggers: {
+            signInWithCredentials,
+            submitForm,
+            redirectTo,
+        },
+        ui: { form },
+    };
+};
