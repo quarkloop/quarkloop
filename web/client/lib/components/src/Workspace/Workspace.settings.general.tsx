4154b0783588dd437f4efb68d3255156a7fedb5f
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-09 23:35:41 +0200

    Add: web/client/lib/components/src/Workspace/Workspace.settings.general.tsx

---
+++ web/client/lib/components/src/Workspace/Workspace.settings.general.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { HTMLAttributes, useCallback, useMemo } from "react";
+
+import {
+    useGetWorkspaceByIdQuery,
+    useUpdateWorkspaceByIdMutation,
+} from "./Workspace.net.client";
+import {
+    WorkspaceGeneralSettingsForm,
+    WorkspaceGeneralSettingsFormData,
+} from "./Workspace.settings.general.form";
+
+const useWorkspaceGeneralSettings = (orgSid: string, workspaceSid: string) => {
+    const { data: workspace } = useGetWorkspaceByIdQuery({
+        orgSid,
+        workspaceSid,
+    });
+    const [updateWorkspace] = useUpdateWorkspaceByIdMutation();
+
+    const onFormSubmit = useCallback(
+        async (data: WorkspaceGeneralSettingsFormData) => {
+            try {
+                await updateWorkspace({
+                    orgSid,
+                    workspaceSid,
+                    payload: data,
+                }).unwrap();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        []
+    );
+
+    const initialValues = useMemo(
+        () =>
+            ({
+                id: workspace?.data.id ?? 0,
+                sid: workspace?.data.sid ?? "",
+                name: workspace?.data.name ?? "",
+                description: workspace?.data.description ?? "",
+                path: workspace?.data.path ?? "",
+                createdBy: workspace?.data.createdBy ?? "",
+                updatedBy: workspace?.data.updatedBy ?? "",
+            } as WorkspaceGeneralSettingsFormData),
+        [workspace?.data]
+    );
+
+    return {
+        initialValues,
+        onFormSubmit,
+    };
+};
+
+interface WorkspaceGeneralSettingsProps extends HTMLAttributes<HTMLDivElement> {
+    orgSid: string;
+    workspaceSid: string;
+}
+
+const WorkspaceGeneralSettings = (props: WorkspaceGeneralSettingsProps) => {
+    const { orgSid, workspaceSid } = props;
+    const { initialValues, onFormSubmit } = useWorkspaceGeneralSettings(
+        orgSid,
+        workspaceSid
+    );
+
+    return (
+        <div className="py-14 flex-1 flex flex-col items-center gap-5">
+            <div className="flex items-center gap-3 text-3xl font-medium">
+                General Settings
+            </div>
+            <WorkspaceGeneralSettingsForm
+                readOnly={false}
+                initialValues={initialValues}
+                onFormSubmit={onFormSubmit}
+            />
+        </div>
+    );
+};
+
+export { WorkspaceGeneralSettings };
