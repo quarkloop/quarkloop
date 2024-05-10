86f82e9e0a61730d914a0689667408745efc9eba
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-10 23:38:58 +0200

    Update: web/client/lib/components/src/Workspace/index.ts

--- web/client/lib/components/src/Workspace/index.ts
+++ web/client/lib/components/src/Workspace/index.ts
@@ -1,3 +1,8 @@
-export { WorkspaceList } from "./WorkspaceList";
-export { WorkspaceCreate } from "./WorkspaceCreate";
-export { WorkspaceSettings } from "./WorkspaceSettings";
+export * from "./Workspace.net.client";
+export * from "./Workspace.schema";
+export * from "./Workspace.create";
+export * from "./Workspace.list";
+export * from "./Workspace.members";
+export * from "./Workspace.overview";
+export * from "./Workspace.settings.general";
+export * from "./Workspace.settings.dangerzone";
