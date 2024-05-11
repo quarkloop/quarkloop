9f78767fe9281ea79ddd95722fc2b200743263ef
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2024-05-11 23:51:27 +0200

    Update: web/client/lib/components/src/Org/Org.create.schema.ts

--- web/client/lib/components/src/Org/Org.create.schema.ts
+++ web/client/lib/components/src/Org/Org.create.schema.ts
@@ -3,21 +3,21 @@ import { z } from "zod";
 import { UseHookReturnType } from "@quarkloop/lib";
 import { orgSchema } from "./Org.schema";
 
-export const orgCreateFormSchema = orgSchema.pick({
-    sid: true,
-    name: true,
-    description: true,
-    visibility: true,
-});
+// export const orgCreateFormSchema = orgSchema.pick({
+//     sid: true,
+//     name: true,
+//     description: true,
+//     visibility: true,
+// });
 
-export type OrgCreateForm = z.infer<typeof orgCreateFormSchema>;
+// export type OrgCreateForm = z.infer<typeof orgCreateFormSchema>;
 
-export type OrgCreateHookReturnType = UseHookReturnType<
-    unknown,
-    {
-        onCreateOrg: (values: OrgCreateForm) => Promise<void>;
-    }
->;
+// export type OrgCreateHookReturnType = UseHookReturnType<
+//     unknown,
+//     {
+//         onCreateOrg: (values: OrgCreateForm) => Promise<void>;
+//     }
+// >;
 
 // export type OrgCreateProps = {
 //     onCreateOrg: (values: OrgCreateForm) => Promise<void>;
